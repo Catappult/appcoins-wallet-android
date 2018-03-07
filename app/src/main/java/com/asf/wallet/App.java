35010e932f989dca2fce39600ad364a1b304feb0
Trinkes <fabiojscosta1583@gmail.com>
2018-03-07 16:48:17 +0000

    [RxJava/APPC-82] - solve a bug where app crashes when an UndeliverableException was thrown by RxJava

--- app/src/main/java/com/asf/wallet/App.java
+++ app/src/main/java/com/asf/wallet/App.java
@@ -3,9 +3,12 @@ package com.asf.wallet;
 import android.app.Activity;
 import android.support.multidex.MultiDexApplication;
 import com.asf.wallet.di.DaggerAppComponent;
+import com.crashlytics.android.Crashlytics;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
+import io.reactivex.exceptions.UndeliverableException;
+import io.reactivex.plugins.RxJavaPlugins;
 import io.realm.Realm;
 import javax.inject.Inject;
 
@@ -20,11 +23,26 @@ public class App extends MultiDexApplication implements HasActivityInjector {
         .application(this)
         .build()
         .inject(this);
+    setupRxJava();
 
     // enable pin code for the application
     //		LockManager<CustomPinActivity> lockManager = LockManager.getInstance();
     //		lockManager.enableAppLock(this, CustomPinActivity.class);
     //		lockManager.getAppLock().setShouldShowForgot(false);
+  }
+
+  private void setupRxJava() {
+    RxJavaPlugins.setErrorHandler(throwable -> {
+      if (throwable instanceof UndeliverableException) {
+        if (Crashlytics.getInstance() != null) {
+          Crashlytics.logException(throwable);
+        } else {
+          throwable.printStackTrace();
+        }
+      } else {
+        throw new RuntimeException(throwable);
+      }
+    });
   }
 
   @Override public AndroidInjector<Activity> activityInjector() {
