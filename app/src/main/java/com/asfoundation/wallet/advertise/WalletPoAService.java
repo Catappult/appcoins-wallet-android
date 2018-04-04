279e46fde4ae618a961402957db94c27f1e2e791
João Raimundo <joao.raimundo@aptoide.com>
2018-04-04 17:23:51 +0100

    [APPC 206] Removed logic to comunicate back to the sdk

--- app/src/main/java/com/asfoundation/wallet/advertise/WalletPoAService.java
+++ app/src/main/java/com/asfoundation/wallet/advertise/WalletPoAService.java
@@ -2,29 +2,25 @@ package com.asfoundation.wallet.advertise;
 
 import android.app.Service;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.util.Log;
-import java.util.ArrayList;
-import java.util.List;
 
 import static com.asfoundation.wallet.advertise.ServiceConnector.ACTION_ACK_BROADCAST;
+import static com.asfoundation.wallet.advertise.ServiceConnector.MSG_REGISTER_CAMPAIGN;
+import static com.asfoundation.wallet.advertise.ServiceConnector.MSG_SEND_PROOF;
 import static com.asfoundation.wallet.advertise.ServiceConnector.PARAM_APP_PACKAGE_NAME;
 import static com.asfoundation.wallet.advertise.ServiceConnector.PARAM_APP_SERVICE_NAME;
 import static com.asfoundation.wallet.advertise.ServiceConnector.PARAM_WALLET_PACKAGE_NAME;
-import static com.asfoundation.wallet.advertise.ServiceConnector.PREFERENCE_SDK_PCKG_NAME;
-import static com.asfoundation.wallet.advertise.ServiceConnector.SHARED_PREFS;
 
 /**
  * Created by Joao Raimundo on 29/03/2018.
  */
 
-public class WalletPoAService extends Service implements MessageListener {
+public class WalletPoAService extends Service {
 
   static final String TAG = WalletPoAService.class.getSimpleName();
 
@@ -34,13 +30,8 @@ public class WalletPoAService extends Service implements MessageListener {
    */
   final Messenger serviceMessenger = new Messenger(new IncomingHandler());
 
+  /** Boolean indicating that we are already bound */
   boolean isBound = false;
-
-  ServiceConnector serviceConnector;
-
-  private String appPackageName;
-
-  private String appServiceName;
 
   /**
    * When binding to the service, we return an interface to our messenger for
@@ -58,24 +49,14 @@ public class WalletPoAService extends Service implements MessageListener {
 
   @Override public void onCreate() {
     super.onCreate();
-    ArrayList<MessageListener> listeners = new ArrayList<>();
-    listeners.add(this);
-    serviceConnector = new PoAServiceConnector(listeners);
   }
 
   @Override public int onStartCommand(Intent intent, int flags, int startId) {
     if (!isBound && intent != null) {
       if (intent.hasExtra(PARAM_APP_PACKAGE_NAME)) {
-        // Store the package name of the sdk that triggered the handshake for later use on
-        // the PoA process
-        SharedPreferences preferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(PREFERENCE_SDK_PCKG_NAME, intent.getStringExtra(PARAM_APP_PACKAGE_NAME));
-        editor.commit();
-
         // send intent to confirm that we receive the broadcast and we want to finish the handshake
-        appPackageName = intent.getStringExtra(PARAM_APP_PACKAGE_NAME);
-        appServiceName = intent.getStringExtra(PARAM_APP_SERVICE_NAME);
+        String appPackageName = intent.getStringExtra(PARAM_APP_PACKAGE_NAME);
+        String appServiceName = intent.getStringExtra(PARAM_APP_SERVICE_NAME);
         Log.e(TAG, "Received broadcast for handshake package name: "
             + appPackageName
             + " and service: "
@@ -89,20 +70,6 @@ public class WalletPoAService extends Service implements MessageListener {
       }
     }
     return super.onStartCommand(intent, flags, startId);
-  }
-
-  @Override public void handle(Message message) {
-    switch (message.what) {
-      case MSG_REGISTER_CAMPAIGN:
-        Log.d(TAG, "MSG_REGISTER_CAMPAIGN");
-        break;
-      case MSG_SEND_PROOF:
-        Log.d(TAG, "MSG_SEND_PROOF");
-        break;
-      case MSG_SIGN_PROOF:
-        Log.d(TAG, "MSG_SIGN_PROOF");
-        break;
-    }
   }
 
   /**
