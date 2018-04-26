66597763ab4d29878441deca2b402c683a036b9a
Trinkes <fabiojscosta1583@gmail.com>
2018-04-26 16:18:48 +0100

    [Ads/APPC-276] - iab transaction mapping

--- app/src/main/java/com/asfoundation/wallet/poa/HashCalculator.java
+++ app/src/main/java/com/asfoundation/wallet/poa/HashCalculator.java
@@ -1,6 +1,7 @@
 package com.asfoundation.wallet.poa;
 
 import com.google.gson.Gson;
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.security.NoSuchAlgorithmException;
 
@@ -28,12 +29,29 @@ public class HashCalculator {
   }
 
   public long calculateNonce(NonceData nonceData) throws NoSuchAlgorithmException {
+    int bytesSize;
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+      bytesSize = Long.BYTES;
+    } else {
+      bytesSize = 8;
+    }
+
+    ByteBuffer buffer = ByteBuffer.allocate(nonceData.getPackageName()
+        .length() + bytesSize);
+    buffer.put(nonceData.getPackageName()
+        .getBytes());
+    buffer.putLong(nonceData.getTimeStamp());
+    String hash = calculator.calculate(buffer.array());
+
+    buffer = ByteBuffer.allocate(bytesSize + hash.length());
     String result;
     long nonce = -1;
-    String hash = calculate(nonceData);
     do {
+      buffer.clear();
       nonce++;
-      result = calculator.calculate(convertToBytes(nonce + hash));
+      buffer.putLong(nonce);
+      buffer.put(hash.getBytes());
+      result = calculator.calculate(buffer.array());
     } while (!result.substring(0, leadingString.length())
         .equals(leadingString));
     return nonce;
