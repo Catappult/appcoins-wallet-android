90e4c020a4922d2b467d2a06b148ff319e4b7091
Trinkes <fabiojscosta1583@gmail.com>
2018-08-07 18:12:51 +0100

    [Billing/APPC-548-APPC-551] - correct a bug where payment proof is submitted multiple times

--- app/src/test/java/com/asfoundation/wallet/repository/InAppPurchaseProofSourceTest.java
+++ app/src/test/java/com/asfoundation/wallet/repository/InAppPurchaseProofSourceTest.java
@@ -2,6 +2,7 @@ package com.asfoundation.wallet.repository;
 
 import android.support.annotation.NonNull;
 import com.appcoins.wallet.billing.AuthorizationProof;
+import com.appcoins.wallet.billing.PaymentProof;
 import com.asfoundation.wallet.entity.TransactionBuilder;
 import com.jakewharton.rxrelay2.PublishRelay;
 import io.reactivex.observers.TestObserver;
@@ -32,22 +33,38 @@ import static org.mockito.Mockito.when;
             new CopyOnWriteArrayList<>());
   }
 
-  @Test public void get() {
+  @Test public void getAuthorization() {
     TestObserver<AuthorizationProof> observer = new TestObserver<>();
     inAppPurchaseProofSource.getAuthorization()
         .subscribe(observer);
     List<PaymentTransaction> payments = new ArrayList<>();
-    payments.add(createPayment("approve_hash", null));
+    payments.add(createAuthorization("approve_hash", null));
     paymentPublishRelay.accept(payments);
     paymentPublishRelay.accept(payments);
     observer.assertNoErrors();
+    observer.assertValueCount(1);
     observer.assertValue(new AuthorizationProof("appcoins", "approve_hash", "skuId", "packageName",
         "0xc41b4160b63d1f9488937f7b66640d2babdbf8ad", "0x0965b2a3e664690315ad20b9e5b0336c19cf172e",
         "toAddress"));
-    observer.assertValueCount(1);
   }
 
-  @NonNull private PaymentTransaction createPayment(String approve_hash, String buyHash) {
+  @Test public void getPayment() {
+    TestObserver<PaymentProof> observer = new TestObserver<>();
+    inAppPurchaseProofSource.getPayment()
+        .subscribe(observer);
+    List<PaymentTransaction> payments = new ArrayList<>();
+    payments.add(createAuthorization("approve_hash", "buy_hash"));
+    paymentPublishRelay.accept(payments);
+    paymentPublishRelay.accept(payments);
+    observer.assertNoErrors();
+    observer.assertValueCount(1);
+    observer.assertValue(
+        new PaymentProof("appcoins", "approve_hash", "buy_hash", "skuId", "packageName",
+            com.asf.wallet.BuildConfig.DEFAULT_STORE_ADDRESS,
+            com.asf.wallet.BuildConfig.DEFAULT_OEM_ADDRESS));
+  }
+
+  @NonNull private PaymentTransaction createAuthorization(String approve_hash, String buyHash) {
     return new PaymentTransaction("uri",
         new TransactionBuilder("symbol", "contractAddress", 1L, "toAddress", BigDecimal.ONE,
             "skuId", 18), PaymentTransaction.PaymentState.APPROVED, approve_hash, buyHash,
