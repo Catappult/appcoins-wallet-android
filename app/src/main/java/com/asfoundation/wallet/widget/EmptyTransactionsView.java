990834676a779e3a1b2a35a29f27574b2b649baf
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2018-08-29 00:26:09 +0100

    [APPC-576] - Changed Copy in EmptyTransactionsView.java when on main network.

--- app/src/main/java/com/asfoundation/wallet/widget/EmptyTransactionsView.java
+++ app/src/main/java/com/asfoundation/wallet/widget/EmptyTransactionsView.java
@@ -5,11 +5,13 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import com.asf.wallet.R;
 
 public class EmptyTransactionsView extends FrameLayout {
 
   private final Button airdropButton;
+  private final TextView noTransactionsTextView;
 
   public EmptyTransactionsView(@NonNull Context context, OnClickListener onClickListener,
       boolean isMainNetwork) {
@@ -18,11 +20,13 @@ public class EmptyTransactionsView extends FrameLayout {
     LayoutInflater.from(getContext())
         .inflate(R.layout.layout_empty_transactions, this, true);
 
+    noTransactionsTextView = findViewById(R.id.no_transactions_text);
     airdropButton = findViewById(R.id.action_air_drop);
     findViewById(R.id.action_learn_more).setOnClickListener(onClickListener);
 
     if (isMainNetwork) {
       airdropButton.setVisibility(GONE);
+      noTransactionsTextView.setText(R.string.no_transactions_yet_main);
     } else {
       airdropButton.setOnClickListener(onClickListener);
     }
