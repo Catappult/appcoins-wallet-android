9236625b47235a278011111961408f2205f2f896
Joao Raimundo <joao.raimundo@aptoide.com>
2018-10-15 17:14:56 +0100

    [APPC-608] 1-Fixed PoA tests. 2- Added equals method to ProofSubmissioFeeData class.

--- app/src/main/java/com/asfoundation/wallet/poa/ProofSubmissionFeeData.java
+++ app/src/main/java/com/asfoundation/wallet/poa/ProofSubmissionFeeData.java
@@ -1,13 +1,14 @@
 package com.asfoundation.wallet.poa;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class ProofSubmissionFeeData {
   private final BigDecimal gasLimit;
   private final BigDecimal gasPrice;
   private final RequirementsStatus status;
 
-  public ProofSubmissionFeeData(RequirementsStatus status, BigDecimal gasLimit,
+  public  ProofSubmissionFeeData(RequirementsStatus status, BigDecimal gasLimit,
       BigDecimal gasPrice) {
     this.gasLimit = gasLimit;
     this.gasPrice = gasPrice;
@@ -28,5 +29,18 @@ public class ProofSubmissionFeeData {
 
   public enum RequirementsStatus {
     READY, NO_FUNDS, NO_NETWORK, NO_WALLET
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof ProofSubmissionFeeData)) return false;
+    ProofSubmissionFeeData that = (ProofSubmissionFeeData) o;
+    return Objects.equals(getGasLimit(), that.getGasLimit()) && Objects.equals(getGasPrice(),
+        that.getGasPrice()) && getStatus() == that.getStatus();
+  }
+
+  @Override public int hashCode() {
+
+    return Objects.hash(getGasLimit(), getGasPrice(), getStatus());
   }
 }
