28e174661242710698d9dece8a3f31d77cad348d
Joao Raimundo <joao.raimundo@aptoide.com>
2018-11-30 06:32:00 +0000

    [APPC-736] 1-Improve what's my level screen ui construction. 2-Added bonce animation.

--- app/src/main/java/com/asfoundation/wallet/ui/rewards/MyLevelFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/rewards/MyLevelFragment.kt
@@ -2,14 +2,23 @@ package com.asfoundation.wallet.ui.rewards
 
 import android.app.Fragment
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.Constraints
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import com.asf.wallet.R
 import dagger.android.AndroidInjection
 import dagger.android.DaggerFragment
 
 class MyLevelFragment: Fragment() {
+  var layout: View? = null
+  var img: ImageView? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -17,7 +26,21 @@ class MyLevelFragment: Fragment() {
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_rewards_level, container, false)
+
+    layout = inflater.inflate(R.layout.fragment_rewards_level, container, false)
+    return layout
+  }
+
+  override fun onResume() {
+    super.onResume()
+    val progress = layout?.findViewById(R.id.level_1) as View
+    img = progress.findViewById(R.id.level_active_icon) as ImageView
+    img!!.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.bounce_animation))
+
+    val button = layout?.findViewById(R.id.details_button) as Button
+    button.setOnClickListener {
+      img!!.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.bounce_animation))
+    }
   }
 
 }
