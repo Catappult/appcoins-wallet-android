844aae8c4d8221f16a174126f38bb6bc09099cc7
Joao Raimundo <joao.raimundo@aptoide.com>
2018-12-13 14:09:27 +0000

    [APPC-776] Add fix for web view crash on rotation

--- app/src/main/java/com/asfoundation/wallet/ui/iab/AndroidBug5497Workaround.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/iab/AndroidBug5497Workaround.kt
@@ -7,35 +7,35 @@ import android.widget.FrameLayout
 
 class AndroidBug5497Workaround constructor(val activity: Activity) {
 
-    private val contentContainer = activity.findViewById(android.R.id.content) as ViewGroup
-    private val rootView = contentContainer.getChildAt(0)
-    private val rootViewLayout = rootView.layoutParams as FrameLayout.LayoutParams
-    private val viewTreeObserver = rootView.viewTreeObserver
-    private val listener = { possiblyResizeChildOfContent() }
+  private val contentContainer = activity.findViewById(android.R.id.content) as ViewGroup
+  private val rootView = contentContainer.getChildAt(0)
+  private val listener = { possiblyResizeChildOfContent() }
 
-    private val contentAreaOfWindowBounds = Rect()
-    private var usableHeightPrevious = 0
+  private val contentAreaOfWindowBounds = Rect()
+  private var usableHeightPrevious = 0
 
-    fun addListener() {
-        viewTreeObserver.addOnGlobalLayoutListener(listener)
+  fun addListener() {
+    rootView?.viewTreeObserver?.addOnGlobalLayoutListener(listener)
+  }
+
+  fun removeListener() {
+    if (!activity.isFinishing) {
+      rootView?.viewTreeObserver?.removeOnGlobalLayoutListener(listener)
     }
+  }
 
-    fun removeListener() {
-        if (!activity.isFinishing) {
-            viewTreeObserver.removeOnGlobalLayoutListener(listener)
-        }
+  private fun possiblyResizeChildOfContent() {
+    contentContainer.getWindowVisibleDisplayFrame(contentAreaOfWindowBounds)
+    val usableHeightNow = contentAreaOfWindowBounds.height()
+    if (rootView != null && !(usableHeightPrevious - usableHeightNow >= -1 && usableHeightPrevious - usableHeightNow <= 1)) {
+      val rootViewLayout = rootView.layoutParams as FrameLayout.LayoutParams
+      rootViewLayout.height = usableHeightNow
+      // Change the bounds of the root view to prevent gap between keyboard and content, and top of content positioned above top screen edge.
+      rootView.layout(contentAreaOfWindowBounds.left, contentAreaOfWindowBounds.top,
+          contentAreaOfWindowBounds.right, contentAreaOfWindowBounds.bottom)
+      rootView.requestLayout()
+
+      usableHeightPrevious = usableHeightNow
     }
-
-    private fun possiblyResizeChildOfContent() {
-        contentContainer.getWindowVisibleDisplayFrame(contentAreaOfWindowBounds)
-        val usableHeightNow = contentAreaOfWindowBounds.height()
-        if (!(usableHeightPrevious - usableHeightNow >= -1 && usableHeightPrevious - usableHeightNow <= 1)) {
-            rootViewLayout.height = usableHeightNow
-            // Change the bounds of the root view to prevent gap between keyboard and content, and top of content positioned above top screen edge.
-            rootView.layout(contentAreaOfWindowBounds.left, contentAreaOfWindowBounds.top, contentAreaOfWindowBounds.right, contentAreaOfWindowBounds.bottom)
-            rootView.requestLayout()
-
-            usableHeightPrevious = usableHeightNow
-        }
-    }
+  }
 }
