af483ef6b27aa7e3a824f6a66cd95730db82bc7f
Joao Raimundo <joao.raimundo@aptoide.com>
2019-01-23 11:21:15 +0000

    [APPC-872] Fix content for sku details bundle content

--- bdsbilling/src/main/java/com/appcoins/wallet/bdsbilling/repository/entity/SKU.kt
+++ bdsbilling/src/main/java/com/appcoins/wallet/bdsbilling/repository/entity/SKU.kt
@@ -3,6 +3,6 @@ package com.appcoins.wallet.bdsbilling.repository.entity
 import com.google.gson.annotations.SerializedName
 
 class SKU(val productId: String, val type: String, val price: String,
-          @field:SerializedName("price_currency_code") val currency: String,
-          @field:SerializedName("price_amount_micros") val amount: Double,
+          @field:SerializedName("price_currency") val currency: String,
+          @field:SerializedName("price_amount_micros") val amount: Int,
           val title: String, val description: String)
