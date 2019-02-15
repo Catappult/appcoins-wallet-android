b56ee94a3e4db150a5c5ae66e822802909112873
Joao Raimundo <joao.raimundo@aptoide.com>
2019-02-15 15:19:05 +0000

    [APPC-906] Changes the response handling to be able to have changes on the json returned.

--- app/src/main/java/com/asfoundation/wallet/entity/AppcToLocalFiatResponseBody.java
+++ app/src/main/java/com/asfoundation/wallet/entity/AppcToLocalFiatResponseBody.java
@@ -1,29 +1,25 @@
 package com.asfoundation.wallet.entity;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonInclude(JsonInclude.Include.NON_NULL) @JsonPropertyOrder({
-    "currency", "value"
-}) public class AppcToLocalFiatResponseBody {
+public class AppcToLocalFiatResponseBody {
 
-  @JsonProperty("currency") private String currency;
+  private String currency;
   @JsonProperty("value") private Double appcValue;
 
-  @JsonProperty("currency") public String getCurrency() {
+  public String getCurrency() {
     return currency;
   }
 
-  @JsonProperty("currency") public void setCurrency(String currency) {
+  public void setCurrency(String currency) {
     this.currency = currency;
   }
 
-  @JsonProperty("value") public Double getAppcValue() {
+  public Double getAppcValue() {
     return appcValue;
   }
 
-  @JsonProperty("value") public void setAppcValue(Double appcValue) {
+  public void setAppcValue(Double appcValue) {
     this.appcValue = appcValue;
   }
 }
