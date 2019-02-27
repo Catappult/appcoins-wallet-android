f1f2ef0968569501866bf055a3ad3669ade90f7f
Trinkes <fabiojscosta1583@gmail.com>
2019-02-27 17:29:35 +0000

    [TransactCredits/APPC-964] - close transact screen after terminating send APPC and ETH flows

--- app/src/main/java/com/asfoundation/wallet/ui/transact/TransactFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/transact/TransactFragment.kt
@@ -1,5 +1,6 @@
 package com.asfoundation.wallet.ui.transact
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,12 +11,14 @@ import com.asfoundation.wallet.entity.TransactionBuilder
 import com.asfoundation.wallet.interact.DefaultTokenProvider
 import com.asfoundation.wallet.interact.FindDefaultWalletInteract
 import com.asfoundation.wallet.router.ConfirmationRouter
+import com.asfoundation.wallet.ui.ActivityResultSharer
 import com.jakewharton.rxbinding2.view.RxView
 import dagger.android.support.DaggerFragment
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.currency_choose_layout.*
 import kotlinx.android.synthetic.main.transact_fragment_layout.*
@@ -39,8 +42,15 @@ class TransactFragment : DaggerFragment(), TransactFragmentView {
   @Inject
   lateinit var defaultTokenInfoProvider: DefaultTokenProvider
 
+  lateinit var activityResultSharer: ActivityResultSharer
+  private var disposable: Disposable? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    disposable =
+        confirmationRouter.transactionResult
+            .doOnNext { activity?.onBackPressed() }
+            .subscribe()
     presenter = TransactPresenter(this, CompositeDisposable(), interactor, Schedulers.io(),
         AndroidSchedulers.mainThread(), findDefaultWalletInteract, context!!.packageName)
   }
@@ -78,12 +88,27 @@ class TransactFragment : DaggerFragment(), TransactFragmentView {
     presenter.present()
   }
 
+  override fun onAttach(context: Context?) {
+    super.onAttach(context)
+    when (context) {
+      is ActivityResultSharer -> activityResultSharer = context
+      else -> throw IllegalArgumentException(
+          "${this.javaClass.simpleName} has to be attached to an activity that implements ${ActivityResultSharer::class}")
+    }
+    activityResultSharer.addOnActivityListener(confirmationRouter)
+  }
+
   override fun getSendClick(): Observable<TransactFragmentView.TransactData> {
     return RxView.clicks(send_button).map {
       TransactFragmentView.TransactData(transact_fragment_recipient_address.text.toString(),
           map(currency_selector.checkedRadioButtonId),
           transact_fragment_amount.text.toString().toBigDecimal())
     }
+  }
+
+  override fun onDetach() {
+    activityResultSharer.remove(confirmationRouter)
+    super.onDetach()
   }
 
   override fun onPause() {
@@ -98,5 +123,12 @@ class TransactFragment : DaggerFragment(), TransactFragmentView {
       R.id.ethereum_credits_radio_button -> TransactFragmentView.Currency.ETH
       else -> throw UnsupportedOperationException("Unknown selected currency")
     }
+  }
+
+  override fun onDestroy() {
+    disposable?.takeIf {
+      !it.isDisposed
+    }.let { it?.dispose() }
+    super.onDestroy()
   }
 }
