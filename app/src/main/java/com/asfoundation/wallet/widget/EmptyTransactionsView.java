2493602740d72da99d935d8160780eb38208e572
João Gouveia <joaogouveia@Joaos-MacBook-Pro.local>
2019-03-07 11:20:51 +0000

    [feature/APPC-923] - 1- Add animation JSON file; 2- Add animation to "layout_empty_transactions"; 3- Modify EmptyTransactionsView to play the animation

--- app/src/main/java/com/asfoundation/wallet/widget/EmptyTransactionsView.java
+++ app/src/main/java/com/asfoundation/wallet/widget/EmptyTransactionsView.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import com.airbnb.lottie.LottieAnimationView;
 import com.asf.wallet.R;
 
 public class EmptyTransactionsView extends FrameLayout {
 
   private final Button airdropButton;
   private final TextView noTransactionsTextView;
+  private final LottieAnimationView noTransactionsAnimationView;
 
   public EmptyTransactionsView(@NonNull Context context, OnClickListener onClickListener,
       boolean isMainNetwork) {
@@ -22,11 +24,14 @@ public class EmptyTransactionsView extends FrameLayout {
 
     noTransactionsTextView = findViewById(R.id.no_transactions_text);
     airdropButton = findViewById(R.id.action_air_drop);
+    noTransactionsAnimationView = findViewById(R.id.transactions_empty_screen_animation);
+
     findViewById(R.id.action_learn_more).setOnClickListener(onClickListener);
 
     if (isMainNetwork) {
       airdropButton.setVisibility(GONE);
       noTransactionsTextView.setText(R.string.no_transactions_yet_main);
+      noTransactionsAnimationView.playAnimation();
     } else {
       airdropButton.setOnClickListener(onClickListener);
     }
