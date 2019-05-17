60ea43947c6f2c8661ee46a64e0ff42261202b49
Joao Raimundo <joao.raimundo@aptoide.com>
2019-05-17 14:55:44 +0100

    [APPC-982] 1-Added tokens after the wallet creation in the onboarding flow. 2- Fix UI dimentions of bottom wallet address in payment methods and credita card details dialog.

--- app/src/main/java/com/asfoundation/wallet/ui/onboarding/OnboardingInteract.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/onboarding/OnboardingInteract.kt
@@ -3,18 +3,25 @@ package com.asfoundation.wallet.ui.onboarding
 import com.appcoins.wallet.bdsbilling.WalletService
 import com.asfoundation.wallet.interact.CreateWalletInteract
 import com.asfoundation.wallet.repository.PreferenceRepositoryType
+import com.asfoundation.wallet.repository.TokenRepository
+import com.asfoundation.wallet.repository.TokenRepositoryType
 import io.reactivex.Single
 
 class OnboardingInteract(private val walletInteract: CreateWalletInteract,
                          private val walletService: WalletService,
-                         private val preferenceRepositoryType: PreferenceRepositoryType) {
+                         private val preferenceRepositoryType: PreferenceRepositoryType,
+                         private val tokenRepository: TokenRepositoryType) {
 
   fun getWalletAddress(): Single<String> {
     return walletService.getWalletAddress()
   }
 
   fun createWallet(): Single<String> {
-    return walletInteract.create().map { it.address }
+    return walletInteract.create().flatMap { wallet ->
+      walletInteract.setDefaultWallet(wallet)
+          .andThen(tokenRepository.fetchAll(wallet.address))
+          .firstOrError().map { wallet.address }
+    }
   }
 
   fun finishOnboarding() {
