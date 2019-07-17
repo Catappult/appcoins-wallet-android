95789a61510207272d5e4d059b4a5c489b045b81
Trinkes <fabiojscosta1583@gmail.com>
2019-07-17 17:31:59 +0100

    [Be-Requests/APPC-1213] - Add user agent header to our backend requests

---
+++ app/src/main/java/com/asfoundation/wallet/util/UserAgentInterceptor.java
@@ -0,0 +1,23 @@
+package com.asfoundation.wallet.util;
+
+import java.io.IOException;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class UserAgentInterceptor implements Interceptor {
+
+  private final String userAgent;
+
+  public UserAgentInterceptor(String userAgent) {
+    this.userAgent = userAgent;
+  }
+
+  @Override public Response intercept(Chain chain) throws IOException {
+    Request originalRequest = chain.request();
+    Request requestWithUserAgent = originalRequest.newBuilder()
+        .header("User-Agent", userAgent)
+        .build();
+    return chain.proceed(requestWithUserAgent);
+  }
+}
