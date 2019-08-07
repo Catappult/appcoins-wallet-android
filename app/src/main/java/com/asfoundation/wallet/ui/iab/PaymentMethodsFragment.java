8d045a56b608e826f6133f08622a51930c56b629
carvalho2707 <carvalho_1992@hotmail.com>
2019-08-07 12:13:20 +0100

    [APPC-1161] Fixed selected payment method layout

--- app/src/main/java/com/asfoundation/wallet/ui/iab/PaymentMethodsFragment.java
+++ app/src/main/java/com/asfoundation/wallet/ui/iab/PaymentMethodsFragment.java
@@ -119,6 +119,7 @@ public class PaymentMethodsFragment extends DaggerFragment implements PaymentMet
 
   private View preSelectedMethodView;
   private ImageView preSelectedIcon;
+  private TextView pareSelectedNameSingle;
   private TextView preSelectedName;
   private TextView preSelectedDescription;
 
@@ -213,6 +214,7 @@ public class PaymentMethodsFragment extends DaggerFragment implements PaymentMet
     preSelectedMethodView = view.findViewById(R.id.layout_pre_selected);
     preSelectedIcon = preSelectedMethodView.findViewById(R.id.payment_method_ic);
     preSelectedName = preSelectedMethodView.findViewById(R.id.payment_method_description);
+    pareSelectedNameSingle = preSelectedMethodView.findViewById(R.id.payment_method_description_single);
     preSelectedDescription = preSelectedMethodView.findViewById(R.id.payment_method_secondary);
 
     setupAppNameAndIcon();
@@ -256,6 +258,7 @@ public class PaymentMethodsFragment extends DaggerFragment implements PaymentMet
     preSelectedMethodView = null;
     preSelectedIcon = null;
     preSelectedName = null;
+    pareSelectedNameSingle = null;
     preSelectedDescription = null;
 
     super.onDestroyView();
@@ -625,14 +628,20 @@ public class PaymentMethodsFragment extends DaggerFragment implements PaymentMet
 
     if (paymentMethod.getId()
         .equals(PaymentMethodId.APPC_CREDITS.getId())) {
+      preSelectedName.setVisibility(View.VISIBLE);
+      preSelectedName.setText(paymentMethod.getLabel());
       preSelectedDescription.setVisibility(View.VISIBLE);
+      pareSelectedNameSingle.setVisibility(View.GONE);
       hideBonus();
     } else {
+      preSelectedName.setVisibility(View.VISIBLE);
+      preSelectedName.setText(paymentMethod.getLabel());
       preSelectedDescription.setVisibility(View.GONE);
+      pareSelectedNameSingle.setVisibility(View.GONE);
     }
 
     preSelectedMethodView.setVisibility(View.VISIBLE);
-    preSelectedName.setText(paymentMethod.getLabel());
+
     loadIcons(paymentMethod, preSelectedIcon);
   }
 
