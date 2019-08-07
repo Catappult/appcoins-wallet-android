93e0d0ce989480814a320c9f2f0a9fac7567eae8
José <jose.messejana@gmail.com>
2019-08-07 10:22:48 +0100

    [APPC-1240] Added more documentation to transaction methods

--- app/src/main/java/com/asfoundation/wallet/billing/purchase/InAppDeepLinkRepository.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/purchase/InAppDeepLinkRepository.kt
@@ -7,6 +7,21 @@ interface InAppDeepLinkRepository {
   /**
    * All optional fields should be passed despite possible being null as these are
    * required by some applications to complete the purchase flow
+   * @param domain package name of the application
+   * @param skuId name of the product that is being bought
+   * @param userWalletAddress address of the user wallet
+   * @param signature signature obtained after signing the wallet
+   * @param originalAmount amount of the transaction. Only needed in one step payments
+   * @param originalCurrency currency of the transaction. Only needed in one step payments
+   * @param paymentMethod Name of the payment method being used
+   * @param developerWalletAddress Wallet address of the apps developer
+   * @param storeWalletAddress Wallet address of the store from which the app was downloaded
+   * @param oemWalletAddress Wallet address of the original equipment manufacturer
+   * @param callbackUrl url used in some purchases by the application to complete the purchase
+   * @param orderReference reference used in some purchases by the application to
+   * complete the purchase
+   * @param payload Group of details used in some purchases by the application to
+   * complete the purchase
    */
   fun getDeepLink(domain: String, skuId: String?, userWalletAddress: String,
                   signature: String,
