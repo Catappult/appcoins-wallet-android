58cc8edaaaedaa882ad310e9c601b4216b0f20f0
joaopmgouveia <jpedrom.gouveia@gmail.com>
2019-08-23 09:59:01 +0100

    [feature/APPC-1251] - 1- Delete old icon PNG files and add new icon SVG files; 2- Create "preference_category_layout", "preference_with_summary_layout" and "preference_without_summary_layout" to be compliant with the new design; 3- Change FragmentSettings Java file for a Kotlin one, while also changing the PreferenceFragment to the AndroidX PreferenceFragmentCompat; 4- Remove "onBackPressed" method in SettingsActivity so that when pressing back in a preference the user is brought back to the SettingsFragment; 5- Add SettingsModule to AppComponent

---
+++ app/src/main/java/com/asfoundation/wallet/ui/SettingsFragment.kt
@@ -0,0 +1,187 @@
+package com.asfoundation.wallet.ui
+
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
+import android.view.View
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.asf.wallet.BuildConfig
+import com.asf.wallet.R
+import com.asfoundation.wallet.interact.FindDefaultWalletInteract
+import com.asfoundation.wallet.permissions.manage.view.ManagePermissionsActivity
+import com.asfoundation.wallet.router.ManageWalletsRouter
+import com.google.android.material.snackbar.Snackbar
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+  @Inject
+  internal lateinit var findDefaultWalletInteract: FindDefaultWalletInteract
+  @Inject
+  internal lateinit var manageWalletsRouter: ManageWalletsRouter
+
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    AndroidSupportInjection.inject(this)
+    super.onCreate(savedInstanceState)
+  }
+
+  override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+    setPreferencesFromResource(R.xml.fragment_settings, rootKey)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    val walletPreference = findPreference<Preference>("pref_wallet")
+    walletPreference?.setOnPreferenceClickListener {
+      manageWalletsRouter.open(activity, false)
+      false
+    }
+
+    findDefaultWalletInteract.find()
+        .subscribe({ wallet ->
+          PreferenceManager.getDefaultSharedPreferences(view.context)
+              .edit()
+              .putString("pref_wallet", wallet.address)
+              .apply()
+          walletPreference?.summary = wallet.address
+        }, {})
+
+    val verifyPreference = findPreference<Preference>("pref_verify")
+    //verifyPreference?.setOnPreferenceClickListener {  }
+
+    val permissionPreference = findPreference<Preference>("pref_permissions")
+    permissionPreference?.setOnPreferenceClickListener {
+      Log.d("tag123", "I was here")
+      openPermissionScreen()
+    }
+
+    val redeemPreference = findPreference<Preference>("pref_redeem")
+    redeemPreference?.setOnPreferenceClickListener {
+      findDefaultWalletInteract.find()
+          .subscribe { wallet ->
+            startBrowserActivity(Uri.parse(
+                BuildConfig.MY_APPCOINS_BASE_HOST + "redeem?wallet_address=" + wallet.address),
+                false)
+          }
+      false
+    }
+
+    val sourcecodePreference = findPreference<Preference>("pref_source_code")
+    sourcecodePreference?.setOnPreferenceClickListener {
+      startBrowserActivity(Uri.parse("https://github.com/Aptoide/appcoins-wallet-android"), false)
+      false
+    }
+
+    val bugReportPreference = findPreference<Preference>("pref_report_bug")
+    bugReportPreference?.setOnPreferenceClickListener {
+      startBrowserActivity(
+          Uri.parse("https://github.com/Aptoide/appcoins-wallet-android/issues/new"), false)
+      false
+    }
+
+    val twitterPreference = findPreference<Preference>("pref_twitter")
+    twitterPreference?.setOnPreferenceClickListener {
+      try {
+        activity?.packageManager?.getPackageInfo("com.twitter.android", 0)
+        startBrowserActivity(Uri.parse("twitter://user?user_id=915531221551255552"), true)
+      } catch (e: Exception) {
+        startBrowserActivity(Uri.parse("https://twitter.com/AppCoinsProject"), false)
+      }
+
+      false
+    }
+
+    val facebookPreference = findPreference<Preference>("pref_facebook")
+    facebookPreference?.setOnPreferenceClickListener {
+      startBrowserActivity(Uri.parse("https://www.facebook.com/AppCoinsOfficial"), false)
+      false
+    }
+
+    val telegramPreference = findPreference<Preference>("pref_telegram")
+    telegramPreference?.setOnPreferenceClickListener {
+      startBrowserActivity(Uri.parse("https://t.me/appcoinsofficial"), false)
+      false
+    }
+
+    val emailPreference = findPreference<Preference>("pref_email")
+    emailPreference?.setOnPreferenceClickListener {
+      val mailto = Intent(Intent.ACTION_SEND_MULTIPLE)
+      mailto.type = "message/rfc822" // use from live device
+      mailto.putExtra(Intent.EXTRA_EMAIL, arrayOf("info@appcoins.io"))
+      mailto.putExtra(Intent.EXTRA_SUBJECT, "Android wallet support question")
+      mailto.putExtra(Intent.EXTRA_TEXT, "Dear AppCoins support,")
+
+      startActivity(Intent.createChooser(mailto, "Select email application."))
+      true
+    }
+
+    val privacyPolicyPreference = findPreference<Preference>("pref_privacy_policy")
+    privacyPolicyPreference?.setOnPreferenceClickListener {
+      startBrowserActivity(Uri.parse("https://catappult.io/appcoins-wallet/privacy-policy"), false)
+      false
+    }
+
+    val termsConditionsPreference = findPreference<Preference>("pref_terms_condition")
+    termsConditionsPreference?.setOnPreferenceClickListener {
+      startBrowserActivity(Uri.parse("https://catappult.io/appcoins-wallet/terms-conditions"),
+          false)
+      false
+    }
+
+    val creditsPreference = findPreference<Preference>("pref_credits")
+    creditsPreference?.setOnPreferenceClickListener {
+      AlertDialog.Builder(activity)
+          .setPositiveButton(R.string.close
+          ) { dialog, _ -> dialog.dismiss() }
+          .setMessage(R.string.settings_fragment_credits)
+          .create()
+          .show()
+      true
+    }
+
+    val versionString = getVersion()
+    val versionPreference = findPreference<Preference>("pref_version")
+    versionPreference?.summary = versionString
+  }
+
+  private fun startBrowserActivity(uri: Uri, newTaskFlag: Boolean) {
+    try {
+      val intent = Intent(Intent.ACTION_VIEW, uri)
+      if (newTaskFlag) {
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+      }
+      startActivity(intent)
+    } catch (exception: ActivityNotFoundException) {
+      exception.printStackTrace()
+      if (view != null) {
+        Snackbar.make(view!!, R.string.unknown_error, Snackbar.LENGTH_SHORT)
+            .show()
+      }
+    }
+  }
+
+  private fun openPermissionScreen(): Boolean {
+    startActivity(ManagePermissionsActivity.newIntent(context!!))
+    return true
+  }
+
+  private fun getVersion(): String? {
+    var version: String? = "N/A"
+    try {
+      val pInfo = activity?.packageManager?.getPackageInfo(activity?.packageName, 0)
+      version = pInfo?.versionName
+    } catch (e: PackageManager.NameNotFoundException) {
+      e.printStackTrace()
+    }
+    return version
+  }
+}
