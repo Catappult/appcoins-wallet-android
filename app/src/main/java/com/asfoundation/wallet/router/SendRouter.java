8cf1cdeba8bbff820f2de7fbdc1c034077d03d83
José <jose.messejana@gmail.com>
2019-09-26 14:36:14 +0100

    [APPC-1386] Clicking twice on BN send will no longer open two screens

--- app/src/main/java/com/asfoundation/wallet/router/SendRouter.java
+++ app/src/main/java/com/asfoundation/wallet/router/SendRouter.java
@@ -1,17 +1,11 @@
 package com.asfoundation.wallet.router;
 
 import android.content.Context;
-import com.asfoundation.wallet.entity.TokenInfo;
-import com.asfoundation.wallet.entity.TransactionBuilder;
 import com.asfoundation.wallet.ui.transact.TransferActivity;
 
 public class SendRouter {
 
-  public void open(Context context, TokenInfo tokenInfo) {
-    open(context, new TransactionBuilder(tokenInfo));
-  }
-
-  public void open(Context context, TransactionBuilder transactionBuilder) {
+  public void open(Context context) {
     context.startActivity(TransferActivity.newIntent(context));
   }
 }
