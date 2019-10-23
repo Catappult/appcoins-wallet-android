f6a6c9389b8ff37f82511015ead46abfdab5049d
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2019-10-23 09:42:08 +0100

    [APPC-1443] - Integrated extractor.jar in OemIdExtractorService.kt.

--- app/src/main/java/com/asfoundation/wallet/billing/partners/OemIdExtractorService.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/partners/OemIdExtractorService.kt
@@ -1,20 +1,43 @@
 package com.asfoundation.wallet.billing.partners
 
 import android.content.Context
+import com.aptoide.injectorextractor.Extractor
 import io.reactivex.Single
+import java.io.File
 import java.util.*
 import java.util.zip.ZipFile
 
 
-class OemIdExtractorService(private val context: Context) {
+class OemIdExtractorService(context: Context) {
+
+  val extractorV1: OemIdExtractorV1 = OemIdExtractorV1(context)
+  val extractorV2: OemIdExtractorV2 = OemIdExtractorV2(context)
 
   fun extractOemId(packageName: String): Single<String> {
+    return extractorV2.extract(packageName)
+        .onErrorResumeNext(extractorV1.extract(packageName))
+  }
+}
+
+class OemIdExtractorV2(private val context: Context,
+                       private val extractor: Extractor = Extractor()) : IExtractOemId {
+  override fun extract(packageName: String): Single<String> {
+    return Single.fromCallable {
+      getPackageName(context, packageName)
+    }
+        .flatMap { sourceDir -> Single.fromCallable { extractor.extract(File(sourceDir)) } }
+        .map { t ->
+          t.split(",")[0]
+        }
+  }
+}
+
+class OemIdExtractorV1(private val context: Context) : IExtractOemId {
+  override fun extract(packageName: String): Single<String> {
     return Single.create {
       try {
         var oemId = ""
-        val sourceDir = context.packageManager
-            .getPackageInfo(packageName, 0)
-            .applicationInfo.sourceDir
+        val sourceDir = getPackageName(context, packageName)
         val myZipFile = ZipFile(sourceDir)
         val entry = myZipFile.getEntry("META-INF/attrib")
         entry?.let {
@@ -31,5 +54,14 @@ class OemIdExtractorService(private val context: Context) {
       }
     }
   }
+}
 
+fun getPackageName(context: Context, packageName: String): String {
+  return context.packageManager
+      .getPackageInfo(packageName, 0)
+      .applicationInfo.sourceDir
+}
+
+interface IExtractOemId {
+  fun extract(packageName: String): Single<String>
 }
