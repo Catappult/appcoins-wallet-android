2a0979b006f0bba9989969511150818f59176b9e
carvalho2707 <carvalho_1992@hotmail.com>
2019-11-29 11:32:57 +0000

    Resolved lint issues in bdsbilling

--- bdsbilling/src/main/java/com/appcoins/wallet/bdsbilling/repository/RegisterAuthorizationResponse.kt
+++
@@ -1,4 +0,0 @@
-package com.appcoins.wallet.bdsbilling.repository
-
-data class RegisterAuthorizationResponse(val uid: String, val type: String, val status: String,
-                                         val data: String)
