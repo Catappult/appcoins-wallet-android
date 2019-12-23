be937d1b97e4e1ae25ae3f1dd75f2d0489fa5573
carvalho2707 <carvalho_1992@hotmail.com>
2019-12-23 10:46:48 +0000

    [APPC-1544] Removed debug logic

--- app/src/main/java/com/asfoundation/wallet/promotions/PromotionsModel.kt
+++ app/src/main/java/com/asfoundation/wallet/promotions/PromotionsModel.kt
@@ -2,7 +2,7 @@ package com.asfoundation.wallet.promotions
 
 import java.math.BigDecimal
 
-data class PromotionsModel(var gamificationAvailable: Boolean, var referralsAvailable: Boolean,
+data class PromotionsModel(val gamificationAvailable: Boolean, val referralsAvailable: Boolean,
                            val level: Int, val nextLevelAmount: BigDecimal? = BigDecimal(-1),
                            val totalSpend: BigDecimal, val link: String? = "",
                            val maxValue: BigDecimal, val numberOfInvitations: Int,
