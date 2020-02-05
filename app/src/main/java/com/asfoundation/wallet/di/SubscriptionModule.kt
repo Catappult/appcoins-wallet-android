1833f98533cf7e2bd798f6edf465ce58a64d9dbf
Joao Raimundo <--global>
2020-02-05 15:43:57 +0000

    Fix SubscriptionsService provide method

--- app/src/main/java/com/asfoundation/wallet/di/SubscriptionModule.kt
+++ app/src/main/java/com/asfoundation/wallet/di/SubscriptionModule.kt
@@ -10,22 +10,13 @@ import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 class SubscriptionModule {
 
   @Provides
   fun provideSubscriptionService(client: OkHttpClient, gson: Gson): SubscriptionService {
-    return Retrofit.Builder()
-        .baseUrl("http://google.pt")
-        .client(client)
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .build()
-        .create(SubscriptionService::class.java)
+    return SubscriptionApiMockedImpl()
   }
 
   @Provides
