2cfcd4db5b4192b866f109e4c011fa95fba35bfd
carvalho2707 <carvalho_1992@hotmail.com>
2020-03-02 14:16:20 +0000

    [MOB-312] Updated fetch gas settings logic to reload every 1 minute after being requested

--- app/src/main/java/com/asfoundation/wallet/repository/GasSettingsRepository.kt
+++ app/src/main/java/com/asfoundation/wallet/repository/GasSettingsRepository.kt
@@ -2,41 +2,57 @@ package com.asfoundation.wallet.repository
 
 import com.asfoundation.wallet.entity.GasSettings
 import com.asfoundation.wallet.service.GasService
-import io.reactivex.Observable
 import io.reactivex.Single
 import java.math.BigDecimal
+import java.util.concurrent.TimeUnit
 
 class GasSettingsRepository(private val gasService: GasService) : GasSettingsRepositoryType {
 
+  private var lastFlushTime = 0L
   private var cachedGasPrice: BigDecimal? = null
 
-  private fun getCachedGasPrice(): Observable<BigDecimal> {
-    return Observable.create<BigDecimal> {
-      if (cachedGasPrice != null) {
-        it.onNext(cachedGasPrice!!)
-      }
-      it.onComplete()
+  override fun getGasSettings(forTokenTransfer: Boolean): Single<GasSettings> {
+    return getGasPrice()
+        .map { GasSettings(it, getGasLimit(forTokenTransfer)) }
+  }
+
+  private fun getGasPriceNetwork(): Single<BigDecimal> {
+    return gasService.getGasPrice()
+        .map { BigDecimal(it.price) }
+        .doOnSuccess {
+          cachedGasPrice = it
+          lastFlushTime = System.nanoTime()
+        }
+        .onErrorReturn {
+          if (cachedGasPrice == null) {
+            BigDecimal(DEFAULT_GAS_PRICE)
+          } else {
+            cachedGasPrice
+          }
+        }
+  }
+
+  private fun shouldRefresh() =
+      System.nanoTime() - lastFlushTime >= TimeUnit.MINUTES.toNanos(1)
+
+  private fun getGasPriceLocal(): Single<BigDecimal> {
+    return Single.just(cachedGasPrice)
+  }
+
+  private fun getGasPrice(): Single<BigDecimal> {
+    return if (shouldRefresh()) {
+      getGasPriceNetwork()
+    } else {
+      getGasPriceLocal()
     }
   }
 
-  private fun getCachedGasPriceNetwork(): Observable<BigDecimal> {
-    return gasService.getGasPrice()
-        .map { BigDecimal(it.price) }
-        .onErrorReturn { BigDecimal(DEFAULT_GAS_PRICE) }
-        .toObservable()
-  }
-
-  override fun getGasSettings(forTokenTransfer: Boolean): Single<GasSettings> {
-    return Observable.concat(getCachedGasPrice(), getCachedGasPriceNetwork())
-        .firstElement()
-        .toObservable()
-        .map { GasSettings(it, getGasLimit(forTokenTransfer)) }
-        .firstOrError()
-  }
-
   private fun getGasLimit(forTokenTransfer: Boolean): BigDecimal {
-    return BigDecimal(DEFAULT_GAS_LIMIT_FOR_TOKENS).takeIf { forTokenTransfer } ?: BigDecimal(
-        DEFAULT_GAS_LIMIT)
+    return if (forTokenTransfer) {
+      BigDecimal(DEFAULT_GAS_LIMIT_FOR_TOKENS)
+    } else {
+      BigDecimal(DEFAULT_GAS_LIMIT)
+    }
   }
 
   companion object {
