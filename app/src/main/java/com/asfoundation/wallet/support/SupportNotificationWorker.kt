1cf491ee504ecf49f8576daa20b75d3e54c6d304
José <jose.messejana@gmail.com>
2020-04-21 09:13:04 +0100

    [MOB-457] Enable notification to be swiped

--- app/src/main/java/com/asfoundation/wallet/support/SupportNotificationWorker.kt
+++ app/src/main/java/com/asfoundation/wallet/support/SupportNotificationWorker.kt
@@ -51,8 +51,8 @@ class SupportNotificationWorker @Inject constructor(private val context: Context
     return if (supportInteractor.shouldShowNotification()) {
       supportInteractor.updateUnreadConversations()
       Completable.fromAction {
-            notificationManager.notify(NOTIFICATION_SERVICE_ID, createNotification().build())
-          }
+        notificationManager.notify(NOTIFICATION_SERVICE_ID, createNotification().build())
+      }
           .andThen(Single.just(Result.success()))
     } else {
       Single.just(Result.failure())
@@ -73,7 +73,6 @@ class SupportNotificationWorker @Inject constructor(private val context: Context
     }
     return builder.setContentTitle(context.getString(R.string.support_new_message_title))
         .setAutoCancel(true)
-        .setOngoing(true)
         .setContentIntent(okPendingIntent)
         .addAction(0, context.getString(R.string.dismiss_button), dismissPendingIntent)
         .setSmallIcon(R.drawable.ic_launcher_foreground)
