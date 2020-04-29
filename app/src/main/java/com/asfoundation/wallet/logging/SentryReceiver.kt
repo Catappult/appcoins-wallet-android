8e94fd23a0d20d9a86935ad195d7f7c8fbed7d42
joaopmgouveia <jpedrom.gouveia@gmail.com>
2020-04-29 12:09:46 +0100

    [feature/MOB-416] - 1- Remove "printStackTrace" from the first "log" method in SentryReceiver; 2- Add logic to send all the info available in each "log" method

--- app/src/main/java/com/asfoundation/wallet/logging/SentryReceiver.kt
+++ app/src/main/java/com/asfoundation/wallet/logging/SentryReceiver.kt
@@ -6,19 +6,19 @@ class SentryReceiver : LogReceiver {
 
   override fun log(tag: String?, throwable: Throwable?) {
     throwable?.let {
-      throwable.printStackTrace()
       Sentry.capture(throwable)
     }
   }
 
   override fun log(tag: String?, message: String?) {
     message?.let {
-      Sentry.capture(message)
+      Sentry.capture("$tag: $message")
     }
   }
 
   override fun log(tag: String?, message: String?, throwable: Throwable?) {
     throwable?.let {
+      Sentry.capture("$tag: $message")
       Sentry.capture(it)
     }
   }
