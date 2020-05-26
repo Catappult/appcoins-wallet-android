0de93b74a7e13cdd3e8e3616eb166ca1349fdd57
carvalho2707 <carvalho_1992@hotmail.com>
2020-05-26 14:36:27 +0100

    [MOB-658] Button in adyentopup show start visible

--- app/src/main/java/com/asfoundation/wallet/topup/payment/AdyenTopUpFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/topup/payment/AdyenTopUpFragment.kt
@@ -423,6 +423,7 @@ class AdyenTopUpFragment : DaggerFragment(), AdyenTopUpView {
 
     topUpView.showToolbar()
     main_value.visibility = INVISIBLE
+    button.visibility = VISIBLE
   }
 
   private fun setupCardConfiguration() {
