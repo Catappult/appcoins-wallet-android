34f4c2287ace05e70be8de235f6897ee9ec4a9ef
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2020-06-01 13:29:01 +0100

    [MOB-497] - Use null for default errorDetails.

--- app/src/main/java/com/asfoundation/wallet/billing/analytics/WalletsEventSender.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/analytics/WalletsEventSender.kt
@@ -4,23 +4,24 @@ interface WalletsEventSender {
   fun sendCreateBackupEvent(action: String, context: String,
                             status: String)
 
+  fun sendCreateBackupEvent(action: String, context: String,
+                            status: String, errorDetails: String? = null)
+
   fun sendSaveBackupEvent(action: String)
   fun sendWalletConfirmationBackupEvent(action: String)
 
   fun sendWalletSaveFileEvent(action: String, status: String,
                               errorDetails: String? = null)
 
-  fun sendWalletImportRestoreEvent(action: String, status: String)
   fun sendWalletImportRestoreEvent(action: String, status: String,
-                                   errorDetails: String?)
+                                   errorDetails: String? = null)
 
   fun sendWalletPasswordRestoreEvent(action: String,
                                      status: String)
 
   fun sendWalletPasswordRestoreEvent(action: String, status: String,
-                                     errorDetails: String?)
+                                     errorDetails: String? = null)
 
-  fun sendWalletCompleteRestoreEvent(status: String)
   fun sendWalletCompleteRestoreEvent(status: String,
-                                     errorDetails: String?)
+                                     errorDetails: String? = null)
 }
