6ced661244fea84d5c5b400b5d8c5e391b4fd309
José <jose.messejana@gmail.com>
2020-06-12 15:24:44 +0100

    [MOB-616] Added card to payment methods icons

--- app/src/main/java/com/asfoundation/wallet/ui/onboarding/OnboardingPaymentMethodViewHolder.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/onboarding/OnboardingPaymentMethodViewHolder.kt
@@ -5,11 +5,12 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.asfoundation.wallet.GlideApp
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import kotlinx.android.synthetic.main.layout_page_intro_payment_icon.view.*
 
 class OnboardingPaymentMethodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
   fun bind(imageSrc: String) {
-    val imageView = itemView as ImageView
+    val imageView = itemView.payment_icon as ImageView
 
     GlideApp.with(itemView)
         .load(imageSrc)
