6a2aa203bb997c23cf3bfe8589c73eed4e560b58
José <jose.messejana@gmail.com>
2020-07-06 14:09:48 +0100

    [MOB-768] Added rounded corners. Removed recycler view logic from code to xml

--- app/src/main/java/com/asfoundation/wallet/ui/gamification/GamificationFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/gamification/GamificationFragment.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.appcoins.wallet.gamification.LevelModel
 import com.asf.wallet.R
 import com.asfoundation.wallet.analytics.gamification.GamificationAnalytics
@@ -74,8 +72,6 @@ class GamificationFragment : DaggerFragment(), GamificationView {
                                        shownLevels: List<LevelModel>,
                                        totalSpend: BigDecimal,
                                        updateDate: Date?) {
-    val layoutManager = LinearLayoutManager(context)
-    layoutManager.orientation = RecyclerView.VERTICAL
     levelsAdapter =
         LevelsAdapter(context!!, hiddenLevels, shownLevels, totalSpend, currentLevel,
             nextLevelAmount, formatter,
@@ -83,7 +79,6 @@ class GamificationFragment : DaggerFragment(), GamificationView {
     gamification_recycler_view.addItemDecoration(
         MarginItemDecoration(resources.getDimension(R.dimen.gamification_card_margin)
             .toInt()))
-    gamification_recycler_view.layoutManager = layoutManager
     gamification_recycler_view.adapter = levelsAdapter
     handleBonusUpdatedText(updateDate)
   }
