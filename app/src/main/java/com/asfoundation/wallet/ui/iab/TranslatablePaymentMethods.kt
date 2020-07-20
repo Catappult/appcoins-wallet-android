d1aadbdb5bf33b9883bde3bdd33b965ffeacc814
joaopmgouveia <jpedrom.gouveia@gmail.com>
2020-07-20 13:46:35 +0100

    [bugfix/MOB-747] - 1- Add "EARN_APPCOINS" as a translatable payment method

--- app/src/main/java/com/asfoundation/wallet/ui/iab/TranslatablePaymentMethods.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/iab/TranslatablePaymentMethods.kt
@@ -4,5 +4,6 @@ import com.asf.wallet.R
 
 enum class TranslatablePaymentMethods(val paymentMethod: String, val stringId: Int) {
   ASK_SOMEONE_PAY("ask_friend", R.string.askafriend_payment_option_button),
-  CREDIT_CARD("credit_card", R.string.dialog_bank_card)
+  CREDIT_CARD("credit_card", R.string.dialog_bank_card),
+  EARN_APPCOINS("earn_appcoins", R.string.purchase_poa_item)
 }
