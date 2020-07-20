607d75fe77b5ba56c8a165df18555b45d58630d8
joaopmgouveia <jpedrom.gouveia@gmail.com>
2020-07-20 12:47:27 +0100

    [bugfix/MOB-747] - 1- Create TranslatablePaymentMethods Enum to indicate which payment methods need translations; 2- Add logic to PaymentMethodFragment and to PaymentMethodViewHolder to translate payment methods.

--- app/src/main/java/com/asfoundation/wallet/topup/paymentMethods/PaymentMethodViewHolder.kt
+++ app/src/main/java/com/asfoundation/wallet/topup/paymentMethods/PaymentMethodViewHolder.kt
@@ -3,6 +3,7 @@ package com.asfoundation.wallet.topup.paymentMethods
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.asfoundation.wallet.GlideApp
+import com.asfoundation.wallet.ui.iab.TranslatablePaymentMethods
 import kotlinx.android.synthetic.main.top_up_payment_method_item.view.*
 
 class PaymentMethodViewHolder(itemView: View) :
@@ -13,9 +14,19 @@ class PaymentMethodViewHolder(itemView: View) :
     GlideApp.with(itemView.context)
         .load(data.imageSrc)
         .into(itemView.payment_method_ic)
-    itemView.payment_method_description.text = data.description
+    itemView.payment_method_description.text = setPaymentMethodLabel(data)
     itemView.radio_button.isChecked = checked
     itemView.setOnClickListener(listener)
+  }
+
+  private fun setPaymentMethodLabel(paymentMethod: PaymentMethodData): String {
+    if (TranslatablePaymentMethods.values()
+            .any { it.paymentMethod == paymentMethod.id }) {
+      TranslatablePaymentMethods.values()
+          .first { it.paymentMethod == paymentMethod.id }
+          .let { return itemView.context.getString(it.stringId) }
+    }
+    return paymentMethod.description
   }
 }
 
