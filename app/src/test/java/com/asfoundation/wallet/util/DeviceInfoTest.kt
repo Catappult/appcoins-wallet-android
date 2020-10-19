68a658c091cd43de25e3eecf9456674b7daef906
Joao Raimundo <--global>
2020-10-19 17:16:57 +0100

    Update dev enpoint for myappcoins. Change apk generated name to AppCoins_Wallet_v<version_name>_<version_code>_<build_variant>.apk

--- app/src/test/java/com/asfoundation/wallet/util/DeviceInfoTest.kt
+++ app/src/test/java/com/asfoundation/wallet/util/DeviceInfoTest.kt
@@ -1,6 +1,6 @@
 package com.asfoundation.wallet.util
 
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
