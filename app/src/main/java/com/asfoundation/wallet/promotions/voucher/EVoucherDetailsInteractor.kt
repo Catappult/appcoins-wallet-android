6d0786b9b6732867bca2f15ca7da3c82bec3f613
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2021-02-11 16:37:55 +0000

    [APPC-2240] - SkuButtonModel.kt now receives only string.

--- app/src/main/java/com/asfoundation/wallet/promotions/voucher/EVoucherDetailsInteractor.kt
+++ app/src/main/java/com/asfoundation/wallet/promotions/voucher/EVoucherDetailsInteractor.kt
@@ -6,12 +6,12 @@ class EVoucherDetailsInteractor {
 
   fun getDiamondModels(): List<SkuButtonModel> {
     val skuButtonModels: MutableList<SkuButtonModel> = LinkedList()
-    skuButtonModels.add(SkuButtonModel(43, "Diamonds"))
-    skuButtonModels.add(SkuButtonModel(218, "Diamonds"))
-    skuButtonModels.add(SkuButtonModel(430, "Diamonds"))
-    skuButtonModels.add(SkuButtonModel(43, "Diamonds"))
-    skuButtonModels.add(SkuButtonModel(218, "Diamonds"))
-    skuButtonModels.add(SkuButtonModel(430, "Diamonds"))
+    skuButtonModels.add(SkuButtonModel("43 Diamonds"))
+    skuButtonModels.add(SkuButtonModel("218 Diamonds"))
+    skuButtonModels.add(SkuButtonModel("430 Diamonds"))
+    skuButtonModels.add(SkuButtonModel("43 Diamonds"))
+    skuButtonModels.add(SkuButtonModel("218 Diamonds"))
+    skuButtonModels.add(SkuButtonModel("430 Diamonds"))
     return skuButtonModels
   }
 
--- app/src/main/java/com/asfoundation/wallet/promotions/voucher/SkuButtonModel.kt
+++ app/src/main/java/com/asfoundation/wallet/promotions/voucher/SkuButtonModel.kt
@@ -1,3 +1,3 @@
 package com.asfoundation.wallet.promotions.voucher
 
-class SkuButtonModel(val amount: Int, val s: String)
+class SkuButtonModel(val s: String)
