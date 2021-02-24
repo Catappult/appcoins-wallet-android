266d490b52d83a187ab81a043310f2c8c8a8cc62
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2021-02-24 14:38:43 +0000

    [APPC-2233] - Fix missing LOGGABLE check.

--- app/src/main/java/com/asfoundation/wallet/util/Log.kt
+++ app/src/main/java/com/asfoundation/wallet/util/Log.kt
@@ -1,15 +1,19 @@
 package com.asfoundation.wallet.util
 
 import android.util.Log
+import com.asf.wallet.BuildConfig
 
 class Log private constructor() {
 
   companion object {
     @JvmStatic
-    fun i(tag: String, msg: String): Int = Log.i(tag, msg)
+    fun i(tag: String, msg: String): Int = if (BuildConfig.LOGGABLE) Log.i(tag, msg) else 0
+
     @JvmStatic
-    fun d(tag: String, msg: String): Int = Log.d(tag, msg)
+    fun d(tag: String, msg: String): Int = if (BuildConfig.LOGGABLE) Log.d(tag, msg) else 0
+
     @JvmStatic
-    fun d(tag: String, msg: String, throwable: Throwable): Int = Log.d(tag, msg, throwable)
+    fun d(tag: String, msg: String, throwable: Throwable): Int =
+        if (BuildConfig.LOGGABLE) Log.d(tag, msg, throwable) else 0
   }
 }
