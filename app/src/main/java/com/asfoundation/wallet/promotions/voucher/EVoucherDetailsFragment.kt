80d566d99daef7f62160df65a0f7674f3f0191fe
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2021-02-26 15:32:11 +0000

    [APPC-2240] - Codestyle.

--- app/src/main/java/com/asfoundation/wallet/promotions/voucher/EVoucherDetailsFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/promotions/voucher/EVoucherDetailsFragment.kt
@@ -43,11 +43,8 @@ class EVoucherDetailsFragment : DaggerFragment(), EVoucherDetailsView {
     }
   }
 
-  override fun onCreateView(
-      inflater: LayoutInflater,
-      container: ViewGroup?,
-      savedInstanceState: Bundle?
-  ): View? {
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                            savedInstanceState: Bundle?): View? {
     return inflater.inflate(R.layout.fragment_e_voucher_details, container, false)
   }
 
