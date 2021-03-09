619686b54832ef60df749601d225553615d96d59
franciscoaleixo <francisco.aleixo16@gmail.com>
2021-03-09 11:17:51 +0000

    APPC-2281 - Nullable fields

--- app/src/main/java/com/asfoundation/wallet/vouchers/api/VoucherAppListResponse.kt
+++ app/src/main/java/com/asfoundation/wallet/vouchers/api/VoucherAppListResponse.kt
@@ -1,4 +1,4 @@
 package com.asfoundation.wallet.vouchers.api
 
-data class VoucherAppListResponse(val next: String, val previous: String,
+data class VoucherAppListResponse(val next: String?, val previous: String?,
                                   val items: List<VoucherAppResponse>)
