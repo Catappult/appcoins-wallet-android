9bbdde1c49d2c4f49e537256ea488b5a19d4ee05
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2021-03-17 17:41:41 +0000

    SkillsFragment.kt now uses view binding.

--- skills/src/main/java/cm/aptoide/skills/SkillsFragment.kt
+++ skills/src/main/java/cm/aptoide/skills/SkillsFragment.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
-import cm.aptoide.skills.interfaces.WalletAddressObtainer
+import cm.aptoide.skills.databinding.FragmentSkillsBinding
 import dagger.android.support.DaggerFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -24,19 +21,15 @@ class SkillsFragment : DaggerFragment() {
   }
 
   @Inject
-  lateinit var walletAddressObtainer: WalletAddressObtainer
-
-  @Inject
   lateinit var viewModel: SkillsViewModel
   private lateinit var disposable: CompositeDisposable
 
-  private lateinit var button: Button
-  private lateinit var progressBar: ProgressBar
-  private lateinit var progressBarTV: TextView
+  private lateinit var binding: FragmentSkillsBinding
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_skills, container, false)
+    binding = FragmentSkillsBinding.inflate(inflater, container, false)
+    return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,11 +37,7 @@ class SkillsFragment : DaggerFragment() {
 
     disposable = CompositeDisposable()
 
-    button = view.findViewById(R.id.find_opponent_button)
-    progressBar = view.findViewById(R.id.progress_bar)
-    progressBarTV = view.findViewById(R.id.progress_bar_tv)
-
-    button.setOnClickListener {
+    binding.findOpponentButton.setOnClickListener {
       disposable.add(viewModel.createTicket()
           .observeOn(AndroidSchedulers.mainThread())
           .doOnSubscribe({ showLoading(R.string.creating_ticket) })
@@ -67,12 +56,12 @@ class SkillsFragment : DaggerFragment() {
   }
 
   private fun showLoading(textId: Int) {
-    button.visibility = View.GONE
+    binding.findOpponentButton.visibility = View.GONE
 
-    progressBarTV.text = requireContext().resources.getString(textId)
+    binding.progressBarTv.text = requireContext().resources.getString(textId)
 
-    progressBar.visibility = View.VISIBLE
-    progressBarTV.visibility = View.VISIBLE
+    binding.progressBar.visibility = View.VISIBLE
+    binding.progressBarTv.visibility = View.VISIBLE
   }
 
   private fun buildDataIntent(): Intent {
