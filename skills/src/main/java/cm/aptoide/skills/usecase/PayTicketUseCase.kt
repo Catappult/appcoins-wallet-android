9be46620274c9bdb0b68bfd0251de1daf72ef911
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2021-03-31 17:14:47 +0100

    PayTicketUseCase.kt now subscribes on io scheduler.

--- skills/src/main/java/cm/aptoide/skills/usecase/PayTicketUseCase.kt
+++ skills/src/main/java/cm/aptoide/skills/usecase/PayTicketUseCase.kt
@@ -2,10 +2,12 @@ package cm.aptoide.skills.usecase
 
 import cm.aptoide.skills.repository.TicketRepository
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 class PayTicketUseCase(private val ticketRepository: TicketRepository) {
 
   fun payTicket(ticketId: String, callbackUrl: String): Single<Any> {
     return ticketRepository.payTicket(ticketId, callbackUrl)
+        .subscribeOn(Schedulers.io())
   }
 }
