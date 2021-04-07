adbd3081dfdd46f5ccf5f68fb62150204f6ac452
Pedro Bilro <pedro.bilro@tecnico.ulisboa.pt>
2021-04-07 12:25:16 +0100

    [APPC-2365] Correction on wallet origin retrieval from DB
    For new installs, only one wallet origin is associated with the same wallet address. 
    For old versions, the wallet origin using the wallet from OnboardingInteractor (with lowercase letters) is not used, because now comparison with wallet address on DB is now case sensitive (using '=' instead of like).
    
    Also, registers on intercom/zendesk now always done with lowercase wallet address

--- app/src/main/java/com/asfoundation/wallet/support/SupportInteractor.kt
+++ app/src/main/java/com/asfoundation/wallet/support/SupportInteractor.kt
@@ -33,7 +33,7 @@ class SupportInteractor(private val supportRepository: SupportRepository,
 
   fun showSupport(walletAddress: String, gamificationLevel: Int): Completable {
     return Completable.fromAction {
-      registerUser(gamificationLevel, walletAddress.toLowerCase(Locale.ROOT))
+      registerUser(gamificationLevel, walletAddress)
       displayChatScreen()
     }
   }
@@ -60,13 +60,16 @@ class SupportInteractor(private val supportRepository: SupportRepository,
   }
 
   fun registerUser(level: Int, walletAddress: String) {
+    // force lowercase to make sure 2 users are not registered with the same wallet address, where
+    // one has uppercase letters (to be check summed), and the other does not
+    val address = walletAddress.toLowerCase(Locale.ROOT)
     val currentUser = supportRepository.getCurrentUser()
-    if (currentUser.userAddress != walletAddress || currentUser.gamificationLevel != level) {
-      if (currentUser.userAddress != walletAddress) {
+    if (currentUser.userAddress != address || currentUser.gamificationLevel != level) {
+      if (currentUser.userAddress != address) {
         Intercom.client()
             .logout()
       }
-      supportRepository.saveNewUser(walletAddress, level)
+      supportRepository.saveNewUser(address, level)
     }
   }
 
