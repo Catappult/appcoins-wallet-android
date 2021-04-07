e778c6f31f2a0550b5734b0c6209af21b9baf565
Pedro Bilro <pedro.bilro@tecnico.ulisboa.pt>
2021-04-07 09:48:35 +0100

    [APPC-2365] Improvements in codestyle - SubscriptionItem.kt

--- app/src/main/java/com/asfoundation/wallet/subscriptions/SubscriptionItem.kt
+++ app/src/main/java/com/asfoundation/wallet/subscriptions/SubscriptionItem.kt
@@ -5,15 +5,13 @@ import java.io.Serializable
 import java.math.BigDecimal
 import java.util.*
 
-data class SubscriptionItem(
-    val itemName: String, val period: Period?, val status: Status,
-    val started: Date?, val renewal: Date?, val expiry: Date?,
-    val ended: Date?, val packageName: String, val appName: String,
-    val appIcon: String, val fiatAmount: BigDecimal, val fiatSymbol: String,
-    val currency: String, val paymentMethod: String,
-    val paymentIcon: String, val appcAmount: BigDecimal,
-    val appcLabel: String, val uid: String
-) : Serializable {
+data class SubscriptionItem(val itemName: String, val period: Period?, val status: Status,
+                            val started: Date?, val renewal: Date?, val expiry: Date?,
+                            val ended: Date?, val packageName: String, val appName: String,
+                            val appIcon: String, val fiatAmount: BigDecimal, val fiatSymbol: String,
+                            val currency: String, val paymentMethod: String,
+                            val paymentIcon: String, val appcAmount: BigDecimal,
+                            val appcLabel: String, val uid: String) : Serializable {
 
   fun isActiveSubscription(): Boolean {
     return status == Status.ACTIVE || status == Status.CANCELED || status == Status.PAUSED ||
