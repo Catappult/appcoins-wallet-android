e46824bffa924d84b0bb7a302d58d385a934fae3
danfbfern <danielfernando.1608@gmail.com>
2021-04-29 10:56:36 +0100

    [APPC-2353] cleaned bottomsheet navigator

--- app/src/main/java/com/asfoundation/wallet/transfers/TransferConfirmationNavigator.kt
+++ app/src/main/java/com/asfoundation/wallet/transfers/TransferConfirmationNavigator.kt
@@ -7,16 +7,6 @@ class TransferConfirmationNavigator(private val fragmentManager: FragmentManager
 
   fun showEtherTransactionBottomSheet(transactionHash: String) {
     val bottomSheet = EtherTransactionBottomSheetFragment.newInstance(transactionHash)
-    fragmentManager.beginTransaction()
-        .setCustomAnimations(R.anim.fragment_slide_up, R.anim.fragment_slide_down,
-            R.anim.fragment_slide_up, R.anim.fragment_slide_down)
-        .add(bottomSheet, "EtherBottomSheet")
-        .show(bottomSheet)
-        .commit()
-  }
-
-  fun showEtherTransactionBottomSheet1(transactionHash: String) {
-    val bottomSheet = EtherTransactionBottomSheetFragment.newInstance(transactionHash)
     bottomSheet.show(fragmentManager, "EtherBottomSheet")
   }
 }
