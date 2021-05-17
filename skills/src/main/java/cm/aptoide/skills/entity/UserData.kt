7bac4635ef4bd9695ae7001a65407cf4a21b2837
Neurophobic Animal <FredericoSantinho@gmail.com>
2021-05-17 18:01:28 +0100

    [ESKL-11] - Added support to refunded ticket.

--- skills/src/main/java/cm/aptoide/skills/entity/UserData.kt
+++ skills/src/main/java/cm/aptoide/skills/entity/UserData.kt
@@ -1,4 +1,6 @@
 package cm.aptoide.skills.entity
 
-data class UserData(val userId: String, val roomId: String, val walletAddress: String,
-                    val session: String)
+data class UserData(
+  val userId: String, val roomId: String, val walletAddress: String,
+  val session: String, val refunded: Boolean = false
+)
