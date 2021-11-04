61403fc79cb903a4f9c16f66b7f2569d8430cb43
pedrotrincheiras <pbtrincheiras@gmail.com>
2021-11-04 11:24:54 +0000

    Deal With no nfts and no image in asset

--- app/src/main/java/com/asfoundation/wallet/nfts/domain/NFTItem.kt
+++
@@ -1,8 +0,0 @@
-package com.asfoundation.wallet.nfts.domain
-
-import java.io.Serializable
-
-data class NFTItem(val name: String, val description: String?, val imageURL : String , val id : String) :
-  Serializable
-
-
--- app/src/main/java/com/asfoundation/wallet/nfts/list/NFTClick.kt
+++
@@ -1,9 +0,0 @@
-package com.asfoundation.wallet.nfts.list
-
-import androidx.navigation.fragment.FragmentNavigator
-import com.asfoundation.wallet.nfts.domain.NFTItem
-
-data class NFTClick (
-    val data: NFTItem,
-    val extras: FragmentNavigator.Extras
-)
