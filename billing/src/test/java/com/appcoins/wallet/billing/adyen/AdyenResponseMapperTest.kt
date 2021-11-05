4ceb5f5b6f9a0cfd0b8f5de6597bafb11a9585f5
franciscoaleixo <francisco.aleixo16@gmail.com>
2021-11-05 14:24:24 +0000

    APPC-2707 - Fix tests

--- billing/src/test/java/com/appcoins/wallet/billing/adyen/AdyenResponseMapperTest.kt
+++ billing/src/test/java/com/appcoins/wallet/billing/adyen/AdyenResponseMapperTest.kt
@@ -332,7 +332,7 @@ class AdyenResponseMapperTest {
   @Test
   fun mapVerificationPaymentModelSuccessTest() {
     val expectedModel = VerificationPaymentModel(true)
-    val model = mapper.mapVerificationPaymentModelSuccess(it)
+    val model = mapper.mapVerificationPaymentModelSuccess()
     Assert.assertEquals(expectedModel, model)
   }
 
@@ -344,7 +344,7 @@ class AdyenResponseMapperTest {
     Mockito.`when`(billingErrorMapper.mapErrorInfo(null, "Error"))
         .thenReturn(errorInfo)
     val expectedModel =
-        VerificationPaymentModel(false, VerificationPaymentModel.ErrorType.OTHER, null, null,
+        VerificationPaymentModel(false, VerificationPaymentModel.ErrorType.OTHER, null, null, null,
             Error(true, false, errorInfo))
     val model = mapper.mapVerificationPaymentModelError(throwable)
     Assert.assertEquals(expectedModel, model)
@@ -363,7 +363,7 @@ class AdyenResponseMapperTest {
     val throwable = HttpException(errorResponse)
     val expectedModel =
         VerificationPaymentModel(false, VerificationPaymentModel.ErrorType.INVALID_REQUEST,
-            "CVC Declined", 24,
+            "CVC Declined", 24, null,
             Error(true, false))
     val model = mapper.mapVerificationPaymentModelError(throwable)
     Assert.assertEquals(expectedModel, model)
@@ -382,7 +382,7 @@ class AdyenResponseMapperTest {
     val throwable = HttpException(errorResponse)
     val expectedModel =
         VerificationPaymentModel(false, VerificationPaymentModel.ErrorType.TOO_MANY_ATTEMPTS,
-            "CVC Declined", 24,
+            "CVC Declined", 24, null,
             Error(true, false))
     val model = mapper.mapVerificationPaymentModelError(throwable)
     Assert.assertEquals(expectedModel, model)
@@ -401,7 +401,7 @@ class AdyenResponseMapperTest {
     val throwable = HttpException(errorResponse)
     val expectedModel =
         VerificationPaymentModel(false, VerificationPaymentModel.ErrorType.OTHER, "CVC Declined",
-            24,
+            24, null,
             Error(true, false))
     val model = mapper.mapVerificationPaymentModelError(throwable)
     Assert.assertEquals(expectedModel, model)
