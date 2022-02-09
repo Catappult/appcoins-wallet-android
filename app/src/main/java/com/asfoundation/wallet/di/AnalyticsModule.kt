96f2e3bc897d8c5b27f4c55f50dd8b5c1f0d240d
Sergei Mitrofanov <goretz.m@gmail.com>
2022-02-09 22:14:11 +0000

    Fix code style and lint warnings

--- app/src/main/java/com/asfoundation/wallet/di/AnalyticsModule.kt
+++ app/src/main/java/com/asfoundation/wallet/di/AnalyticsModule.kt
@@ -42,84 +42,90 @@ class AnalyticsModule {
   @Provides
   @Named("bi_event_list")
   fun provideBiEventList() =
-      listOf(BillingAnalytics.PURCHASE_DETAILS, BillingAnalytics.PAYMENT_METHOD_DETAILS,
-          BillingAnalytics.PAYMENT, PoaAnalytics.POA_STARTED, PoaAnalytics.POA_COMPLETED)
+    listOf(
+      BillingAnalytics.PURCHASE_DETAILS, BillingAnalytics.PAYMENT_METHOD_DETAILS,
+      BillingAnalytics.PAYMENT, PoaAnalytics.POA_STARTED, PoaAnalytics.POA_COMPLETED
+    )
 
   @Singleton
   @Provides
   @Named("rakam_event_list")
   fun provideRakamEventList() =
-      listOf(LaunchAnalytics.FIRST_LAUNCH, HomeAnalytics.WALLET_HOME_INTERACTION_EVENT,
-          BillingAnalytics.RAKAM_PRESELECTED_PAYMENT_METHOD, BillingAnalytics.RAKAM_PAYMENT_METHOD,
-          BillingAnalytics.RAKAM_PAYMENT_CONFIRMATION, BillingAnalytics.RAKAM_PAYMENT_CONCLUSION,
-          BillingAnalytics.RAKAM_PAYMENT_START, BillingAnalytics.RAKAM_PAYPAL_URL,
-          BillingAnalytics.RAKAM_PAYMENT_METHOD_DETAILS, BillingAnalytics.RAKAM_PAYMENT_BILLING,
-          TopUpAnalytics.WALLET_TOP_UP_START, TopUpAnalytics.WALLET_TOP_UP_SELECTION,
-          TopUpAnalytics.WALLET_TOP_UP_CONFIRMATION, TopUpAnalytics.WALLET_TOP_UP_CONCLUSION,
-          TopUpAnalytics.WALLET_TOP_UP_PAYPAL_URL, TopUpAnalytics.RAKAM_TOP_UP_BILLING,
-          PoaAnalytics.RAKAM_POA_EVENT, WalletsAnalytics.WALLET_CREATE_BACKUP,
-          WalletsAnalytics.WALLET_SAVE_BACKUP, WalletsAnalytics.WALLET_CONFIRMATION_BACKUP,
-          WalletsAnalytics.WALLET_SAVE_FILE, WalletsAnalytics.WALLET_IMPORT_RESTORE,
-          WalletsAnalytics.WALLET_MY_WALLETS_INTERACTION_EVENT,
-          WalletsAnalytics.WALLET_PASSWORD_RESTORE, PageViewAnalytics.WALLET_PAGE_VIEW,
-          TopUpABTestingAnalytics.TOPUP_DEFAULT_VALUE_PARTICIPATING_EVENT,
-          RatingAnalytics.WALLET_RATING_WELCOME_EVENT, RatingAnalytics.WALLET_RATING_POSITIVE_EVENT,
-          RatingAnalytics.WALLET_RATING_NEGATIVE_EVENT, RatingAnalytics.WALLET_RATING_FINISH_EVENT,
-          VerificationAnalytics.START_EVENT, VerificationAnalytics.INSERT_CARD_EVENT,
-          VerificationAnalytics.REQUEST_CONCLUSION_EVENT, VerificationAnalytics.CONFIRM_EVENT,
-          VerificationAnalytics.CONCLUSION_EVENT,
-          PaymentMethodsAnalytics.WALLET_PAYMENT_LOADING_TOTAL,
-          PaymentMethodsAnalytics.WALLET_PAYMENT_LOADING_STEP)
+    listOf(
+      LaunchAnalytics.FIRST_LAUNCH, HomeAnalytics.WALLET_HOME_INTERACTION_EVENT,
+      BillingAnalytics.RAKAM_PRESELECTED_PAYMENT_METHOD, BillingAnalytics.RAKAM_PAYMENT_METHOD,
+      BillingAnalytics.RAKAM_PAYMENT_CONFIRMATION, BillingAnalytics.RAKAM_PAYMENT_CONCLUSION,
+      BillingAnalytics.RAKAM_PAYMENT_START, BillingAnalytics.RAKAM_PAYPAL_URL,
+      BillingAnalytics.RAKAM_PAYMENT_METHOD_DETAILS, BillingAnalytics.RAKAM_PAYMENT_BILLING,
+      TopUpAnalytics.WALLET_TOP_UP_START, TopUpAnalytics.WALLET_TOP_UP_SELECTION,
+      TopUpAnalytics.WALLET_TOP_UP_CONFIRMATION, TopUpAnalytics.WALLET_TOP_UP_CONCLUSION,
+      TopUpAnalytics.WALLET_TOP_UP_PAYPAL_URL, TopUpAnalytics.RAKAM_TOP_UP_BILLING,
+      PoaAnalytics.RAKAM_POA_EVENT, WalletsAnalytics.WALLET_CREATE_BACKUP,
+      WalletsAnalytics.WALLET_SAVE_BACKUP, WalletsAnalytics.WALLET_CONFIRMATION_BACKUP,
+      WalletsAnalytics.WALLET_SAVE_FILE, WalletsAnalytics.WALLET_IMPORT_RESTORE,
+      WalletsAnalytics.WALLET_MY_WALLETS_INTERACTION_EVENT,
+      WalletsAnalytics.WALLET_PASSWORD_RESTORE, PageViewAnalytics.WALLET_PAGE_VIEW,
+      TopUpABTestingAnalytics.TOPUP_DEFAULT_VALUE_PARTICIPATING_EVENT,
+      RatingAnalytics.WALLET_RATING_WELCOME_EVENT, RatingAnalytics.WALLET_RATING_POSITIVE_EVENT,
+      RatingAnalytics.WALLET_RATING_NEGATIVE_EVENT, RatingAnalytics.WALLET_RATING_FINISH_EVENT,
+      VerificationAnalytics.START_EVENT, VerificationAnalytics.INSERT_CARD_EVENT,
+      VerificationAnalytics.REQUEST_CONCLUSION_EVENT, VerificationAnalytics.CONFIRM_EVENT,
+      VerificationAnalytics.CONCLUSION_EVENT,
+      PaymentMethodsAnalytics.WALLET_PAYMENT_LOADING_TOTAL,
+      PaymentMethodsAnalytics.WALLET_PAYMENT_LOADING_STEP
+    )
 
   @Singleton
   @Provides
   @Named("indicative_event_list")
   fun provideIndicativeEventList() =
-      listOf(LaunchAnalytics.FIRST_LAUNCH, HomeAnalytics.WALLET_HOME_INTERACTION_EVENT,
-          BillingAnalytics.RAKAM_PRESELECTED_PAYMENT_METHOD, BillingAnalytics.RAKAM_PAYMENT_METHOD,
-          BillingAnalytics.RAKAM_PAYMENT_CONFIRMATION, BillingAnalytics.RAKAM_PAYMENT_CONCLUSION,
-          BillingAnalytics.RAKAM_PAYMENT_START, BillingAnalytics.RAKAM_PAYPAL_URL,
-          BillingAnalytics.RAKAM_PAYMENT_METHOD_DETAILS, BillingAnalytics.RAKAM_PAYMENT_BILLING,
-          TopUpAnalytics.WALLET_TOP_UP_START, TopUpAnalytics.WALLET_TOP_UP_SELECTION,
-          TopUpAnalytics.WALLET_TOP_UP_CONFIRMATION, TopUpAnalytics.WALLET_TOP_UP_CONCLUSION,
-          TopUpAnalytics.WALLET_TOP_UP_PAYPAL_URL, TopUpAnalytics.RAKAM_TOP_UP_BILLING,
-          PoaAnalytics.RAKAM_POA_EVENT, WalletsAnalytics.WALLET_CREATE_BACKUP,
-          WalletsAnalytics.WALLET_SAVE_BACKUP, WalletsAnalytics.WALLET_CONFIRMATION_BACKUP,
-          WalletsAnalytics.WALLET_SAVE_FILE, WalletsAnalytics.WALLET_IMPORT_RESTORE,
-          WalletsAnalytics.WALLET_MY_WALLETS_INTERACTION_EVENT,
-          WalletsAnalytics.WALLET_PASSWORD_RESTORE, PageViewAnalytics.WALLET_PAGE_VIEW,
-          TopUpABTestingAnalytics.TOPUP_DEFAULT_VALUE_PARTICIPATING_EVENT,
-          RatingAnalytics.WALLET_RATING_WELCOME_EVENT, RatingAnalytics.WALLET_RATING_POSITIVE_EVENT,
-          RatingAnalytics.WALLET_RATING_NEGATIVE_EVENT, RatingAnalytics.WALLET_RATING_FINISH_EVENT,
-          VerificationAnalytics.START_EVENT, VerificationAnalytics.INSERT_CARD_EVENT,
-          VerificationAnalytics.REQUEST_CONCLUSION_EVENT, VerificationAnalytics.CONFIRM_EVENT,
-          VerificationAnalytics.CONCLUSION_EVENT,
-          PaymentMethodsAnalytics.WALLET_PAYMENT_LOADING_TOTAL,
-          PaymentMethodsAnalytics.WALLET_PAYMENT_LOADING_STEP)
+    listOf(
+      LaunchAnalytics.FIRST_LAUNCH, HomeAnalytics.WALLET_HOME_INTERACTION_EVENT,
+      BillingAnalytics.RAKAM_PRESELECTED_PAYMENT_METHOD, BillingAnalytics.RAKAM_PAYMENT_METHOD,
+      BillingAnalytics.RAKAM_PAYMENT_CONFIRMATION, BillingAnalytics.RAKAM_PAYMENT_CONCLUSION,
+      BillingAnalytics.RAKAM_PAYMENT_START, BillingAnalytics.RAKAM_PAYPAL_URL,
+      BillingAnalytics.RAKAM_PAYMENT_METHOD_DETAILS, BillingAnalytics.RAKAM_PAYMENT_BILLING,
+      TopUpAnalytics.WALLET_TOP_UP_START, TopUpAnalytics.WALLET_TOP_UP_SELECTION,
+      TopUpAnalytics.WALLET_TOP_UP_CONFIRMATION, TopUpAnalytics.WALLET_TOP_UP_CONCLUSION,
+      TopUpAnalytics.WALLET_TOP_UP_PAYPAL_URL, TopUpAnalytics.RAKAM_TOP_UP_BILLING,
+      PoaAnalytics.RAKAM_POA_EVENT, WalletsAnalytics.WALLET_CREATE_BACKUP,
+      WalletsAnalytics.WALLET_SAVE_BACKUP, WalletsAnalytics.WALLET_CONFIRMATION_BACKUP,
+      WalletsAnalytics.WALLET_SAVE_FILE, WalletsAnalytics.WALLET_IMPORT_RESTORE,
+      WalletsAnalytics.WALLET_MY_WALLETS_INTERACTION_EVENT,
+      WalletsAnalytics.WALLET_PASSWORD_RESTORE, PageViewAnalytics.WALLET_PAGE_VIEW,
+      TopUpABTestingAnalytics.TOPUP_DEFAULT_VALUE_PARTICIPATING_EVENT,
+      RatingAnalytics.WALLET_RATING_WELCOME_EVENT, RatingAnalytics.WALLET_RATING_POSITIVE_EVENT,
+      RatingAnalytics.WALLET_RATING_NEGATIVE_EVENT, RatingAnalytics.WALLET_RATING_FINISH_EVENT,
+      VerificationAnalytics.START_EVENT, VerificationAnalytics.INSERT_CARD_EVENT,
+      VerificationAnalytics.REQUEST_CONCLUSION_EVENT, VerificationAnalytics.CONFIRM_EVENT,
+      VerificationAnalytics.CONCLUSION_EVENT,
+      PaymentMethodsAnalytics.WALLET_PAYMENT_LOADING_TOTAL,
+      PaymentMethodsAnalytics.WALLET_PAYMENT_LOADING_STEP
+    )
 
   @Singleton
   @Provides
-  fun provideAnalyticsManager(@Named("default") okHttpClient: OkHttpClient, api: AnalyticsAPI,
-                              context: Context,
-                              @Named("bi_event_list") biEventList: List<String>,
-                              @Named("rakam_event_list") rakamEventList: List<String>,
-                              @Named("indicative_event_list") indicativeEventList: List<String>,
-                              indicativeAnalytics: IndicativeAnalytics
-                              ): AnalyticsManager {
+  fun provideAnalyticsManager(
+    @Named("default") okHttpClient: OkHttpClient, api: AnalyticsAPI,
+    @Named("bi_event_list") biEventList: List<String>,
+    @Named("rakam_event_list") rakamEventList: List<String>,
+    @Named("indicative_event_list") indicativeEventList: List<String>,
+    indicativeAnalytics: IndicativeAnalytics
+  ): AnalyticsManager {
     return AnalyticsManager.Builder()
-        .addLogger(BackendEventLogger(api), biEventList)
-        .addLogger(IndicativeEventLogger(indicativeAnalytics), indicativeEventList)
-        .addLogger(RakamEventLogger(), rakamEventList)
-        .setAnalyticsNormalizer(KeysNormalizer())
-        .setDebugLogger(LogcatAnalyticsLogger())
-        .setKnockLogger(HttpClientKnockLogger(okHttpClient))
-        .build()
+      .addLogger(BackendEventLogger(api), biEventList)
+      .addLogger(IndicativeEventLogger(indicativeAnalytics), indicativeEventList)
+      .addLogger(RakamEventLogger(), rakamEventList)
+      .setAnalyticsNormalizer(KeysNormalizer())
+      .setDebugLogger(LogcatAnalyticsLogger())
+      .setKnockLogger(HttpClientKnockLogger(okHttpClient))
+      .build()
   }
 
   @Singleton
   @Provides
   fun provideWalletEventSender(analytics: AnalyticsManager): WalletsEventSender =
-      WalletsAnalytics(analytics)
+    WalletsAnalytics(analytics)
 
   @Singleton
   @Provides
@@ -143,21 +149,29 @@ class AnalyticsModule {
 
   @Singleton
   @Provides
-  fun provideRakamAnalyticsSetup(context: Context, idsRepository: IdsRepository,
-                                 promotionsRepository: PromotionsRepository, logger: Logger,
-                                 promoCodeLocalDataSource: PromoCodeLocalDataSource): RakamAnalytics {
-    return RakamAnalytics(context, idsRepository, promotionsRepository, logger,
-        promoCodeLocalDataSource)
+  fun provideRakamAnalyticsSetup(
+    context: Context, idsRepository: IdsRepository,
+    promotionsRepository: PromotionsRepository, logger: Logger,
+    promoCodeLocalDataSource: PromoCodeLocalDataSource
+  ): RakamAnalytics {
+    return RakamAnalytics(
+      context, idsRepository, promotionsRepository, logger,
+      promoCodeLocalDataSource
+    )
   }
 
-    @Singleton
-    @Provides
-    fun provideIndicativeAnalyticsSetup(context: Context, idsRepository: IdsRepository,
-                                   promotionsRepository: PromotionsRepository, logger: Logger,
-                                   promoCodeLocalDataSource: PromoCodeLocalDataSource): IndicativeAnalytics {
-        return IndicativeAnalytics(context, idsRepository, promotionsRepository, logger,
-            promoCodeLocalDataSource)
-    }
+  @Singleton
+  @Provides
+  fun provideIndicativeAnalyticsSetup(
+    context: Context, idsRepository: IdsRepository,
+    promotionsRepository: PromotionsRepository, logger: Logger,
+    promoCodeLocalDataSource: PromoCodeLocalDataSource
+  ): IndicativeAnalytics {
+    return IndicativeAnalytics(
+      context, idsRepository, promotionsRepository, logger,
+      promoCodeLocalDataSource
+    )
+  }
 
   @Singleton
   @Provides
@@ -165,16 +179,25 @@ class AnalyticsModule {
 
   @Singleton
   @Provides
-  fun provideTopUpAnalytics(analyticsManager: AnalyticsManager,
-                            abTestingAnalytics: TopUpABTestingAnalytics) =
-      TopUpAnalytics(analyticsManager, abTestingAnalytics)
+  fun provideTopUpAnalytics(
+    analyticsManager: AnalyticsManager,
+    abTestingAnalytics: TopUpABTestingAnalytics
+  ) =
+    TopUpAnalytics(analyticsManager, abTestingAnalytics)
 
   @Provides
-  fun providePaymentMethodsAnalytics(analyticsManager: AnalyticsManager,
-                                     billingAnalytics: BillingAnalytics,
-                                     rakamAnalytics: RakamAnalytics,
-                                     indicativeAnalytics: IndicativeAnalytics): PaymentMethodsAnalytics {
-    return PaymentMethodsAnalytics(analyticsManager, billingAnalytics, rakamAnalytics, indicativeAnalytics)
+  fun providePaymentMethodsAnalytics(
+    analyticsManager: AnalyticsManager,
+    billingAnalytics: BillingAnalytics,
+    rakamAnalytics: RakamAnalytics,
+    indicativeAnalytics: IndicativeAnalytics
+  ): PaymentMethodsAnalytics {
+    return PaymentMethodsAnalytics(
+      analyticsManager,
+      billingAnalytics,
+      rakamAnalytics,
+      indicativeAnalytics
+    )
   }
 
   @Singleton
@@ -186,7 +209,7 @@ class AnalyticsModule {
   @Singleton
   @Provides
   fun providesRatingAnalytics(analyticsManager: AnalyticsManager) =
-      RatingAnalytics(analyticsManager)
+    RatingAnalytics(analyticsManager)
 
   @Singleton
   @Provides
