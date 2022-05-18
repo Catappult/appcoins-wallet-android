9a55f15d6b608c4304f1b9f1c0ff2e3299b14d61
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2022-05-18 17:53:40 +0100

    [APPC-3120] code improvements

--- app/src/main/java/com/asfoundation/wallet/util/UrlParser.kt
+++ app/src/main/java/com/asfoundation/wallet/util/UrlParser.kt
@@ -8,21 +8,21 @@ class UrlUtmParser {
   companion object {
 
     @Throws(UnsupportedEncodingException::class)
-    fun splitQuery(urlParams: String): Map<String, MutableList<String?>>? {
+    fun splitQuery(urlParams: String): Map<String, MutableList<String?>> {
       val queryPairs: MutableMap<String, MutableList<String?>> = LinkedHashMap()
       val pairs: List<String> = urlParams.split("&")
       for (pair in pairs) {
-        val idx = pair.indexOf("=")
-        val key = if (idx > 0) URLDecoder.decode(pair.substring(0, idx), "UTF-8") else pair
-        if (!queryPairs.containsKey(key)) {
-          queryPairs[key] = mutableListOf<String?>()
+        val keyVal: List<String> = pair.split("=")
+        if (keyVal.isNotEmpty()) {
+          val key = keyVal[0]
+          if (!queryPairs.containsKey(key)) {
+            queryPairs[key] = mutableListOf<String?>()
+          }
+          val value: String = if (keyVal.size > 1)
+            keyVal[1]
+          else ""
+          queryPairs[key]?.add(value)
         }
-        val value: String? = if (idx > 0 && pair.length > idx + 1)
-          URLDecoder.decode(
-            pair.substring(idx + 1),
-            "UTF-8"
-          ) else null
-        queryPairs[key]?.add(value)
       }
       return queryPairs
     }
