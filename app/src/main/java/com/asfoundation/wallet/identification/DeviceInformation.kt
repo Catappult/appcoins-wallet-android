0da8c586e9094c2021afc225adde430e68b835eb
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2022-06-06 18:12:28 +0100

    [APPC-3148] Device info

---
+++ app/src/main/java/com/asfoundation/wallet/identification/DeviceInformation.kt
@@ -0,0 +1,9 @@
+package com.asfoundation.wallet.identification
+
+class DeviceInformation (
+  val osVersion: String,
+  val brand: String,
+  val model: String,
+  val language: String,
+  val isProbablyEmulator: Boolean
+)
--- app/src/main/java/com/asfoundation/wallet/identification/IdsRepository.kt
+++ app/src/main/java/com/asfoundation/wallet/identification/IdsRepository.kt
@@ -1,18 +1,22 @@
 package com.asfoundation.wallet.identification
 
 import android.content.Context
+import android.os.Build
 import android.provider.Settings
 import com.appcoins.wallet.gamification.repository.UserStatsLocalData
 import com.asfoundation.wallet.billing.partners.InstallerService
 import com.asfoundation.wallet.repository.PreferencesRepositoryType
 import dagger.hilt.android.qualifiers.ApplicationContext
 import io.reactivex.Single
+import java.util.*
 import javax.inject.Inject
 
-class IdsRepository @Inject constructor(@ApplicationContext private val context: Context,
-                                        private val sharedPreferencesRepository: PreferencesRepositoryType,
-                                        private val userStatsLocalData: UserStatsLocalData,
-                                        private val installerService: InstallerService) {
+class IdsRepository @Inject constructor(
+  @ApplicationContext private val context: Context,
+  private val sharedPreferencesRepository: PreferencesRepositoryType,
+  private val userStatsLocalData: UserStatsLocalData,
+  private val installerService: InstallerService
+) {
 
   fun getAndroidId(): String {
     var androidId = sharedPreferencesRepository.getAndroidId()
@@ -33,5 +37,40 @@ class IdsRepository @Inject constructor(@ApplicationContext private val context:
 
   fun getInstallerPackage(packageName: String): Single<String> {
     return installerService.getInstallerPackageName(packageName)
+  }
+
+  fun getDeviceInfo(): DeviceInformation {
+    return DeviceInformation(
+      osVersion = Build.VERSION.RELEASE,
+      brand = Build.BRAND,
+      model = Build.MODEL,
+      language = Locale.getDefault().language,
+      isProbablyEmulator = isProbablyEmulator()
+    )
+  }
+
+  private fun isProbablyEmulator(): Boolean {
+    return (
+        (Build.FINGERPRINT.startsWith("google/sdk_gphone_")
+            && Build.FINGERPRINT.endsWith(":user/release-keys")
+            && Build.MANUFACTURER == "Google"
+            && Build.PRODUCT.startsWith("sdk_gphone_")
+            && Build.BRAND == "google"
+            && Build.MODEL.startsWith("sdk_gphone_")
+            )
+            || Build.FINGERPRINT.startsWith("generic")
+            || Build.FINGERPRINT.startsWith("unknown")
+            || Build.MODEL.contains("google_sdk")
+            || Build.MODEL.contains("Emulator")
+            || Build.MODEL.contains("Android SDK built for x86")
+            || "QC_Reference_Phone" == Build.BOARD && !"Xiaomi".equals(
+              Build.MANUFACTURER,
+              ignoreCase = true
+            ) //bluestacks
+            || Build.MANUFACTURER.contains("Genymotion")
+            || Build.HOST.startsWith("Build") //MSI App Player
+            || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+            || Build.PRODUCT == "google_sdk"
+        )
   }
 }
