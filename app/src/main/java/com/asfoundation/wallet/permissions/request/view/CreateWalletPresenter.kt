621c644a7213fe36473ebf7c76f6b52c6ce770b0
Sergei Mitrofanov <goretz.m@gmail.com>
2022-07-07 11:48:06 +0100

    Auto format to match our code style

--- app/src/main/java/com/asfoundation/wallet/permissions/request/view/CreateWalletPresenter.kt
+++ app/src/main/java/com/asfoundation/wallet/permissions/request/view/CreateWalletPresenter.kt
@@ -1,17 +1,16 @@
 package com.asfoundation.wallet.permissions.request.view
 
-import com.asfoundation.wallet.entity.Wallet
 import com.asfoundation.wallet.wallets.WalletCreatorInteract
 import io.reactivex.Scheduler
-import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.BiFunction
 import java.util.concurrent.TimeUnit
 
-class CreateWalletPresenter(private val view: CreateWalletView,
-                            private val disposables: CompositeDisposable,
-                            private val interactor: WalletCreatorInteract,
-                            private val viewScheduler: Scheduler) {
+class CreateWalletPresenter(
+  private val view: CreateWalletView,
+  private val disposables: CompositeDisposable,
+  private val interactor: WalletCreatorInteract,
+  private val viewScheduler: Scheduler
+) {
   fun present() {
     handleOnCreateWalletClick()
     handleOnCancelClick()
@@ -28,16 +27,13 @@ class CreateWalletPresenter(private val view: CreateWalletView,
 
   private fun handleOnCreateWalletClick() {
     disposables.add(
-        view.getOnCreateWalletClick().doOnNext { view.showLoading() }
-            .flatMapSingle {
-              Single.zip(interactor.create(), Single.timer(1, TimeUnit.SECONDS),
-                  BiFunction { wallet: Wallet, _: Long -> wallet })
-            }
-            .observeOn(viewScheduler)
-            .subscribe { view.showFinishAnimation() })
+      view.getOnCreateWalletClick()
+        .doOnNext { view.showLoading() }
+        .flatMapSingle { interactor.create() }
+        .delay(1, TimeUnit.SECONDS)
+        .observeOn(viewScheduler)
+        .subscribe { view.showFinishAnimation() })
   }
 
-  fun stop() {
-    disposables.clear()
-  }
+  fun stop() = disposables.clear()
 }
