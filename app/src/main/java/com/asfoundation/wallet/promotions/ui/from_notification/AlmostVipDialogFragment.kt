97b6aa54fd90cbb21345dc6034792a6543bf68d7
danfbfern <danielfernando.1608@gmail.com>
2022-09-20 16:03:55 +0100

    [APPC-3224] unused imports

--- app/src/main/java/com/asfoundation/wallet/promotions/ui/from_notification/AlmostVipDialogFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/promotions/ui/from_notification/AlmostVipDialogFragment.kt
@@ -2,11 +2,9 @@ package com.asfoundation.wallet.promotions.ui.from_notification
 
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.asf.wallet.R
 import com.asf.wallet.databinding.AlmostVipFragmentBinding
