8a31b928566bd0e6ce8ec102dd517facc60d0d39
Sergei Mitrofanov <goretz.m@gmail.com>
2022-09-29 17:15:20 +0100

    Add notification to show for success

--- app/src/main/java/com/asfoundation/wallet/promotions/worker/GetVipReferralWorker.kt
+++ app/src/main/java/com/asfoundation/wallet/promotions/worker/GetVipReferralWorker.kt
@@ -1,9 +1,15 @@
 package com.asfoundation.wallet.promotions.worker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.work.*
 import com.appcoins.wallet.gamification.repository.entity.VipReferralResponse.Companion.invalidReferral
+import com.asf.wallet.R
 import com.asfoundation.wallet.base.RxSchedulers
+import com.asfoundation.wallet.main.PendingIntentNavigator
 import com.asfoundation.wallet.promotions.usecases.GetVipReferralUseCase
 import com.asfoundation.wallet.wallets.usecases.GetCurrentWalletUseCase
 import io.reactivex.Single
@@ -11,7 +17,7 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class GetVipReferralWorker(
-  context: Context,
+  private val context: Context,
   params: WorkerParameters
 ) : RxWorker(context, params) {
 
@@ -22,6 +28,12 @@ class GetVipReferralWorker(
   lateinit var getCurrentWallet: GetCurrentWalletUseCase
 
   @Inject
+  lateinit var pendingIntentNavigator: PendingIntentNavigator
+
+  @Inject
+  lateinit var notificationManager: NotificationManager
+
+  @Inject
   lateinit var rxSchedulers: RxSchedulers
 
   override fun getBackgroundScheduler() = rxSchedulers.io
@@ -29,6 +41,7 @@ class GetVipReferralWorker(
   override fun createWork(): Single<Result> = getCurrentWallet()
     .flatMap(getVipReferralUseCase::invoke)
     .filter { it != invalidReferral }
+    .doOnSuccess { showNotification() }
     .map { Result.success() }
     .toSingle()
     .onErrorReturn {
@@ -39,10 +52,34 @@ class GetVipReferralWorker(
       }
     }
 
+  private fun showNotification() = notificationManager.notify(
+    NOTIFICATION_SERVICE_ID,
+    NotificationCompat.Builder(context, CHANNEL_ID)
+      .setAutoCancel(true)
+      .setContentIntent(pendingIntentNavigator.getPromotionsPendingIntent())
+      .setPriority(NotificationCompat.PRIORITY_HIGH)
+      .setSmallIcon(R.drawable.ic_appcoins_notification_icon)
+      .setContentTitle(context.getString(R.string.vip_program_referral_notification_title))
+      .setContentText(context.getString(R.string.vip_program_referral_notification_body))
+      .apply {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          notificationManager.createNotificationChannel(
+            NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+          )
+        } else {
+          setVibrate(LongArray(0))
+        }
+      }
+      .build(),
+  )
+
   companion object {
     const val NAME = "com.asfoundation.wallet.promotions.worker.GetVipReferralWorker"
     private const val RETRY_MINUTES = 5L
     private const val RETRY_COUNTS = 24
+    private const val CHANNEL_NAME = "VIP Referral Notification Channel"
+    private const val CHANNEL_ID = "notification_channel_vip_referral"
+    private const val NOTIFICATION_SERVICE_ID = 77777
 
     val workRequest by lazy {
       OneTimeWorkRequestBuilder<GetVipReferralWorker>()
