5bd5d2643e0effac85791b8e44b648beeab36d5d
danfbfern <danielfernando.1608@gmail.com>
2022-11-19 00:46:46 +0000

    [APPC-3379] fixed the dismiss button on the update card

--- app/src/main/java/com/asfoundation/wallet/service/AutoUpdateService.kt
+++ app/src/main/java/com/asfoundation/wallet/service/AutoUpdateService.kt
@@ -1,26 +1,28 @@
 package com.asfoundation.wallet.service
 
+import com.asfoundation.wallet.base.RxSchedulers
 import com.asfoundation.wallet.entity.AutoUpdateResponse
 import com.asfoundation.wallet.viewmodel.AutoUpdateModel
 import io.reactivex.Single
 import retrofit2.http.GET
 import javax.inject.Inject
 
-class AutoUpdateService @Inject constructor(private val api: AutoUpdateApi) {
+class AutoUpdateService @Inject constructor(
+  private val api: AutoUpdateApi,
+  private val rxSchedulers: RxSchedulers
+) {
 
   fun loadAutoUpdateModel(): Single<AutoUpdateModel> {
     return api.getAutoUpdateInfo()
-        .map {
-          AutoUpdateModel(it.latestVersion.versionCode, it.latestVersion.minSdk, it.blackList)
-        }
-        .onErrorReturn { AutoUpdateModel() }
+      .subscribeOn(rxSchedulers.io)
+      .map {
+        AutoUpdateModel(it.latestVersion.versionCode, it.latestVersion.minSdk, it.blackList)
+      }
+      .onErrorReturn { AutoUpdateModel() }
   }
 
   interface AutoUpdateApi {
-
     @GET("appc/wallet_version")
     fun getAutoUpdateInfo(): Single<AutoUpdateResponse>
-
   }
-
 }
