a5876c1a2f8ff199f791c3103660a328bf82d279
danfbfern <danielfernando.1608@gmail.com>
2022-11-29 02:40:33 +0000

    [APPC-3374] arranged various layouts for different layout devices

--- app/src/main/java/com/asfoundation/wallet/ui/common/WalletButtonView.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/common/WalletButtonView.kt
@@ -17,6 +17,14 @@ import com.asfoundation.wallet.util.convertDpToPx
 
 class WalletButtonView : FrameLayout {
 
+  init {
+    if(isInEditMode) {
+      imageLeft = null
+      imageRight = null
+      type = Type.FILLED
+    }
+  }
+
   private val views =
     LayoutWalletButtonViewBinding.inflate(LayoutInflater.from(context), this, true)
 
