777ce66f87b94ce7125d815ef4331b0bc8e5d1ae
danfbfern <danielfernando.1608@gmail.com>
2023-01-20 00:37:47 +0000

    [APPC-3366] Added success buttons and navigation changes

--- app/src/main/java/com/asfoundation/wallet/onboarding_new_payment/payment_result/OnboardingPaymentResultNavigator.kt
+++ app/src/main/java/com/asfoundation/wallet/onboarding_new_payment/payment_result/OnboardingPaymentResultNavigator.kt
@@ -1,14 +1,35 @@
 package com.asfoundation.wallet.onboarding_new_payment.payment_result
 
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.asfoundation.wallet.base.Navigator
+import com.asfoundation.wallet.main.PendingIntentNavigator
 import javax.inject.Inject
 
-class OnboardingPaymentResultNavigator @Inject constructor(private val fragment: Fragment) :
+class OnboardingPaymentResultNavigator @Inject constructor(
+  private val fragment: Fragment,
+  private val packageManager: PackageManager,
+  private val pendingIntentNavigator: PendingIntentNavigator
+) :
   Navigator {
 
   fun navigateBack() {
     fragment.findNavController().popBackStack()
+  }
+
+  fun navigateBackToGame(packageName: String) {
+    try {
+      fragment.startActivity(
+        packageManager.getLaunchIntentForPackage(packageName)
+      )
+    } catch (e: Throwable) {
+      e.printStackTrace()
+      fragment.activity?.finishAffinity()
+    }
+  }
+
+  fun navigateToHome(){
+    pendingIntentNavigator.getHomePendingIntent().send()
   }
 }
