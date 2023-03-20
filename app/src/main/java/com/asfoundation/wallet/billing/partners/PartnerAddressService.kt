e843ce4ff2c6aa3930ecd86278c786e7d562fdf6
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2023-03-20 18:20:23 +0000

    [APPC-3591] oemid with priority for gamesHub

--- app/src/main/java/com/asfoundation/wallet/billing/partners/PartnerAddressService.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/partners/PartnerAddressService.kt
@@ -1,19 +1,21 @@
 package com.asfoundation.wallet.billing.partners
 
 import com.asf.wallet.BuildConfig
+import com.asfoundation.wallet.util.Log
 import io.reactivex.Single
 import it.czerwinski.android.hilt.annotations.BoundTo
 import javax.inject.Inject
 
 @BoundTo(supertype = AddressService::class)
-class PartnerAddressService @Inject constructor(private val installerService: InstallerService,
-                                                private val oemIdExtractorService: OemIdExtractorService) :
-    AddressService {
+class PartnerAddressService @Inject constructor(
+  private val installerService: InstallerService,
+  private val oemIdExtractorService: OemIdExtractorService
+) :
+  AddressService {
 
   private val defaultStoreAddress: String = BuildConfig.DEFAULT_STORE_ADDRESS
   private val defaultOemAddress: String = BuildConfig.DEFAULT_OEM_ADDRESS
   private val defaultGamesHubPackage: String = BuildConfig.GAMESHUB_PACKAGE
-  private val defaultGamesOemId: String = BuildConfig.GAMESHUB_OEMID
 
   override fun getStoreAddress(suggestedStoreAddress: String?): String {
     return suggestedStoreAddress?.let { suggestedStoreAddress } ?: defaultStoreAddress
@@ -31,20 +33,16 @@ class PartnerAddressService @Inject constructor(private val installerService: In
       AttributionEntity(oemId.ifEmpty { null }, installerPackage.ifEmpty { null })
     }
       .flatMap { attributionFromGame ->
-        // If a game comes from gamesHub, then tries to send gamesHub's oemid instead. Otherwise
-        // just sends the oemid of the game.
-        if (attributionFromGame.oemId != null && attributionFromGame.oemId == defaultGamesOemId) {
-          oemIdExtractorService.extractOemId(defaultGamesHubPackage)
-            .map { gamesHubOemId ->
-              if(gamesHubOemId.isEmpty()) {
-                attributionFromGame
-              } else {
-                AttributionEntity(gamesHubOemId.ifEmpty { null }, attributionFromGame.domain)
-              }
+        // Tries to send gamesHub's oemid, if available. Otherwise sends the oemid of the game.
+        oemIdExtractorService.extractOemId(defaultGamesHubPackage)
+          .map { gamesHubOemId ->
+            if (gamesHubOemId.isEmpty()) {
+              attributionFromGame
+            } else {
+              AttributionEntity(gamesHubOemId.ifEmpty { null }, attributionFromGame.domain)
             }
-        } else {
-          Single.just(attributionFromGame)
-        }
+          }
+          .doOnSuccess { Log.d("oemid", "oemid: ${it?.oemId ?: ""}")}
       }
   }
 }
