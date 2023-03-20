d8fb5d4be2c6c5fc7e943e818ace4c38529a4eca
danfbfern <danielfernando.1608@gmail.com>
2023-03-20 14:07:34 +0000

    fixed aar duplicated implementation error

--- core/network/analytics/build.gradle.kts
+++ core/network/analytics/build.gradle.kts
@@ -7,7 +7,7 @@ android {
 }
 
 dependencies {
-  implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+  compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
   implementation(project(":core:network:base"))
   implementation(libs.bundles.network)
   implementation(libs.bundles.jackson)
