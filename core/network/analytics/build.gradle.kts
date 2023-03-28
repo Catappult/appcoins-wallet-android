f4db0133148f1b25efefe643c3b9a915c902945a
giovannievangelista <giovanni.luca@aptoide.com>
2023-03-28 12:51:32 +0100

    revert changes

--- core/network/analytics/build.gradle.kts
+++ core/network/analytics/build.gradle.kts
@@ -7,6 +7,7 @@ android {
 }
 
 dependencies {
+  compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
   implementation(project(":core:network:base"))
   implementation(libs.bundles.network)
   implementation(libs.bundles.jackson)
--- core/network/analytics/src/main/java/com/appcoins/wallet/core/network/analytics/api/AnalyticsApi.kt
+++ core/network/analytics/src/main/java/com/appcoins/wallet/core/network/analytics/api/AnalyticsApi.kt
@@ -1,5 +1,6 @@
 package com.appcoins.wallet.core.network.analytics.api
 
+import cm.aptoide.analytics.AnalyticsManager
 import com.appcoins.wallet.core.network.analytics.AnalyticsBody
 import io.reactivex.Completable
 import retrofit2.http.Body
@@ -9,7 +10,7 @@ import retrofit2.http.Path
 interface AnalyticsApi {
   @POST("user/addEvent/action={action}/context=WALLET/name={name}")
   fun registerEvent(
-    @Path("action") action: String,
+    @Path("action") action: AnalyticsManager.Action?,
     @Path("name") eventName: String?,
     @Body body: AnalyticsBody?
   ): Completable
