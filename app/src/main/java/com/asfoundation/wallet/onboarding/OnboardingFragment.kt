9671b04caa167f2459d21502997c0108ea6e184d
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2023-03-29 17:56:42 +0100

    [APPC-3617] notifications prompt for target 33

--- app/src/main/java/com/asfoundation/wallet/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/onboarding/OnboardingFragment.kt
@@ -20,10 +20,9 @@ import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.appcoins.wallet.core.utils.properties.PRIVACY_POLICY_URL
 import com.appcoins.wallet.core.utils.properties.TERMS_CONDITIONS_URL
-import com.asf.wallet.BuildConfig
+import com.appcoins.wallet.ui.arch.SingleStateFragment
 import com.asf.wallet.R
 import com.asf.wallet.databinding.FragmentOnboardingBinding
-import com.appcoins.wallet.ui.arch.SingleStateFragment
 import com.asfoundation.wallet.my_wallets.create_wallet.CreateWalletDialogFragment
 import com.asfoundation.wallet.viewmodel.BasePageViewFragment
 import dagger.hilt.android.AndroidEntryPoint
