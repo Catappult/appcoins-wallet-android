a77c8ecca1a4f9b94cc5ebfba64d7b354e49a8f1
Ricardo Frango <ricardo.frango@aptoide.com>
2023-04-03 14:41:39 +0100

    migrate RatingFinishFragment

--- app/src/main/java/com/asfoundation/wallet/rating/finish/RatingFinishFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/rating/finish/RatingFinishFragment.kt
@@ -5,13 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.asf.wallet.R
+import com.asf.wallet.databinding.FragmentRatingFinishBinding
 import com.asfoundation.wallet.rating.RatingActivity
 import com.asfoundation.wallet.viewmodel.BasePageViewFragment
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.fragment_rating_finish.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -22,6 +21,13 @@ class RatingFinishFragment : BasePageViewFragment(), RatingFinishView {
 
   private val animationEndSubject = PublishSubject.create<Any>()
 
+  private var _binding: FragmentRatingFinishBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
+
+  private val animation get() = binding.animation
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     (requireActivity() as RatingActivity).disableBack()
@@ -29,7 +35,8 @@ class RatingFinishFragment : BasePageViewFragment(), RatingFinishView {
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_rating_finish, container, false)
+    _binding = FragmentRatingFinishBinding.inflate(inflater, container, false)
+    return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
