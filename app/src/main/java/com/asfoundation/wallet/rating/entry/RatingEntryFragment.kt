4a4187226d5f6ac821aadd5fbdb51cc3535dfde7
Ricardo Frango <ricardo.frango@aptoide.com>
2023-04-03 14:40:17 +0100

    migrate RatingEntryFragment

--- app/src/main/java/com/asfoundation/wallet/rating/entry/RatingEntryFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/rating/entry/RatingEntryFragment.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.asf.wallet.R
+import com.asf.wallet.databinding.FragmentRatingEntryBinding
 import com.asfoundation.wallet.viewmodel.BasePageViewFragment
 import com.jakewharton.rxbinding2.view.RxView
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.Observable
-import kotlinx.android.synthetic.main.fragment_rating_entry.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,9 +17,19 @@ class RatingEntryFragment : BasePageViewFragment(), RatingEntryView {
   @Inject
   lateinit var presenter: RatingEntryPresenter
 
+  private var _binding: FragmentRatingEntryBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
+
+  private val animation get() = binding.animation
+  private val yes_button get() = binding.yesButton
+  private val no_button get() = binding.noButton
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_rating_entry, container, false)
+    _binding = FragmentRatingEntryBinding.inflate(inflater, container, false)
+    return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
