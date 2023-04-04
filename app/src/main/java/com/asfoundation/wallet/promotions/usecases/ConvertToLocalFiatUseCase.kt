894e045befe20e0780a417f30494a338e9fffc28
danfbfern <danielfernando.1608@gmail.com>
2023-04-04 11:40:34 +0100

    [APPC-3634] refactor use cases

--- app/src/main/java/com/asfoundation/wallet/promotions/usecases/ConvertToLocalFiatUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/promotions/usecases/ConvertToLocalFiatUseCase.kt
@@ -1,28 +1,28 @@
 package com.asfoundation.wallet.promotions.usecases
 
-import com.appcoins.wallet.core.utils.android_common.RxSchedulers
+import com.appcoins.wallet.core.utils.android_common.Dispatchers
 import com.asfoundation.wallet.change_currency.use_cases.GetSelectedCurrencyUseCase
 import com.asfoundation.wallet.service.currencies.LocalCurrencyConversionService
 import com.asfoundation.wallet.ui.iab.FiatValue
 import com.asfoundation.wallet.wallets.repository.BalanceRepository
+import com.github.michaelbull.result.get
 import io.reactivex.Single
+import kotlinx.coroutines.rx2.rxSingle
 import javax.inject.Inject
 
 class ConvertToLocalFiatUseCase @Inject constructor(
   private val getSelectedCurrencyUseCase: GetSelectedCurrencyUseCase,
   private val localCurrencyConversionService: LocalCurrencyConversionService,
-  private val rxSchedulers: RxSchedulers
+  private val dispatchers: Dispatchers
 ) {
 
   operator fun invoke(valueToConvert: String, originalCurrency: String): Single<FiatValue> {
-    return getSelectedCurrencyUseCase(bypass = false)
-      .subscribeOn(rxSchedulers.io)
+    return rxSingle(dispatchers.io) { getSelectedCurrencyUseCase(bypass = false) }
       .flatMap { targetCurrency ->
         localCurrencyConversionService.getValueToFiat(
           valueToConvert, originalCurrency,
-          targetCurrency, BalanceRepository.FIAT_SCALE
+          targetCurrency.get(), BalanceRepository.FIAT_SCALE
         )
-          .subscribeOn(rxSchedulers.io)
       }
   }
 }
