078c70d604c25948eaacad4a74c3c20aa80c44e6
danfbfern <danielfernando.1608@gmail.com>
2023-04-04 11:40:34 +0100

    [APPC-3634] refactor use cases

--- app/src/main/java/com/asfoundation/wallet/change_currency/FiatCurrenciesMapper.kt
+++ app/src/main/java/com/asfoundation/wallet/change_currency/FiatCurrenciesMapper.kt
@@ -1,6 +1,6 @@
 package com.asfoundation.wallet.change_currency
 
-import com.appcoins.wallet.core.network.microservices.model.FiatCurrenciesResponse
+import com.appcoins.wallet.core.network.microservices.model.response.FiatCurrenciesResponse
 import javax.inject.Inject
 
 class FiatCurrenciesMapper @Inject constructor(){
--- app/src/main/java/com/asfoundation/wallet/change_currency/use_cases/GetChangeFiatCurrencyModelUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/change_currency/use_cases/GetChangeFiatCurrencyModelUseCase.kt
@@ -1,27 +1,28 @@
 package com.asfoundation.wallet.change_currency.use_cases
 
+import com.appcoins.wallet.ui.arch.data.DataResult
+import com.appcoins.wallet.ui.arch.data.toDataResult
 import com.asfoundation.wallet.change_currency.ChangeFiatCurrency
 import com.asfoundation.wallet.change_currency.FiatCurrenciesRepository
 import com.asfoundation.wallet.service.currencies.LocalCurrencyConversionService
-import io.reactivex.Single
+import com.github.michaelbull.result.get
+import kotlinx.coroutines.rx2.await
 import javax.inject.Inject
 
 class GetChangeFiatCurrencyModelUseCase @Inject constructor(
-    private val fiatCurrenciesRepository: FiatCurrenciesRepository,
-    private val conversionService: LocalCurrencyConversionService) {
+  private val fiatCurrenciesRepository: FiatCurrenciesRepository,
+  private val conversionService: LocalCurrencyConversionService
+) {
 
-  operator fun invoke(): Single<ChangeFiatCurrency> {
-    return Single.zip(fiatCurrenciesRepository.getCurrenciesList(),
-        fiatCurrenciesRepository.getSelectedCurrency(),
-        { list, selectedCurrency -> ChangeFiatCurrency(list, selectedCurrency) })
-        .flatMap { changeFiatCurrencyModel ->
-          if (changeFiatCurrencyModel.selectedCurrency.isEmpty()) {
-            return@flatMap conversionService.localCurrency
-                .map { localCurrency ->
-                  changeFiatCurrencyModel.copy(selectedCurrency = localCurrency.currency)
-                }
-          }
-          return@flatMap Single.just(changeFiatCurrencyModel)
-        }
+  suspend operator fun invoke(): DataResult<ChangeFiatCurrency> {
+    val selectedCurrency = fiatCurrenciesRepository.getSelectedCurrency().get()
+    val fiatCurrencyList = fiatCurrenciesRepository.getCurrenciesList().get()
+    val changeFiatCurrencyModel = ChangeFiatCurrency(fiatCurrencyList!!, selectedCurrency!!)
+    return if (changeFiatCurrencyModel.selectedCurrency.isEmpty()) {
+      val localCurrency = conversionService.localCurrency.await()
+      changeFiatCurrencyModel.copy(selectedCurrency = localCurrency.currency).toDataResult()
+    } else {
+      changeFiatCurrencyModel.toDataResult()
+    }
   }
 }
