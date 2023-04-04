5904aa104b899440632dc12ae83af1ea280ad86d
danfbfern <danielfernando.1608@gmail.com>
2023-04-04 11:40:34 +0100

    [APPC-3634] refactor use cases

--- app/src/main/java/com/asfoundation/wallet/change_currency/use_cases/GetSelectedCurrencyUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/change_currency/use_cases/GetSelectedCurrencyUseCase.kt
@@ -1,13 +1,15 @@
 package com.asfoundation.wallet.change_currency.use_cases
 
+import com.appcoins.wallet.ui.arch.data.DataResult
 import com.asfoundation.wallet.change_currency.FiatCurrenciesRepository
-import io.reactivex.Single
 import javax.inject.Inject
 
 class GetSelectedCurrencyUseCase @Inject constructor(
-    private val fiatCurrenciesRepository: FiatCurrenciesRepository) {
+  private val fiatCurrenciesRepository: FiatCurrenciesRepository
+) {
 
-  operator fun invoke(bypass: Boolean): Single<String> {
-    return if (bypass) fiatCurrenciesRepository.getCachedSelectedCurrency() else fiatCurrenciesRepository.getSelectedCurrency()
+  suspend operator fun invoke(bypass: Boolean): DataResult<String> {
+    return if (bypass) fiatCurrenciesRepository.getCachedSelectedCurrency()
+    else fiatCurrenciesRepository.getSelectedCurrency()
   }
 }
--- app/src/main/java/com/asfoundation/wallet/nfts/usecases/EstimateNFTSendGasUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/nfts/usecases/EstimateNFTSendGasUseCase.kt
@@ -1,27 +1,33 @@
 package com.asfoundation.wallet.nfts.usecases
 
+import com.appcoins.wallet.core.utils.android_common.Dispatchers
 import com.asfoundation.wallet.change_currency.use_cases.GetSelectedCurrencyUseCase
 import com.asfoundation.wallet.nfts.domain.GasInfo
 import com.asfoundation.wallet.nfts.domain.NFTItem
 import com.asfoundation.wallet.nfts.repository.NFTRepository
 import com.asfoundation.wallet.wallets.usecases.GetCurrentWalletUseCase
+import com.github.michaelbull.result.get
 import io.reactivex.Single
+import kotlinx.coroutines.rx2.rxSingle
 import javax.inject.Inject
 
 class EstimateNFTSendGasUseCase @Inject constructor(
   private val getCurrentWallet: GetCurrentWalletUseCase,
   private val getSelectedCurrencyUseCase: GetSelectedCurrencyUseCase,
-  private val NFTRepository: NFTRepository
+  private val NFTRepository: NFTRepository,
+  private val dispatchers: Dispatchers
 ) {
 
   operator fun invoke(item: NFTItem, toAddress: String): Single<GasInfo> {
-    return getCurrentWallet().flatMap { wallet ->
-      getSelectedCurrencyUseCase(bypass = false).flatMap { selected ->
-        return@flatMap NFTRepository.estimateSendNFTGas(
-          wallet.address, toAddress, item.tokenId,
-          item.contractAddress, item.schema, selected
-        )
+    return getCurrentWallet()
+      .flatMap { wallet ->
+        rxSingle(dispatchers.io) { getSelectedCurrencyUseCase(bypass = false) }
+          .flatMap { selected ->
+            return@flatMap NFTRepository.estimateSendNFTGas(
+              wallet.address, toAddress, item.tokenId,
+              item.contractAddress, item.schema, selected.get()!!
+            )
+          }
       }
-    }
   }
 }
