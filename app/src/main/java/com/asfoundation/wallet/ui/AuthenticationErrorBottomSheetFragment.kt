13f0620f543963bc4a3c577e97ad5912b4337fbb
Ricardo Frango <ricardo.frango@aptoide.com>
2023-04-04 10:55:17 +0100

    migrated AuthenticationErrorBottomSheetFragment

--- app/src/main/java/com/asfoundation/wallet/ui/AuthenticationErrorBottomSheetFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/AuthenticationErrorBottomSheetFragment.kt
@@ -6,22 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.asf.wallet.R
+import com.asf.wallet.databinding.AuthenticationErrorBottomsheetBinding
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import kotlinx.android.synthetic.main.authentication_error_bottomsheet.*
 
 class AuthenticationErrorBottomSheetFragment : Fragment(), AuthenticationErrorBottomSheetView {
 
   private lateinit var presenter: AuthenticationErrorBottomSheetPresenter
 
   private val errorTimer: Long by lazy {
-    if (arguments!!.containsKey(ERROR_TIMER_KEY)) {
-      arguments!!.getLong(ERROR_TIMER_KEY, 0)
+    if (requireArguments().containsKey(ERROR_TIMER_KEY)) {
+      requireArguments().getLong(ERROR_TIMER_KEY, 0)
     } else {
       throw IllegalArgumentException("Error message not found")
     }
   }
+
+  private var _binding: AuthenticationErrorBottomsheetBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
+
+  private val retry_authentication get() = binding.retryAuthentication
+  private val authentication_error_message get() = binding.authenticationErrorMessage
 
   companion object {
     private const val ERROR_TIMER_KEY = "error_message"
@@ -44,7 +52,8 @@ class AuthenticationErrorBottomSheetFragment : Fragment(), AuthenticationErrorBo
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.authentication_error_bottomsheet, container, false)
+    _binding = AuthenticationErrorBottomsheetBinding.inflate(inflater, container, false)
+    return binding.root
   }
 
   override fun getButtonClick() = RxView.clicks(retry_authentication)
