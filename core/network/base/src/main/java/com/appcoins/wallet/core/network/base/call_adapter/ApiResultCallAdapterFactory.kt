8ae5e06df1637c728c69596f088d40aee88fc8b0
danfbfern <danielfernando.1608@gmail.com>
2023-04-04 11:36:39 +0100

    [APPC-3634] implemented the call adapter to the base module

---
+++ core/network/base/src/main/java/com/appcoins/wallet/core/network/base/call_adapter/ApiResultCallAdapterFactory.kt
@@ -0,0 +1,42 @@
+package com.appcoins.wallet.core.network.base.call_adapter
+
+import com.github.michaelbull.result.Result
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Retrofit
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import com.appcoins.wallet.ui.arch.data.Error
+
+class ApiResultCallAdapterFactory : CallAdapter.Factory() {
+
+  override fun get(
+    returnType: Type, annotations: Array<out Annotation>,
+    retrofit: Retrofit
+  ): CallAdapter<*, *>? {
+    if (getRawType(returnType) != Call::class.java) return null
+    check(
+      returnType is ParameterizedType
+    ) { "$returnType must be parameterized. Raw types are not supported" }
+
+    val resultType = getParameterUpperBound(0, returnType)
+    if (getRawType(resultType) != Result::class.java) return null
+    check(
+      resultType is ParameterizedType
+    ) { "$returnType must be parameterized. Raw types are not supported" }
+
+    val successBodyType = getParameterUpperBound(0, resultType)
+    val errorBodyType = getParameterUpperBound(1, resultType)
+
+    if (getRawType(errorBodyType) != Error.ApiError::class.java) return null
+    check(
+      errorBodyType is ParameterizedType
+    ) { "ApiFail must be parameterized. Raw types are not supported" }
+
+    val errorInnerBodyType = getParameterUpperBound(0, errorBodyType)
+
+    val errorBodyConverter =
+      retrofit.nextResponseBodyConverter<Any>(null, errorInnerBodyType, annotations)
+    return ApiResultCallAdapter<Any, Any>(successBodyType, errorBodyConverter)
+  }
+}
