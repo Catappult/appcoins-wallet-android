a14cf39d2e41b2b37b6cc6e6184c34f445e72dfe
Ricardo Frango <ricardo.frango@aptoide.com>
2023-04-05 10:40:44 +0100

    migrated SubscriptionViewHolder

--- app/src/main/java/com/asfoundation/wallet/subscriptions/SubscriptionViewHolder.kt
+++ app/src/main/java/com/asfoundation/wallet/subscriptions/SubscriptionViewHolder.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.asf.wallet.R
 import com.asfoundation.wallet.GlideApp
 import com.appcoins.wallet.core.utils.android_common.CurrencyFormatUtils
+import com.asf.wallet.databinding.SubscriptionItemBinding
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
@@ -13,12 +14,21 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.subscription_item.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class SubscriptionViewHolder(itemView: View, private val currencyFormatUtils: CurrencyFormatUtils) :
     RecyclerView.ViewHolder(itemView) {
+
+    private val binding by lazy { SubscriptionItemBinding.bind(itemView) }
+
+    private val app_name get() = binding.appName
+    private val app_icon get() = binding.appIcon
+    private val more_button get() = binding.moreButton
+    private val item_parent get() = binding.itemParent
+    private val app_icon_skeleton get() = binding.appIconSkeleton.root
+    private val expires_on get() = binding.expiresOn
+    private val recurrence_value get() = binding.recurrenceValue
 
   fun bind(item: SubscriptionItem, clickCallback: PublishSubject<Pair<SubscriptionItem, View>>?,
            position: Int) {
@@ -39,30 +49,30 @@ class SubscriptionViewHolder(itemView: View, private val currencyFormatUtils: Cu
         .asBitmap()
         .load(item.appIcon)
         .apply { RequestOptions().dontTransform() }
-        .listener(SkeletonGlideRequestListener(itemView.app_icon_skeleton))
+        .listener(SkeletonGlideRequestListener(app_icon_skeleton))
         .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-        .into(itemView.app_icon)
+        .into(app_icon)
   }
 
   private fun showPriceInfo(view: View, item: SubscriptionItem) {
     val formattedAmount = currencyFormatUtils.formatCurrency(item.fiatAmount)
-    view.expires_on.visibility = View.GONE
-    view.recurrence_value.visibility = View.VISIBLE
+    expires_on.visibility = View.GONE
+    recurrence_value.visibility = View.VISIBLE
 
     item.period?.let {
-      view.recurrence_value.text = it.mapToSubsFrequency(view.context,
+      recurrence_value.text = it.mapToSubsFrequency(view.context,
           view.context.getString(R.string.value_fiat, formattedAmount, item.fiatSymbol))
     }
   }
 
   private fun showToExpireInfo(view: View, item: SubscriptionItem) {
-    view.recurrence_value.visibility = View.GONE
-    view.expires_on.visibility = View.VISIBLE
+    recurrence_value.visibility = View.GONE
+    expires_on.visibility = View.VISIBLE
 
     val dateFormat = SimpleDateFormat("MMM dd", Locale.getDefault())
 
     item.expiry?.let {
-      view.expires_on.text = view.context.getString(R.string.subscriptions_expiration_body,
+      expires_on.text = view.context.getString(R.string.subscriptions_expiration_body,
           dateFormat.format(it))
     }
   }
