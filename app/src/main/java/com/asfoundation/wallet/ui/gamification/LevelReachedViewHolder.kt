389a6428c263e839f083071e1dee73615513c024
Ricardo Frango <ricardo.frango@aptoide.com>
2023-04-05 11:07:55 +0100

    migrated LevelReachedViewHolder

--- app/src/main/java/com/asfoundation/wallet/ui/gamification/LevelReachedViewHolder.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/gamification/LevelReachedViewHolder.kt
@@ -1,15 +1,21 @@
 package com.asfoundation.wallet.ui.gamification
 
 import android.view.View
-import kotlinx.android.synthetic.main.reached_level_layout.view.*
+import com.asf.wallet.databinding.ReachedLevelLayoutBinding
 
 class LevelReachedViewHolder(itemView: View, private val mapper: GamificationMapper) :
     LevelsViewHolder(itemView) {
 
+  private val binding by lazy { ReachedLevelLayoutBinding.bind(itemView) }
+
+  private val level_icon get() = binding.levelIcon
+  private val level_title get() = binding.levelTitle
+  private val level_description get() = binding.levelDescription
+
   override fun bind(level: LevelItem) {
     val reachedLevelInfo = mapper.mapReachedLevelInfo(level.level)
-    itemView.level_icon.setImageDrawable(reachedLevelInfo.planet)
-    itemView.level_title.text = reachedLevelInfo.reachedTitle
-    itemView.level_description.text = reachedLevelInfo.reachedSubtitle
+    level_icon.setImageDrawable(reachedLevelInfo.planet)
+    level_title.text = reachedLevelInfo.reachedTitle
+    level_description.text = reachedLevelInfo.reachedSubtitle
   }
 }
