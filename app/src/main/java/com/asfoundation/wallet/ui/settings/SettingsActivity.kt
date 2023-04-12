1e30c805eb1e83e411cedf5859e2a018cd607944
giovannievangelista <giovanni.luca@aptoide.com>
2023-04-12 18:23:43 +0100

    add bottom toolbar

--- app/src/main/java/com/asfoundation/wallet/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/settings/SettingsActivity.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.FrameLayout
 import com.asf.wallet.R
+import com.asfoundation.wallet.home.usecases.DisplayChatUseCase
 import com.asfoundation.wallet.ui.AuthenticationPromptActivity
 import com.asfoundation.wallet.ui.BaseActivity
 import com.asfoundation.wallet.ui.settings.entry.SettingsFragment
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SettingsActivity : BaseActivity(), SettingsActivityView {
+
+  @Inject
+  lateinit var displayChat: DisplayChatUseCase
 
   private var authenticationResultSubject: PublishSubject<Boolean>? = null
 
@@ -38,9 +44,18 @@ class SettingsActivity : BaseActivity(), SettingsActivityView {
     authenticationResultSubject = PublishSubject.create()
     if (savedInstanceState == null) {
       supportFragmentManager.beginTransaction()
-          .replace(R.id.fragment_container,
-              SettingsFragment.newInstance(intent.getBooleanExtra(TURN_ON_FINGERPRINT, false)))
-          .commit()
+        .replace(
+          R.id.fragment_container,
+          SettingsFragment.newInstance(intent.getBooleanExtra(TURN_ON_FINGERPRINT, false))
+        )
+        .commit()
+    }
+    setToolbarListener()
+  }
+
+  private fun setToolbarListener() {
+    findViewById<FrameLayout>(R.id.action_button_support).setOnClickListener {
+      displayChat()
     }
   }
 
