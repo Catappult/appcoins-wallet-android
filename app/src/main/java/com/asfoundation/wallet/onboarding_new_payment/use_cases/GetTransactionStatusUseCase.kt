298e1755ff3e16ba0fdb8ce0e229d9e45cdf5e8c
gustavo Andreazzi Prado <gustavo.andreazzi@aptoide.com>
2023-05-08 14:18:52 +0100

    Fix problem with get Transactions Status and start job to transaction status in error case Too, and create validation to not create two instance to same job

--- app/src/main/java/com/asfoundation/wallet/onboarding_new_payment/use_cases/GetTransactionStatusUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/onboarding_new_payment/use_cases/GetTransactionStatusUseCase.kt
@@ -3,16 +3,17 @@ package com.asfoundation.wallet.onboarding_new_payment.use_cases
 import com.appcoins.wallet.bdsbilling.WalletService
 import com.appcoins.wallet.bdsbilling.repository.RemoteRepository
 import com.appcoins.wallet.core.network.microservices.model.Transaction
+import com.appcoins.wallet.core.utils.android_common.RxSchedulers
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class GetTransactionStatusUseCase @Inject constructor(
     private val walletService: WalletService,
-    private val remoteRepository: RemoteRepository
+    private val remoteRepository: RemoteRepository,
+    private val rxSchedulers: RxSchedulers
 ) {
     operator fun invoke(uid: String): Single<Transaction> {
-        return walletService.getAndSignCurrentWalletAddress().subscribeOn(Schedulers.io())
+        return walletService.getAndSignCurrentWalletAddress().subscribeOn(rxSchedulers.io)
             .flatMap {
                 remoteRepository.getAppcoinsTransaction(
                     uid,
