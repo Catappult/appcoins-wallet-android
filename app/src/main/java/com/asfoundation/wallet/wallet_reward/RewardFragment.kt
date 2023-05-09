c652ed6f6bed1d528c1750fd13743fe198b13151
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2023-05-09 11:56:34 +0100

    [APPC-3623] Rewards actions carrousel

--- app/src/main/java/com/asfoundation/wallet/wallet_reward/RewardFragment.kt
+++ app/src/main/java/com/asfoundation/wallet/wallet_reward/RewardFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,12 +18,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.appcoins.wallet.ui.arch.SingleStateFragment
 import com.appcoins.wallet.ui.common.theme.WalletColors
+import com.appcoins.wallet.ui.widgets.ActionCard
+import com.appcoins.wallet.ui.widgets.RewardsActions
 import com.appcoins.wallet.ui.widgets.TopBar
 import com.asfoundation.wallet.viewmodel.BasePageViewFragment
 
@@ -92,7 +96,11 @@ class RewardFragment : BasePageViewFragment(), SingleStateFragment<RewardState, 
                 .padding(padding),
         ) {
             DummyCard()
-            DummyCard()
+            RewardsActions(
+                { },
+                { },
+                { }
+            )
             DummyCard()
         }
     }
