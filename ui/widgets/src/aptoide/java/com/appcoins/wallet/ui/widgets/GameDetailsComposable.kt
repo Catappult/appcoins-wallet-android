b436a6c2a2634b8c45f4280f8b82cb8b185e70cc
Tiago Pinto <tiagopinto.dev@gmail.com>
2023-07-19 16:52:07 +0100

    GameDetailsComposable.kt -> Game Details Model

---
+++ ui/widgets/src/aptoide/java/com/appcoins/wallet/ui/widgets/GameDetailsComposable.kt
@@ -0,0 +1,10 @@
+package com.appcoins.wallet.ui.widgets
+
+data class GameDetailsData(
+    val title: String,
+    val gameIcon: String,
+    val gameBackground: String,
+    val gamePackage: String,
+    val description: String,
+    val screenshots: List<String>?
+)
