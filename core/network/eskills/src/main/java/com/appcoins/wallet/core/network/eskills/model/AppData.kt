93410406a2db9ac9cc68ea043bb454b55fb23f89
Tiago Pinto <tiagopinto.dev@gmail.com>
2023-07-19 16:48:31 +0100

    App Data model updated.

--- core/network/eskills/src/main/java/com/appcoins/wallet/core/network/eskills/model/AppData.kt
+++ core/network/eskills/src/main/java/com/appcoins/wallet/core/network/eskills/model/AppData.kt
@@ -7,5 +7,38 @@ data class AppData(
   var uname: String,
 
   @JsonProperty("name")
-  var name: String
+  var name: String,
+
+  @JsonProperty("package")
+  val packageName: String,
+
+  @JsonProperty("icon")
+  val appIcon: String,
+
+  @JsonProperty("graphic")
+  val background: String,
+
+  @JsonProperty("media")
+  val media: Media
+
+)
+
+data class Media(
+  @JsonProperty("description")
+  val description : String,
+
+  @JsonProperty("screenshots")
+  val screenshots : List<Screenshot>
+
+)
+
+data class Screenshot(
+  @JsonProperty("url")
+  val imageUrl : String,
+
+  @JsonProperty("height")
+  val height: Int,
+
+  @JsonProperty("width")
+  val width: Int
 )
