acbd3cacc6ac8c8a5cf692c45fe567513a16b1d6
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2023-08-11 15:41:41 +0100

    GamesHub Broadcast rename txid parameter to uid

--- app/src/main/java/com/asfoundation/wallet/billing/gameshub/GamesHubBroadcastService.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/gameshub/GamesHubBroadcastService.kt
@@ -9,6 +9,7 @@ class GamesHubBroadcastService {
 
   companion object {
     const val KEY_TXID = "txid"
+    const val KEY_UID = "uid"
     const val KEY_PACKAGE_NAME = "package_name"
     const val KEY_USD_AMOUNT = "usd_amount"
     const val KEY_APPC_AMOUNT = "appc_amount"
@@ -17,20 +18,20 @@ class GamesHubBroadcastService {
 
     fun sendSuccessPaymentBroadcast(
       context: Context,
-      txId: String,
+      uid: String,
       packageName: String,
       usdAmount: String,
       appcAmount: String
     ) {
       Log.d(
         GAMES_HUB_BROADCAST,
-        "$KEY_TXID = $txId, $KEY_PACKAGE_NAME = $packageName, $KEY_USD_AMOUNT = $usdAmount"
+        "$KEY_UID = $uid, $KEY_PACKAGE_NAME = $packageName, $KEY_USD_AMOUNT = $usdAmount"
       )
       val intent = Intent(
         "${MiscProperties.GAMESHUB_PACKAGE}.${MiscProperties.GAMESHUB_BROADCAST_IAP_ACTION}"
       )
       intent.setPackage(MiscProperties.GAMESHUB_PACKAGE)
-      intent.putExtra(KEY_TXID, txId)
+      intent.putExtra(KEY_UID, uid)
       intent.putExtra(KEY_PACKAGE_NAME, packageName)
       intent.putExtra(KEY_USD_AMOUNT, usdAmount)
 //      intent.putExtra(KEY_APPC_AMOUNT, appcAmount)
