cb27a488b300baad90090985f6b1e3262e8bae80
AngeloMendonca <angelo.mendonca@aptoide.com>
2023-09-25 14:20:51 +0100

    [APPC-3907] Add all Challenge reward dependencies

--- feature/challenge-reward/data/build.gradle.kts
+++ feature/challenge-reward/data/build.gradle.kts
@@ -7,6 +7,12 @@ android {
 }
 
 dependencies {
+  implementation(project(":legacy:billing"))
+  implementation(project(":core:network:microservices"))
+
   // FYBER
   implementation("com.fyber:fairbid-sdk:3.42.0")
+
+  // RX JAVA
+  implementation("io.reactivex.rxjava2:rxjava:2.2.19")
 }
