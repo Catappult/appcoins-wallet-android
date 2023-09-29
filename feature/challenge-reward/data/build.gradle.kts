79ec40032ea7c8af40fd48e939722d174bd5fefb
AngeloMendonca <angelo.mendonca@aptoide.com>
2023-09-29 11:04:50 +0100

    [APPC-3882] Add compose and core analytics challenge-reward dependencies

--- feature/challenge-reward/data/build.gradle.kts
+++ feature/challenge-reward/data/build.gradle.kts
@@ -4,10 +4,21 @@ plugins {
 
 android {
   namespace = "com.appcoins.wallet.feature.challengereward.data"
+
+  @Suppress("UnstableApiUsage")
+  buildFeatures {
+    compose = true
+  }
+
+  @Suppress("UnstableApiUsage")
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.4.4"
+  }
 }
 
 dependencies {
   implementation(project(":legacy:billing"))
+  implementation(project(":core:analytics"))
   implementation(project(":core:network:microservices"))
   implementation(libs.fyber.sdk)
   implementation(libs.rx.rxjava)
