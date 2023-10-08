8c15bf61827ab6e0e63a9e619a3a1f47f298eab0
AngeloMendonca <angelo.mendonca@aptoide.com>
2023-10-08 21:50:46 +0100

    [APPC-3914] Rename check challenge-reward view model

--- feature/challenge-reward/data/src/main/java/com/appcoins/wallet/feature/challengereward/data/presentation/CheckChallengeRewardViewModel.kt
+++ feature/challenge-reward/data/src/main/java/com/appcoins/wallet/feature/challengereward/data/presentation/CheckChallengeRewardViewModel.kt
@@ -14,7 +14,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CheckChallengeRewardPaymentMethodViewModel @Inject constructor(
+class CheckChallengeRewardViewModel @Inject constructor(
   private val bdsRepository: BdsRepository,
 ) : ViewModel() {
   private val viewModelState = MutableStateFlow(false)
