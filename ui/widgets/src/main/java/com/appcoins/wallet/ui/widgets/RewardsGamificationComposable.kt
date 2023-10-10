e4cc52d57c5536e613cca489cf87c3c19f4b9954
giovannievangelista <giovanni.luca@aptoide.com>
2023-10-10 14:44:05 +0100

    fix design issues on RewardsGamificationComposable.kt

--- ui/widgets/src/main/java/com/appcoins/wallet/ui/widgets/RewardsGamificationComposable.kt
+++ ui/widgets/src/main/java/com/appcoins/wallet/ui/widgets/RewardsGamificationComposable.kt
@@ -11,10 +11,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -30,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.painterResource
@@ -67,16 +68,19 @@ fun GamificationHeader(
     Modifier
       .padding(start = 16.dp, end = 16.dp, top = 16.dp)
       .fillMaxWidth()
-      .height(170.dp)
       .clickable { onClick() },
     shape = RoundedCornerShape(8.dp),
     colors = CardDefaults.cardColors(WalletColors.styleguide_blue_secondary),
   ) {
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
+    Column(
+      modifier = Modifier
+        .padding(start = 16.dp, top = 16.dp, bottom = 8.dp),
+      verticalArrangement = Arrangement.SpaceBetween
+    ) {
       Row( // Top main content
         modifier = Modifier
           .fillMaxWidth()
-          .height(122.dp),
+          .heightIn(min = 80.dp),
         horizontalArrangement = Arrangement.SpaceBetween
       ) {
         if (isMaxVip) {
@@ -84,10 +88,8 @@ fun GamificationHeader(
             // vip text
             modifier =
             Modifier
-              .fillMaxHeight()
               .weight(2F)
-              .padding(start = 16.dp, top = 22.dp),
-            verticalArrangement = Arrangement.Top,
+              .align(Alignment.CenterVertically),
           ) {
             Text(
               text =
@@ -96,8 +98,8 @@ fun GamificationHeader(
                 valueSpendForNextLevel,
                 currencySpend
               ),
-              style = MaterialTheme.typography.titleMedium,
-              color = WalletColors.styleguide_white,
+              style = MaterialTheme.typography.titleSmall,
+              color = WalletColors.styleguide_light_grey,
             )
           }
         } else {
@@ -105,24 +107,26 @@ fun GamificationHeader(
             // spend text, progressBar, progress
             modifier =
             Modifier
-              .fillMaxHeight()
               .weight(2F)
-              .padding(start = 16.dp, top = 16.dp),
+              .padding(end = 16.dp),
             verticalArrangement = Arrangement.SpaceEvenly,
           ) {
             Text(
+              modifier = Modifier.padding(end = 8.dp, top = 8.dp, bottom = 8.dp),
               text =
               stringResource(
                 id = R.string.rewards_spend_to_next_level_body,
                 valueSpendForNextLevel,
                 currencySpend
               ),
-              style = MaterialTheme.typography.titleMedium,
-              color = WalletColors.styleguide_white,
+              style = MaterialTheme.typography.titleSmall,
+              color = WalletColors.styleguide_light_grey,
             )
             Column(
               // progressBar, progress
-              modifier = Modifier.padding(top = 8.dp),
+              modifier = Modifier
+                .padding(vertical = 8.dp)
+                .fillMaxWidth(),
               verticalArrangement = Arrangement.SpaceEvenly,
             ) {
               if (currentProgress >= 0 && maxProgress > 0) {
@@ -132,24 +136,29 @@ fun GamificationHeader(
                   Modifier
                     .background(Color.Transparent)
                     .clip(CircleShape)
-                    .height(8.dp),
+                    .height(8.dp)
+                    .fillMaxWidth(),
                   color = if (isVip) styleguide_vip_yellow else indicatorColor,
                   trackColor = styleguide_grey_blue,
                 )
 
                 Text(
                   text = "$currentProgress / $maxProgress",
-                  style = MaterialTheme.typography.bodyMedium,
+                  style = MaterialTheme.typography.bodySmall,
                   color = WalletColors.styleguide_dark_grey,
-                  modifier = Modifier.align(alignment = Alignment.End)
+                  modifier = Modifier
+                    .padding(end = 8.dp, top = 8.dp)
+                    .align(alignment = Alignment.End),
+                  fontWeight = FontWeight.Bold
                 )
               }
             }
           }
         }
-        Box(modifier = Modifier
-          .weight(1F)
-          .fillMaxHeight()) {
+        Box(
+          modifier = Modifier
+            .weight(1F)
+        ) {
           planetDrawable?.let {
             val bitmap =
               Bitmap.createBitmap(
@@ -164,23 +173,26 @@ fun GamificationHeader(
               bitmap = bitmap.asImageBitmap(),
               contentDescription = "Planet",
               modifier = Modifier
-                .size(82.dp)
+                .size(96.dp)
                 .align(Alignment.Center)
+                .scale(1.1F)
             )
           }
         }
       }
-
-      Row(
-        // Bottom main content
-        modifier =
-        Modifier
-          .fillMaxWidth()
-          .height(48.dp)
-          .background(styleguide_grey_blue_background)
-          .padding(horizontal = 16.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-      ) {
+    }
+    Row(
+      // Bottom main content
+      modifier =
+      Modifier
+        .fillMaxWidth()
+        .height(48.dp)
+        .background(styleguide_grey_blue_background)
+        .padding(horizontal = 16.dp),
+      horizontalArrangement = Arrangement.SpaceBetween,
+      verticalAlignment = Alignment.CenterVertically
+    ) {
+      Row() {
         val composition by
         rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.bonus_gift_animation))
         val progress by animateLottieCompositionAsState(composition, iterations = Int.MAX_VALUE)
@@ -193,25 +205,25 @@ fun GamificationHeader(
 
         Text(
           text = stringResource(id = R.string.rewards_bonus_every_purchase_title, bonusValue),
-          fontSize = 16.sp,
+          style = MaterialTheme.typography.titleSmall,
           fontWeight = FontWeight.Bold,
           color = WalletColors.styleguide_light_grey,
           lineHeight = 24.sp,
           modifier =
           Modifier
             .align(Alignment.CenterVertically)
-            .padding(horizontal = 6.dp)
-            .weight(1f, fill = false)
-        )
-        Image(
-          painter = painterResource(R.drawable.ic_arrow_right),
-          "Arrow",
-          modifier = Modifier
-            .height(36.dp)
-            .width(36.dp)
-            .align(Alignment.CenterVertically)
+            .padding(start = 8.dp)
         )
       }
+
+      Image(
+        painter = painterResource(R.drawable.ic_arrow_right),
+        contentDescription = null,
+        modifier = Modifier
+          .height(40.dp)
+          .scale(1.3F)
+          .align(Alignment.CenterVertically)
+      )
     }
   }
 }
@@ -324,8 +336,8 @@ fun VipReferralCard(onClick: () -> Unit, vipBonus: String) {
       horizontalArrangement = Arrangement.Start,
     ) {
       Image(
-        painter = painterResource(R.drawable.ic_vip_symbol), // TODO
-        "VIP",
+        painter = painterResource(R.drawable.ic_vip_symbol),
+        contentDescription = stringResource(R.string.vip),
         modifier = Modifier
           .height(64.dp)
           .width(64.dp)
@@ -353,7 +365,7 @@ fun VipReferralCard(onClick: () -> Unit, vipBonus: String) {
       }
       Image(
         painter = painterResource(R.drawable.ic_arrow_right),
-        "Arrow",
+        contentDescription = null,
         modifier = Modifier
           .height(36.dp)
           .width(36.dp)
