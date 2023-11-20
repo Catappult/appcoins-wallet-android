32cdd996eb7a1b7bcf923660b0a65beb62a69a93
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2023-11-20 09:19:06 +0000

    feature(APPC-4035) VK Pay new design: First payment flow

---
+++ app/src/main/java/com/asfoundation/wallet/onboarding_new_payment/vkPayment/OnboardingVkPaymentNavigator.kt
@@ -0,0 +1,47 @@
+package com.asfoundation.wallet.onboarding_new_payment.vkPayment
+
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.appcoins.wallet.billing.adyen.PaymentModel
+import com.appcoins.wallet.core.arch.data.Navigator
+import com.appcoins.wallet.core.arch.data.navigate
+import com.appcoins.wallet.gamification.repository.ForecastBonusAndLevel
+import com.asfoundation.wallet.billing.adyen.PaymentType
+import com.asfoundation.wallet.entity.TransactionBuilder
+import javax.inject.Inject
+
+class OnboardingVkPaymentNavigator @Inject constructor(private val fragment: Fragment) :
+  Navigator {
+
+  fun navigateBack() {
+    fragment.findNavController().popBackStack()
+  }
+
+  /**
+   * passed navController should be used to be able to replicate the design, but fragment.findNavController()
+   * is a temporary solution to navigate, even though its to the wrong navController
+   * see: https://stackoverflow.com/questions/50730494/new-navigation-component-from-arch-with-nested-navigation-graph
+   * see: https://stackoverflow.com/questions/74717334/android-nested-fragment-navigation?noredirect=1#comment132543414_74717334
+   */
+
+  fun navigateToPaymentResult(
+    transactionBuilder: TransactionBuilder,
+    paymentType: PaymentType,
+    amount: String,
+    currency: String,
+    forecastBonus: ForecastBonusAndLevel
+  ) {
+    navigate(
+      fragment.findNavController(),
+      OnboardingVkPaymentFragmentDirections.actionNavigateToOnboardingPaymentResult(
+        PaymentModel(),
+        transactionBuilder,
+        paymentType,
+        amount,
+        currency,
+        forecastBonus
+      )
+    )
+  }
+}
