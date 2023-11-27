10511f878c32878c2feb21bf9d5d2aaa2ccf5d37
Giovanni Di Luca Evangelista <127108758+giovanni-aptoide@users.noreply.github.com>
2023-11-27 16:16:33 +0000

    feat(APPC-3853): update methods endpoint version

--- core/network/microservices/src/main/java/com/appcoins/wallet/core/network/microservices/api/broker/BrokerBdsApi.kt
+++ core/network/microservices/src/main/java/com/appcoins/wallet/core/network/microservices/api/broker/BrokerBdsApi.kt
@@ -35,7 +35,7 @@ interface BrokerBdsApi {
    * if null no filter is applied by transactionType
    *
    */
-  @GET("8.20231110/methods")
+  @GET("8.20231117/methods")
   fun getPaymentMethods(
     @Query("price.value") value: String? = null,
     @Query("price.currency") currency: String? = null,
