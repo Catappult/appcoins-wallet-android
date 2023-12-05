7cf50b6c3c057d9b366f31e05b037562eed53846
Rodolfo Cardoso <97184858+rodolfobcardoso@users.noreply.github.com>
2023-12-05 18:37:36 +0000

    Revert "Revert "[APPC-3973] Recover wallet from web download""

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 android {
   defaultConfig {
     applicationId = "com.appcoins.wallet"
-    versionCode = 289
-    versionName = "3.7.1"
+    versionCode = 288
+    versionName = "3.7.0"
   }
 }
 
---
+++ app/src/main/java/com/asfoundation/wallet/onboarding/OnboardingAnalytics.kt
@@ -0,0 +1,22 @@
+package com.asfoundation.wallet.onboarding
+
+import cm.aptoide.analytics.AnalyticsManager
+import java.util.*
+import javax.inject.Inject
+
+class OnboardingAnalytics @Inject constructor(private val analyticsManager: AnalyticsManager) {
+
+  companion object {
+    private const val WALLET = "WALLET"
+    const val WALLET_ONBOARDING_RECOVER_WEB = "wallet_onboarding_recover_web"
+    const val BONUS = "bonus"
+    const val BONUS_CURRENCY = "bonus_curency"
+  }
+
+  fun sendRecoverGuestWalletEvent(bonus: String, bonusCurrency: String) {
+    val data = HashMap<String, Any>()
+    data[BONUS] = bonus
+    data[BONUS_CURRENCY] = bonusCurrency
+    analyticsManager.logEvent(data, WALLET_ONBOARDING_RECOVER_WEB, AnalyticsManager.Action.CLICK, WALLET)
+  }
+}
