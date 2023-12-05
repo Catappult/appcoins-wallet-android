3aec81daa18d8f76a5add30d331358d1fb0d9b39
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2023-12-05 10:36:28 +0000

    289 version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 android {
   defaultConfig {
     applicationId = "com.appcoins.wallet"
-    versionCode = 286
-    versionName = "3.6.0"
+    versionCode = 289
+    versionName = "3.7.1"
   }
 }
 
