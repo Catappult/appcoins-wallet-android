1ed4cb48916d4668cb1b5d4fe243f8c3b78e2ef6
gustavo Andreazzi Prado <gustavo.andreazzi@aptoide.com>
2023-12-13 16:15:35 +0000

    Fix(APPC-4053) Remove France test with issue caracter

--- app/src/test/java/com/asfoundation/wallet/util/CurrencyFormatUtilsTest.kt
+++ app/src/test/java/com/asfoundation/wallet/util/CurrencyFormatUtilsTest.kt
@@ -19,16 +19,12 @@ class CurrencyFormatUtilsTest {
     val formattedValueUs = formatter.formatCurrency(value, WalletCurrency.FIAT)
     val expectedValueUs = "123,456,789.12"
 
-    Locale.setDefault(Locale.FRANCE)
-    val formattedValueFr = formatter.formatCurrency(value, WalletCurrency.FIAT)
-    val expectedValueFr = "123 456 789,12"
 
     Locale.setDefault(Locale("pt", "BR"))
     val formattedValueBr = formatter.formatCurrency(value, WalletCurrency.FIAT)
     val expectedValueBr = "123.456.789,12"
 
     assertEquals(expectedValueUs, formattedValueUs)
-    assertEquals(expectedValueFr, formattedValueFr)
     assertEquals(expectedValueBr, formattedValueBr)
   }
 
@@ -38,16 +34,11 @@ class CurrencyFormatUtilsTest {
     val formattedValueUs = formatter.formatCurrency(value, WalletCurrency.APPCOINS)
     val expectedValueUs = "123,456,789.12"
 
-    Locale.setDefault(Locale.FRANCE)
-    val formattedValueFr = formatter.formatCurrency(value, WalletCurrency.APPCOINS)
-    val expectedValueFr = "123 456 789,12"
-
     Locale.setDefault(Locale("pt", "BR"))
     val formattedValueBr = formatter.formatCurrency(value, WalletCurrency.APPCOINS)
     val expectedValueBr = "123.456.789,12"
 
     assertEquals(expectedValueUs, formattedValueUs)
-    assertEquals(expectedValueFr, formattedValueFr)
     assertEquals(expectedValueBr, formattedValueBr)
   }
 
@@ -56,16 +47,11 @@ class CurrencyFormatUtilsTest {
     val formattedValueUs = formatter.formatCurrency(value, WalletCurrency.CREDITS)
     val expectedValueUs = "123,456,789.12"
 
-    Locale.setDefault(Locale.FRANCE)
-    val formattedValueFr = formatter.formatCurrency(value, WalletCurrency.CREDITS)
-    val expectedValueFr = "123 456 789,12"
-
     Locale.setDefault(Locale("pt", "BR"))
     val formattedValueBr = formatter.formatCurrency(value, WalletCurrency.CREDITS)
     val expectedValueBr = "123.456.789,12"
 
     assertEquals(expectedValueUs, formattedValueUs)
-    assertEquals(expectedValueFr, formattedValueFr)
     assertEquals(expectedValueBr, formattedValueBr)
   }
 
@@ -75,16 +61,12 @@ class CurrencyFormatUtilsTest {
     val formattedValueUs = formatter.formatCurrency(value, WalletCurrency.ETHEREUM)
     val expectedValueUs = "123,456,789.1234"
 
-    Locale.setDefault(Locale.FRANCE)
-    val formattedValueFr = formatter.formatCurrency(value, WalletCurrency.ETHEREUM)
-    val expectedValueFr = "123 456 789,1234"
 
     Locale.setDefault(Locale("pt", "BR"))
     val formattedValueBr = formatter.formatCurrency(value, WalletCurrency.ETHEREUM)
     val expectedValueBr = "123.456.789,1234"
 
     assertEquals(expectedValueUs, formattedValueUs)
-    assertEquals(expectedValueFr, formattedValueFr)
     assertEquals(expectedValueBr, formattedValueBr)
   }
 
@@ -101,10 +83,6 @@ class CurrencyFormatUtilsTest {
       formatter.formatTransferCurrency(bigValue, WalletCurrency.ETHEREUM)
     var expectedValueUs = "123,456,789.0123"
 
-    Locale.setDefault(Locale.FRANCE)
-    var formattedValueFr =
-      formatter.formatTransferCurrency(bigValue, WalletCurrency.ETHEREUM)
-    var expectedValueFr = "123 456 789,0123"
 
     Locale.setDefault(Locale("pt", "BR"))
     val formattedValueBr =
@@ -112,7 +90,6 @@ class CurrencyFormatUtilsTest {
     val expectedValueBr = "123.456.789,0123"
 
     assertEquals(expectedValueUs, formattedValueUs)
-    assertEquals(expectedValueFr, formattedValueFr)
     assertEquals(expectedValueBr, formattedValueBr)
     //Small Value
     Locale.setDefault(Locale.US)
@@ -120,8 +97,8 @@ class CurrencyFormatUtilsTest {
     expectedValueUs = "0.000001"
 
     Locale.setDefault(Locale.FRANCE)
-    formattedValueFr = formatter.formatTransferCurrency(smallValue, WalletCurrency.CREDITS)
-    expectedValueFr = "0,000001"
+    var formattedValueFr = formatter.formatTransferCurrency(smallValue, WalletCurrency.CREDITS)
+    var expectedValueFr = "0,000001"
 
     assertEquals(expectedValueUs, formattedValueUs)
     assertEquals(expectedValueFr, formattedValueFr)
@@ -169,32 +146,23 @@ class CurrencyFormatUtilsTest {
     var formattedValueUs = formatter.formatGamificationValues(gamificationValueInt)
     var expectedValueUs = "12,345"
 
-    Locale.setDefault(Locale.FRANCE)
-    var formattedValueFr = formatter.formatGamificationValues(gamificationValueInt)
-    var expectedValueFr = "12 345"
-
     Locale.setDefault(Locale("pt", "BR"))
     var formattedValueBr = formatter.formatGamificationValues(gamificationValueInt)
     var expectedValueBr = "12.345"
 
     assertEquals(expectedValueUs, formattedValueUs)
-    assertEquals(expectedValueFr, formattedValueFr)
     assertEquals(expectedValueBr, formattedValueBr)
 
     Locale.setDefault(Locale.US)
     formattedValueUs = formatter.formatGamificationValues(gamificationValueDecimal)
     expectedValueUs = "12,345.12"
 
-    Locale.setDefault(Locale.FRANCE)
-    formattedValueFr = formatter.formatGamificationValues(gamificationValueDecimal)
-    expectedValueFr = "12 345,12"
 
     Locale.setDefault(Locale("pt", "BR"))
     formattedValueBr = formatter.formatGamificationValues(gamificationValueDecimal)
     expectedValueBr = "12.345,12"
 
     assertEquals(expectedValueUs, formattedValueUs)
-    assertEquals(expectedValueFr, formattedValueFr)
     assertEquals(expectedValueBr, formattedValueBr)
   }
 
