4f66f36e56d1ed3c8a8cb4bb560e732d977e08f6
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2023-12-21 17:17:29 +0000

    feat(APPC-4084): remove comments

--- app/src/main/java/com/asfoundation/wallet/billing/vkpay/usecases/CreateVkPayTransactionTopUpUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/vkpay/usecases/CreateVkPayTransactionTopUpUseCase.kt
@@ -31,9 +31,9 @@ class CreateVkPayTransactionTopUpUseCase @Inject constructor(
           callbackUrl = null,
           transactionType = TOP_UP_TRANSACTION_TYPE,
           developerWallet = null,
-          entityOemId = null,  //TODO
-          entityDomain = null,  //TODO
-          entityPromoCode = null,  //TODO
+          entityOemId = null,
+          entityDomain = null,
+          entityPromoCode = null,
           userWallet = null,
           referrerUrl = null,
           method = METHOD_VK_PAY,
