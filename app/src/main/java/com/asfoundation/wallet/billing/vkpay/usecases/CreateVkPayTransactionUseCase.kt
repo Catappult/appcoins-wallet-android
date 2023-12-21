746c129265ea5bb026bfbfca124e9c59240cf53e
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2023-12-21 17:06:40 +0000

    feat(APPC-4084): VK Pay: add promocodes and oemid to payments with VK

--- app/src/main/java/com/asfoundation/wallet/billing/vkpay/usecases/CreateVkPayTransactionUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/vkpay/usecases/CreateVkPayTransactionUseCase.kt
@@ -1,15 +1,18 @@
 package com.asfoundation.wallet.billing.vkpay.usecases
 
-import com.appcoins.wallet.core.walletservices.WalletService
-import com.asf.wallet.BuildConfig
+import com.appcoins.wallet.core.analytics.analytics.partners.AddressService
 import com.appcoins.wallet.core.network.microservices.model.VkPayTransaction
 import com.appcoins.wallet.core.network.microservices.model.VkPrice
+import com.appcoins.wallet.core.walletservices.WalletService
+import com.appcoins.wallet.feature.promocode.data.use_cases.GetCurrentPromoCodeUseCase
 import com.asfoundation.wallet.billing.vkpay.repository.VkPayRepository
 import io.reactivex.Single
 import javax.inject.Inject
 
 class CreateVkPayTransactionUseCase @Inject constructor(
+  private val partnerAddressService: AddressService,
   private val walletService: WalletService,
+  private val getCurrentPromoCodeUseCase: GetCurrentPromoCodeUseCase,
   private val vkPayRepository: VkPayRepository,
 ) {
 
@@ -22,28 +25,35 @@ class CreateVkPayTransactionUseCase @Inject constructor(
     email: String,
     phone: String
   ): Single<VkPayTransaction> {
-    return walletService.getWalletAddress()
-      .flatMap { address ->
-        vkPayRepository.createTransaction(
-          price = price,
-          reference = reference,
-          walletAddress = address,
-          origin = origin,
-          packageName = packageName,
-          metadata = metadata,
-          sku = sku,
-          callbackUrl = callbackUrl,
-          transactionType = transactionType,
-          developerWallet = developerWallet,
-          entityOemId = null, // TODO
-          entityDomain = null, //TODO
-          entityPromoCode = null, //TODO
-          userWallet = null,
-          referrerUrl = referrerUrl,
-          method = METHOD_VK_PAY,
-          email = email,
-          phone = phone
-        )
+    return Single.zip(
+      walletService.getWalletAddress(),
+      partnerAddressService.getAttribution(packageName ?: "")
+    ) { address, attributionEntity -> Pair(address, attributionEntity) }
+      .flatMap { pair ->
+        val address = pair.first
+        val attrEntity = pair.second
+        getCurrentPromoCodeUseCase().flatMap { promoCode ->
+          vkPayRepository.createTransaction(
+            price = price,
+            reference = reference,
+            walletAddress = address,
+            origin = origin,
+            packageName = packageName,
+            metadata = metadata,
+            sku = sku,
+            callbackUrl = callbackUrl,
+            transactionType = transactionType,
+            developerWallet = developerWallet,
+            entityOemId = attrEntity.oemId,
+            entityDomain = attrEntity.domain,
+            entityPromoCode = promoCode.code,
+            userWallet = address,
+            referrerUrl = referrerUrl,
+            method = METHOD_VK_PAY,
+            email = email,
+            phone = phone
+          )
+        }
       }
   }
 
