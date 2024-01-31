513ecebd15529384fcdd4611dbf86290c936cb41
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2024-01-31 14:59:47 +0000

    fix(APPC-3850): unity tests

--- legacy/appcoinsRewards/src/test/kotlin/com/appcoins/wallet/appcoins/rewards/AppcoinsRewardsTest.kt
+++ legacy/appcoinsRewards/src/test/kotlin/com/appcoins/wallet/appcoins/rewards/AppcoinsRewardsTest.kt
@@ -4,11 +4,9 @@ import com.appcoins.wallet.appcoins.rewards.repository.BdsAppcoinsRewardsReposit
 import com.appcoins.wallet.appcoins.rewards.repository.RemoteRepository
 import com.appcoins.wallet.appcoins.rewards.repository.WalletService
 import com.appcoins.wallet.bdsbilling.Billing
-import com.appcoins.wallet.core.network.microservices.model.Transaction as TransactionCore
 import com.appcoins.wallet.core.network.microservices.model.Gateway
 import com.appcoins.wallet.core.utils.jvm_common.MemoryCache
 import com.google.gson.Gson
-import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.observers.TestObserver
 import io.reactivex.schedulers.TestScheduler
@@ -25,6 +23,7 @@ import retrofit2.Response
 import java.math.BigDecimal
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
+import com.appcoins.wallet.core.network.microservices.model.Transaction as TransactionCore
 
 @RunWith(MockitoJUnitRunner::class)
 class AppcoinsRewardsTest {
@@ -32,12 +31,10 @@ class AppcoinsRewardsTest {
     private const val USER_ADDRESS: String = "0xd9BA3c6932a5084D0CA0769893353D60b23AAfC4"
     private const val USER_ADDRESS_SIGNATURE: String =
         "27c3217155834a21fa8f97df99053f2874727837c03805c2eb1ba56383473b2a07fd865dd5db1359a717dfec9aa14bab6437184b14969ec3551b86e9d29c98d401"
-    private const val DEVELOPER_ADDRESS: String = "0x652d25ac09f79e9619fba99f34f0d8420d0956b1"
+    private const val DEVELOPER_ADDRESS: String = ""
     private const val OEM_ADDRESS: String = "0x652d25ac09f79e9619fba99f34f0d8420d0956b1"
     private const val STORE_ADDRESS: String = "0x652d25ac09f79e9619fba99f34f0d8420d0956b1"
     private const val SKU: String = "cm.aptoide.pt:gas"
-    private const val PURCHASE_UID = "ve43f95meo"
-    private val BALANCE: BigDecimal = BigDecimal(2)
     private const val TYPE: String = "INAPP"
     private const val TYPE_TRANSFER: String = "TRANSFER"
     private const val PACKAGE_NAME = "PACKAGE_NAME"
@@ -146,12 +143,12 @@ class AppcoinsRewardsTest {
         .assertComplete()
     val mutableListOf = mutableListOf(
         Transaction(
-            SKU, TYPE, STORE_ADDRESS, DEVELOPER_ADDRESS, OEM_ADDRESS, PACKAGE_NAME,
+            SKU, TYPE, DEVELOPER_ADDRESS, STORE_ADDRESS, OEM_ADDRESS, PACKAGE_NAME,
             PRICE, BDS_ORIGIN, Transaction.Status.PROCESSING, null, null, null, null, null, null,
             null
         ),
         Transaction(
-            SKU, TYPE, STORE_ADDRESS, DEVELOPER_ADDRESS, OEM_ADDRESS, PACKAGE_NAME,
+            SKU, TYPE, DEVELOPER_ADDRESS, STORE_ADDRESS, OEM_ADDRESS, PACKAGE_NAME,
             PRICE, BDS_ORIGIN, Transaction.Status.COMPLETED, UID, null, null, null, null,
             null,
             null
