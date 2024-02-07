fed00ddbbf3cccfabf47f0030dcd2d4c5af2486a
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2024-02-07 12:57:25 +0000

    fix(APPC-4049): gp build variant line up

--- ui/widgets/src/gp/java/com/appcoins/wallet/ui/widgets/GamesBundleComposable.kt
+++ ui/widgets/src/gp/java/com/appcoins/wallet/ui/widgets/GamesBundleComposable.kt
@@ -1,10 +1,12 @@
 package com.appcoins.wallet.ui.widgets
 
 import androidx.compose.runtime.Composable
+import com.appcoins.wallet.core.analytics.analytics.legacy.GetAppAnalytics
 
 @Composable
 fun GamesBundle(
   items: List<GameData>,
+  analytics: GetAppAnalytics,
   fetchFromApiCallback: () -> Unit
 ) {
 
--- ui/widgets/src/gp/java/com/appcoins/wallet/ui/widgets/OpenGameFunction.kt
+++ ui/widgets/src/gp/java/com/appcoins/wallet/ui/widgets/OpenGameFunction.kt
@@ -4,17 +4,25 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat.startActivity
+import com.appcoins.wallet.core.analytics.analytics.legacy.GetAppAnalytics
 
 
-fun openGame(gamePackage: String?, actionUrl: String?, context: Context) {
+fun openGame(
+  gamePackage: String?,
+  actionUrl: String?,
+  context: Context,
+  getAppAnalytics: GetAppAnalytics?
+) {
   try {
     val launchIntent: Intent? = gamePackage?.let {
       context.packageManager.getLaunchIntentForPackage(
         it
       )
     }
-    if (launchIntent != null)
+    if (launchIntent != null) {
+      getAppAnalytics?.sendGetAppAEvent(gamePackage, GetAppAnalytics.OPEN_ACTION)
       startActivity(context, launchIntent, null)
+    }
   } catch (e: Throwable) {
   }
 }
