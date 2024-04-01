b158e0a04576d8cea69d7d4c7b13941b3953be26
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2024-04-01 15:53:37 +0100

    feat(APPC-4187): multiple cards: use the user's currency

--- app/src/main/java/com/asfoundation/wallet/manage_cards/usecases/GetStoredCardsUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/manage_cards/usecases/GetStoredCardsUseCase.kt
@@ -2,22 +2,21 @@ package com.asfoundation.wallet.manage_cards.usecases
 
 import com.adyen.checkout.components.model.paymentmethods.StoredPaymentMethod
 import com.appcoins.wallet.billing.adyen.AdyenPaymentRepository
-import com.appcoins.wallet.billing.adyen.PaymentInfoModel
 import com.appcoins.wallet.core.network.base.EwtAuthenticatorService
 import com.appcoins.wallet.core.utils.android_common.RxSchedulers
 import com.appcoins.wallet.core.walletservices.WalletService
-import com.wallet.appcoins.feature.support.data.SupportRepository
-import io.intercom.android.sdk.Intercom
+import com.appcoins.wallet.feature.changecurrency.data.use_cases.GetCachedCurrencyUseCase
 import io.reactivex.Single
 import javax.inject.Inject
 
 class GetStoredCardsUseCase @Inject constructor(
   private val adyenPaymentRepository: AdyenPaymentRepository,
   private val walletService: WalletService,
+  private val getCachedCurrencyUseCase: GetCachedCurrencyUseCase,
   private val ewtObtainer: EwtAuthenticatorService,
   private val rxSchedulers: RxSchedulers,
 
-) {
+  ) {
 
   operator fun invoke(): Single<List<StoredPaymentMethod>> {
     return Single.zip(
@@ -29,11 +28,12 @@ class GetStoredCardsUseCase @Inject constructor(
       .flatMap { pair ->
         val wallet = pair.first
         val ewt = pair.second
+        val currency = getCachedCurrencyUseCase()
         adyenPaymentRepository
           .getStoredCards(
             AdyenPaymentRepository.Methods.CREDIT_CARD,
             "10",
-            "USD",
+            currency, // this shouldn't be dependent on the currency, the user should be able to see all cards listed. This limitation arises because we have 2 separate merchants
             wallet,
             ewt
           )
