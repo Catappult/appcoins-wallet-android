cfe0f4931320ab75efe64e393a8152cd5783608a
Ricardo Frango <ricardo.frango@aptoide.com>
2024-08-28 11:23:34 +0100

    [APPC-4534] Add IAP new entry point

---
+++ app/src/main/java/com/asfoundation/wallet/iab/PaymentActivity.kt
@@ -0,0 +1,33 @@
+package com.asfoundation.wallet.iab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.asf.wallet.R
+import com.asfoundation.wallet.iab.di.PaymentActivityNavigatorFactory
+import com.asfoundation.wallet.iab.domain.model.PurchaseData
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class PaymentActivity : AppCompatActivity() {
+
+  private val navHostFragment: NavHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.fragment_nav_host) as NavHostFragment }
+
+  @Inject
+  lateinit var paymentActivityFactory: PaymentActivityNavigatorFactory
+
+  private val navigator by lazy { paymentActivityFactory.create(navHostFragment.navController) }
+
+  private val uri by lazy { intent?.data }
+
+  private val purchaseData by lazy { PurchaseData() }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_payment)
+
+    navigator.navigateToInitialScreen(purchaseData)
+  }
+
+}
