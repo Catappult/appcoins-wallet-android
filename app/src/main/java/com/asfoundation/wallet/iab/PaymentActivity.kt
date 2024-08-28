5663acfcbf24e888649ad0a0488c79a035d3af15
Ricardo Frango <ricardo.frango@aptoide.com>
2024-08-28 16:45:56 +0100

    [APPC-4534] Add PurchaseData model

--- app/src/main/java/com/asfoundation/wallet/iab/PaymentActivity.kt
+++ app/src/main/java/com/asfoundation/wallet/iab/PaymentActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.asf.wallet.R
 import com.asfoundation.wallet.iab.di.PaymentActivityNavigatorFactory
-import com.asfoundation.wallet.iab.domain.model.PurchaseData
+import com.asfoundation.wallet.iab.domain.model.emptyPurchaseData
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -21,7 +21,7 @@ class PaymentActivity : AppCompatActivity() {
 
   private val uri by lazy { intent?.data }
 
-  private val purchaseData by lazy { PurchaseData() }
+  private val purchaseData by lazy { emptyPurchaseData }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
--- app/src/main/java/com/asfoundation/wallet/iab/domain/model/PurchaseData.kt
+++ app/src/main/java/com/asfoundation/wallet/iab/domain/model/PurchaseData.kt
@@ -1,9 +1,40 @@
 package com.asfoundation.wallet.iab.domain.model
 
+import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.math.BigDecimal
+import kotlin.random.Random
 
 @Parcelize
 data class PurchaseData(
-  val domain: String = ""
+  val referrerUrl: String,
+  val type: String, // this one is strange. we have a TYPE but we use the ESKILLS to build this in OneStepTransactionParser
+  val origin: String?,
+  val skuId: String?,
+  val domain: String,
+  val productToken: String?,
+  val payload: String?,
+  val callbackUrl: String?,
+  val orderReference: String?,
+  val value: String?, // originalOneStepValue in previous implementation
+  val currency: String?, // originalOneStepCurrency in previous implementation
+  val oemId: String?,
+  val oemPackage: String?,
 ) : Parcelable
+
+val emptyPurchaseData = PurchaseData(
+  skuId = "PurchaseRequest product",
+  type = "Type",
+  origin = "Origin",
+  domain = "PurchaseRequest domain",
+  callbackUrl = "PurchaseRequest callback url",
+  orderReference = "PurchaseRequest order reference",
+  value = Random.nextDouble(1.0, 100.0).toString(),
+  currency = "PurchaseRequest currency",
+  oemId = "PurchaseRequest oemid",
+  oemPackage = "PurchaseRequest oempackage",
+  referrerUrl = Uri.EMPTY.toString(),
+  payload = "metadata",
+  productToken = "Product Token",
+)
