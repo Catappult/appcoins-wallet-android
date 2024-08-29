baf69edde2de83c12f0b567697c937e066d0eae0
Ricardo Frango <ricardo.frango@aptoide.com>
2024-08-29 15:43:02 +0100

    [APPC-4532] Add OSP uri parser

--- app/src/main/java/com/asfoundation/wallet/iab/PaymentActivity.kt
+++ app/src/main/java/com/asfoundation/wallet/iab/PaymentActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.asf.wallet.R
+import com.asfoundation.wallet.iab.di.GenericUriParser
 import com.asfoundation.wallet.iab.di.PaymentActivityNavigatorFactory
-import com.asfoundation.wallet.iab.domain.model.emptyPurchaseData
+import com.asfoundation.wallet.iab.domain.model.PurchaseData
+import com.asfoundation.wallet.iab.error.IABError
+import com.asfoundation.wallet.iab.parser.UriParser
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -17,17 +20,29 @@ class PaymentActivity : AppCompatActivity() {
   @Inject
   lateinit var paymentActivityFactory: PaymentActivityNavigatorFactory
 
+  @Inject
+  @GenericUriParser
+  lateinit var uriParser: UriParser
+
   private val navigator by lazy { paymentActivityFactory.create(navHostFragment.navController) }
 
   private val uri by lazy { intent?.data }
 
-  private val purchaseData by lazy { emptyPurchaseData }
+  private val purchaseData: Pair<PurchaseData?, IABError?> by lazy {
+    runCatching { uriParser.parse(uri) }
+      .mapCatching { it to null }
+      .onFailure {
+        it.printStackTrace()
+        null to IABError(it.message ?: "Unknown error")
+      }
+      .getOrDefault(null to IABError("Unknown error occurred while parsing"))
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_payment)
 
-    navigator.navigateToInitialScreen(purchaseData)
+    navigator.navigateToInitialScreen(purchaseData.first, purchaseData.second)
   }
 
 }
--- app/src/main/java/com/asfoundation/wallet/iab/PaymentActivityNavigator.kt
+++ app/src/main/java/com/asfoundation/wallet/iab/PaymentActivityNavigator.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import com.asf.wallet.R
 import com.asfoundation.wallet.iab.domain.model.PurchaseData
+import com.asfoundation.wallet.iab.error.IABError
 import com.asfoundation.wallet.iab.presentation.main.MainFragmentArgs
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -16,10 +17,10 @@ class PaymentActivityNavigator @AssistedInject constructor(
     navController.navigate(navDirections)
   }
 
-  fun navigateToInitialScreen(purchaseData: PurchaseData) {
+  fun navigateToInitialScreen(purchaseData: PurchaseData?, error: IABError?) {
     navController.setGraph(
       graphResId = R.navigation.iab_graph,
-      startDestinationArgs = MainFragmentArgs(purchaseData).toBundle(),
+      startDestinationArgs = MainFragmentArgs(purchaseData, error).toBundle(),
     )
   }
 }
