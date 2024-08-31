ffba7d2d980ff4ea859237288988dca3f519c40a
Ricardo Frango <ricardo.frango@aptoide.com>
2024-08-31 10:37:41 +0100

    [APPC-4532] Add SDK uri parser

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,12 @@ android {
     applicationId = "com.appcoins.wallet"
     versionCode = 338
     versionName = "3.33.0"
+
+    val ospScheme = "https"
+
+    buildConfigField("String", "OSP_SCHEME", "\"$ospScheme\"")
+    manifestPlaceholders["ospScheme"] = ospScheme
+    manifestPlaceholders["sdkScheme"] = "appcoins-sdk"
   }
 }
 
