1086b9454f6ca71f6a0168bd8566b7ab15ac0b31
Gustavo Andreazzi Prado <gustavo.andreazzi@aptoide,com>
2024-09-03 16:08:34 +0100

    Feature(APPC-4530) Remove extra subscribeOn in Use Case

--- app/src/main/java/com/asfoundation/wallet/home/usecases/GetImpressionUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/home/usecases/GetImpressionUseCase.kt
@@ -1,6 +1,6 @@
 package com.asfoundation.wallet.home.usecases
 
-import com.appcoins.wallet.core.utils.android_common.RxSchedulers
+
 import com.asfoundation.wallet.repository.ImpressionRepository
 import io.reactivex.Completable
 import javax.inject.Inject
@@ -9,9 +9,8 @@ class GetImpressionUseCase
 @Inject
 constructor(
   private val impressionRepository: ImpressionRepository,
-  private val schedulers: RxSchedulers
 ) {
   operator fun invoke(): Completable {
-    return impressionRepository.getImpression().subscribeOn(schedulers.io)
+    return impressionRepository.getImpression()
   }
 }
