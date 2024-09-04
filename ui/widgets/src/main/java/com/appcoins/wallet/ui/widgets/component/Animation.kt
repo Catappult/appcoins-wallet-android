f6cce3137846f3bf1cb2133d989fd31275efac28
Ricardo Frango <ricardo.frango@aptoide.com>
2024-09-04 17:36:26 +0100

    [APPC-4515] Add Success screen

--- ui/widgets/src/main/java/com/appcoins/wallet/ui/widgets/component/Animation.kt
+++ ui/widgets/src/main/java/com/appcoins/wallet/ui/widgets/component/Animation.kt
@@ -3,14 +3,15 @@ package com.appcoins.wallet.ui.widgets.component
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 
 @Composable
-fun Animation(modifier: Modifier, animationRes: Int, iterations: Int = Int.MAX_VALUE) {
+fun Animation(modifier: Modifier = Modifier, animationRes: Int, iterations: Int = Int.MAX_VALUE, contentScale: ContentScale = ContentScale.Fit) {
   val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(animationRes))
   val progress by animateLottieCompositionAsState(composition, iterations = iterations)
-  LottieAnimation(modifier = modifier, composition = composition, progress = { progress })
+  LottieAnimation(modifier = modifier, composition = composition, progress = { progress }, contentScale = contentScale)
 }
