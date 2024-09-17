703cca6c39150dafbfe534e44765e7cbbb43ee79
Ricardo Frango <ricardo.frango@aptoide.com>
2024-09-17 11:41:06 +0100

    [APPC-4523] Add Withdrawal disclaimer api

--- app/src/main/java/com/asfoundation/wallet/ui/iab/InAppPurchaseInteractor.java
+++ app/src/main/java/com/asfoundation/wallet/ui/iab/InAppPurchaseInteractor.java
@@ -537,7 +537,7 @@ public class InAppPurchaseInteractor {
     }
   }
 
-  boolean hasPreSelectedPaymentMethod() {
+  public boolean hasPreSelectedPaymentMethod() {
     return sharedPreferences.contains(PRE_SELECTED_PAYMENT_METHOD_KEY);
   }
 
