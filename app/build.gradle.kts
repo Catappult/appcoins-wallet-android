3dfc9d80815cfaaced2001d7552f76b2448e47d4
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2024-10-09 13:50:56 +0100

    [APPC-4591] version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
   namespace = "com.asf.wallet"
   defaultConfig {
     applicationId = "com.appcoins.wallet"
-    versionCode = 326
-    versionName = "3.29.1"
+    versionCode = 327
+    versionName = "3.29.2"
   }
 }
 
