aa8f46dbaf33ed7466692bb100a11138fd229cda
Ricardo Frango <ricardo.frango@aptoide.com>
2024-10-11 18:15:16 +0100

    [APPC-4447] Update zxing libs
    Because of this change, the swipe refresh layout lib had to be added

--- ui/widgets/build.gradle.kts
+++ ui/widgets/build.gradle.kts
@@ -33,4 +33,5 @@ dependencies {
   implementation(libs.bundles.androidx.compose)
   implementation(libs.bundles.coil)
   implementation(libs.compose.lottie)
+  implementation(libs.androidx.swiperefreshlayout)
 }
