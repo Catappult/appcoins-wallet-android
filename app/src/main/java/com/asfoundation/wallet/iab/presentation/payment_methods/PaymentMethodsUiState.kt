120960c74361aadaa60d917fbd72945205b5b1f2
Ricardo Frango <ricardo.frango@aptoide.com>
2024-10-18 14:26:41 +0100

    [APPC-4573] Add initial setup of payment methods screen

---
+++ app/src/main/java/com/asfoundation/wallet/iab/presentation/payment_methods/PaymentMethodsUiState.kt
@@ -0,0 +1,11 @@
+package com.asfoundation.wallet.iab.presentation.payment_methods
+
+import com.asfoundation.wallet.iab.presentation.PaymentMethodData
+import com.asfoundation.wallet.iab.presentation.PurchaseInfoData
+
+sealed class PaymentMethodsUiState {
+  data class LoadingPaymentMethods(val purchaseInfo: PurchaseInfoData): PaymentMethodsUiState()
+  data class PaymentMethodsIdle(val purchaseInfo: PurchaseInfoData, val paymentMethods: List<PaymentMethodData>): PaymentMethodsUiState()
+  data object PaymentMethodsError: PaymentMethodsUiState()
+  data object NoConnection: PaymentMethodsUiState()
+}
