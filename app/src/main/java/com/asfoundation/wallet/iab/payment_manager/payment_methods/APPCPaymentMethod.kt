051c79307098a24f1faa7e7c0442a94f0a67d96c
Ricardo Frango <ricardo.frango@aptoide.com>
2024-10-30 22:36:15 +0000

    [APPC-4659] Fix the cost on IAP header for APPC

--- app/src/main/java/com/asfoundation/wallet/iab/payment_manager/payment_methods/APPCPaymentMethod.kt
+++ app/src/main/java/com/asfoundation/wallet/iab/payment_manager/payment_methods/APPCPaymentMethod.kt
@@ -1,11 +1,13 @@
 package com.asfoundation.wallet.iab.payment_manager.payment_methods
 
+import android.content.Context
 import com.appcoins.wallet.core.network.microservices.model.PaymentMethodEntity
 import com.appcoins.wallet.core.utils.android_common.CurrencyFormatUtils
 import com.appcoins.wallet.feature.walletInfo.data.wallet.domain.WalletInfo
 import com.asfoundation.wallet.iab.domain.model.ProductInfoData
 import com.asfoundation.wallet.iab.domain.model.PurchaseData
 import com.asfoundation.wallet.iab.payment_manager.PaymentMethod
+import java.math.BigDecimal
 
 class APPCPaymentMethod(
   paymentMethod: PaymentMethodEntity,
@@ -15,8 +17,33 @@ class APPCPaymentMethod(
   private val walletInfo: WalletInfo
 ) : PaymentMethod(paymentMethod) {
 
+  private val hasFunds
+    get() = walletInfo.walletBalance.creditsBalance.fiat.amount >= paymentMethod.price.value
+
+  override val isEnable: Boolean
+    get() = paymentMethod.isAvailable() && hasFunds
+  override val cost: BigDecimal
+    get() = productInfoData.transaction.amount
+  override val taxes: BigDecimal?
+    get() = null
+  override val subtotal: BigDecimal?
+    get() = null
+  override val currency: String
+    get() = productInfoData.transaction.currency
+  override val hasFees: Boolean
+    get() = false
+
   override val onBuyClick: () -> Unit
     get() = { }
+
+  override fun getDescription(context: Context) =
+    walletInfo.walletBalance.creditsBalance.fiat.run {
+      "$symbol ${currencyFormatUtils.formatCurrency(amount)}"
+    }
+      .takeIf { hasFunds }
+      ?: walletInfo.walletBalance.creditsBalance.fiat.run {
+        "Insufficient funds: $symbol ${currencyFormatUtils.formatCurrency(amount)}"
+      }
 
   override fun createTransaction() {
     TODO("Not yet implemented")
