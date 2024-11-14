d4a260a134e560b88236c4f7e317c09661403099
Ricardo Frango <ricardo.frango@aptoide.com>
2024-11-14 17:38:24 +0000

    [APPC-4627] Create a default PurchaseInfo with click implemented

--- app/src/main/java/com/asfoundation/wallet/iab/presentation/PurchaseInfoScreen.kt
+++ app/src/main/java/com/asfoundation/wallet/iab/presentation/PurchaseInfoScreen.kt
@@ -19,6 +19,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -56,6 +59,31 @@ fun PurchaseInfo(
     appName = appName,
     purchaseInfo = purchaseInfo,
     isExpanded = isExpanded,
+    showLoadingPrice = showLoadingPrice,
+  )
+}
+
+@Composable
+fun PurchaseInfo(
+  modifier: Modifier = Modifier,
+  purchaseInfo: PurchaseInfoData,
+  showLoadingPrice: Boolean = false,
+) {
+  var isPurchaseInfoExpanded by rememberSaveable { mutableStateOf(false) }
+  val onPurchaseInfoExpandClick = { isPurchaseInfoExpanded = !isPurchaseInfoExpanded }
+
+  PurchaseInfo(
+    modifier = modifier
+      .fillMaxWidth()
+      .clip(RoundedCornerShape(12.dp))
+      .background(IAPTheme.colors.primaryContainer)
+      .addClick(
+        onClick = onPurchaseInfoExpandClick,
+        testTag = "onPurchaseInfoExpandClick"
+      )
+      .padding(16.dp),
+    purchaseInfo = purchaseInfo,
+    isExpanded = isPurchaseInfoExpanded,
     showLoadingPrice = showLoadingPrice,
   )
 }
