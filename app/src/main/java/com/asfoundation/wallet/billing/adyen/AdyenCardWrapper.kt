45b7d7abd348269f2008e42e0be3df6fdf0dbe62
Ricardo Frango <ricardo.frango@aptoide.com>
2024-11-19 16:36:01 +0000

    [APPC-4627] Change AdyenCardWrapper to be parcelable

--- app/src/main/java/com/asfoundation/wallet/billing/adyen/AdyenCardWrapper.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/adyen/AdyenCardWrapper.kt
@@ -1,10 +1,13 @@
 package com.asfoundation.wallet.billing.adyen
 
+import android.os.Parcelable
 import com.adyen.checkout.components.model.payments.request.CardPaymentMethod
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class AdyenCardWrapper(
   val cardPaymentMethod: CardPaymentMethod,
   val shouldStoreCard: Boolean,
   val hasCvc: Boolean,
   val supportedShopperInteractions: List<String>
-)
+) : Parcelable
