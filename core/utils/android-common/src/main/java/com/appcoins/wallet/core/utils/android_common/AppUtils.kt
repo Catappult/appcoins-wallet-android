dee71e813d8d63720a74de88e924bc7268c7c201
Ricardo Frango <ricardo.frango@aptoide.com>
2024-11-28 10:46:23 +0000

    [APPC-4669] Recover intent data on restart
    There was an issue when the app was launched from a promocode or giftcard and there was no wallet. In this scenario the promocode/giftcard screens were not showing up with the data. Fix this issue by recovering the intent on restart. The onboarding is not recovering the intent because if the onboarding is appearing, its because the app was not started from a promocode/giftcard deeplink

--- core/utils/android-common/src/main/java/com/appcoins/wallet/core/utils/android_common/AppUtils.kt
+++ core/utils/android-common/src/main/java/com/appcoins/wallet/core/utils/android_common/AppUtils.kt
@@ -1,20 +1,25 @@
 package com.appcoins.wallet.core.utils.android_common
 
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import kotlinx.coroutines.delay
 
 class AppUtils {
   companion object {
-    suspend fun restartApp(context: Context) {
+    suspend fun restartApp(activity: Activity, copyIntent: Boolean = false) {
       delay(100)
-      val packageManager: PackageManager = context.packageManager
-      val intent = packageManager.getLaunchIntentForPackage(context.packageName)
+      val packageManager: PackageManager = activity.packageManager
+      val intent = packageManager.getLaunchIntentForPackage(activity.packageName)
       val componentName = intent!!.component
       val mainIntent = Intent.makeRestartActivityTask(componentName)
-      mainIntent.setPackage(context.packageName)
-      context.startActivity(mainIntent)
+      mainIntent.setPackage(activity.packageName)
+      if (copyIntent) {
+        mainIntent.data = activity.intent.data
+        mainIntent.action = activity.intent.action
+        activity.intent.extras?.let { mainIntent.putExtras(it) }
+      }
+      activity.startActivity(mainIntent)
       Runtime.getRuntime().exit(0)
     }
   }
