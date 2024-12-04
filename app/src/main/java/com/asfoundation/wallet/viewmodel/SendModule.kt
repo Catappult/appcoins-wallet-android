dbfd8e1732f6394428a7d7ff49a9fdd038ec7168
Ricardo Frango <ricardo.frango@aptoide.com>
2024-12-04 11:07:58 +0000

    [APPC-4681] Remove unused class

--- app/src/main/java/com/asfoundation/wallet/viewmodel/SendModule.kt
+++ app/src/main/java/com/asfoundation/wallet/viewmodel/SendModule.kt
@@ -1,11 +1,9 @@
-package com.asfoundation.wallet.di
+package com.asfoundation.wallet.viewmodel
 
 import com.appcoins.wallet.feature.walletInfo.data.wallet.FindDefaultWalletInteract
 import com.asfoundation.wallet.interact.FetchGasSettingsInteract
-import com.asfoundation.wallet.router.TransactionsRouter
 import com.asfoundation.wallet.router.TransferConfirmationRouter
 import com.asfoundation.wallet.util.TransferParser
-import com.asfoundation.wallet.viewmodel.SendViewModelFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,11 +18,12 @@ class SendModule {
     transferConfirmationRouter: TransferConfirmationRouter,
     fetchGasSettingsInteract: FetchGasSettingsInteract,
     transferParser: TransferParser,
-    transactionsRouter: TransactionsRouter
   ): SendViewModelFactory {
     return SendViewModelFactory(
-      findDefaultWalletInteract, fetchGasSettingsInteract,
-      transferConfirmationRouter, transferParser, transactionsRouter
+      findDefaultWalletInteract,
+      fetchGasSettingsInteract,
+      transferConfirmationRouter,
+      transferParser
     )
   }
 }
