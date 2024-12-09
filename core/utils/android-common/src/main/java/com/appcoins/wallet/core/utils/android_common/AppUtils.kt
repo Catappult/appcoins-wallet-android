ec0944265959f4d2d94619fb36594147d36b41d5
Ricardo Frango <ricardo.frango@aptoide.com>
2024-12-09 17:20:58 +0000

    [APPC-4669] Fix issue when user launches deeplink and user is already in the wallet creation screen without wallet

--- core/utils/android-common/src/main/java/com/appcoins/wallet/core/utils/android_common/AppUtils.kt
+++ core/utils/android-common/src/main/java/com/appcoins/wallet/core/utils/android_common/AppUtils.kt
@@ -5,22 +5,22 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import kotlinx.coroutines.delay
 
-class AppUtils {
-  companion object {
-    suspend fun restartApp(activity: Activity, copyIntent: Boolean = false) {
-      delay(100)
-      val packageManager: PackageManager = activity.packageManager
-      val intent = packageManager.getLaunchIntentForPackage(activity.packageName)
-      val componentName = intent!!.component
-      val mainIntent = Intent.makeRestartActivityTask(componentName)
-      mainIntent.setPackage(activity.packageName)
-      if (copyIntent) {
-        mainIntent.data = activity.intent.data
-        mainIntent.action = activity.intent.action
-        activity.intent.extras?.let { mainIntent.putExtras(it) }
-      }
-      activity.startActivity(mainIntent)
-      Runtime.getRuntime().exit(0)
+object AppUtils {
+  suspend fun restartApp(activity: Activity, copyIntent: Boolean = false) {
+    delay(100)
+    val packageManager: PackageManager = activity.packageManager
+    val intent = packageManager.getLaunchIntentForPackage(activity.packageName)
+    val componentName = intent!!.component
+    val mainIntent = Intent.makeRestartActivityTask(componentName)
+    mainIntent.setPackage(activity.packageName)
+    if (copyIntent) {
+      val copiedIntent =
+        if (activity is OnNewIntentActivityHandler) activity.getCurrentIntent() else activity.intent
+      mainIntent.data = copiedIntent.data
+      mainIntent.action = copiedIntent.action
+      activity.intent.extras?.let { mainIntent.putExtras(it) }
     }
+    activity.startActivity(mainIntent)
+    Runtime.getRuntime().exit(0)
   }
 }
