1c218bf30c3706976e85ce86bbb055bfd62528d6
Jenkins <jenkins@ip-10-40-51-45.eu-west-1.compute.internal>
2025-01-22 18:21:13 +0000

    set version code to 346 and version name to 3.37.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
   namespace = "com.asf.wallet"
   defaultConfig {
     applicationId = "com.appcoins.wallet"
-    versionCode = 345
-    versionName = "3.37.0"
+    versionCode = 346
+    versionName = "3.37.1"
   }
 }
 
