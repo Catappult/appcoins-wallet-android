a69c1bb544287fa222319a0d58560f8a7202aab2
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2025-02-06 11:46:07 +0000

    [APPC-4502] Balance string update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
   namespace = "com.asf.wallet"
   defaultConfig {
     applicationId = "com.appcoins.wallet"
-    versionCode = 347
-    versionName = "3.37.2"
+    versionCode = 348
+    versionName = "3.38.0"
   }
 }
 
