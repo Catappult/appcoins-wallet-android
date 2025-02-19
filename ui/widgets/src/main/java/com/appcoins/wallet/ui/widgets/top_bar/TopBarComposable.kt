5e24171d3bd4697cc88d37608cb8642bb49ca555
gustavo andreazzi <gustavo.andreazzi@paysimplex>
2025-02-19 15:48:30 -0300

    [APPC-4787] Change  Top bar Size and app Logo

--- ui/widgets/src/main/java/com/appcoins/wallet/ui/widgets/top_bar/TopBarComposable.kt
+++ ui/widgets/src/main/java/com/appcoins/wallet/ui/widgets/top_bar/TopBarComposable.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -111,7 +114,9 @@ fun WalletLogo() {
   Image(
     painter = painterResource(R.drawable.ic_app_logo),
     null,
-    modifier = Modifier.heightIn(max = 36.dp)
+    modifier = Modifier
+      .fillMaxHeight()
+      .width(85.dp)
   )
 }
 
