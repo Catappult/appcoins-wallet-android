de5414159657beafa915ee39f09135da3d11365c
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2025-08-19 19:34:22 +0100

    [APPC-5008] Sandbox subscriptions: base api

--- core/network/microservices/src/main/java/com/appcoins/wallet/core/network/microservices/api/broker/SandboxApi.kt
+++ core/network/microservices/src/main/java/com/appcoins/wallet/core/network/microservices/api/broker/SandboxApi.kt
@@ -3,15 +3,23 @@ package com.appcoins.wallet.core.network.microservices.api.broker
 import com.appcoins.wallet.core.network.microservices.model.*
 import io.reactivex.Single
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface SandboxApi {
 
-  @POST("8.20240524/gateways/sandbox/transactions")
+  @POST("8.20250730/gateways/sandbox/transactions")
   fun createTransaction(
     @Query("wallet.address") walletAddress: String,
     @Body sandboxPayment: SandboxPayment
+  ): Single<SandboxResponse>
+
+  @Headers("Content-Type: application/json;format=product_token")
+  @POST("8.20250730/gateways/sandbox/transactions")
+  fun createTokenTransaction(
+    @Query("wallet.address") walletAddress: String,
+    @Body sandboxPayment: SandboxTokenPayment
   ): Single<SandboxResponse>
 
 }
