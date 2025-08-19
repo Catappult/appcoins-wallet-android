b9081f0622b2044c601c514b662b77924851c3c3
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2025-08-19 19:36:12 +0100

    [APPC-5008] Sandbox subscriptions: product token logic

--- app/src/main/java/com/asfoundation/wallet/billing/sandbox/usecases/CreateSandboxTransactionUseCase.kt
+++ app/src/main/java/com/asfoundation/wallet/billing/sandbox/usecases/CreateSandboxTransactionUseCase.kt
@@ -3,6 +3,7 @@ package com.asfoundation.wallet.billing.sandbox.usecases
 import com.appcoins.wallet.core.analytics.analytics.partners.AddressService
 import com.appcoins.wallet.core.network.microservices.model.SandboxTransaction
 import com.appcoins.wallet.core.walletservices.WalletService
+import com.appcoins.wallet.feature.promocode.data.use_cases.GetCurrentPromoCodeUseCase
 import com.asfoundation.wallet.billing.sandbox.repository.SandboxRepository
 import io.reactivex.Single
 import javax.inject.Inject
@@ -10,7 +11,7 @@ import javax.inject.Inject
 class CreateSandboxTransactionUseCase @Inject constructor(
   private val partnerAddressService: AddressService,
   private val walletService: WalletService,
-  private val getCurrentPromoCodeUseCase: com.appcoins.wallet.feature.promocode.data.use_cases.GetCurrentPromoCodeUseCase,
+  private val getCurrentPromoCodeUseCase: GetCurrentPromoCodeUseCase,
   private val sandboxRepository: SandboxRepository,
 ) {
 
@@ -28,34 +29,35 @@ class CreateSandboxTransactionUseCase @Inject constructor(
     guestWalletId: String?,
   ): Single<SandboxTransaction> {
     return Single.zip(
-      walletService.getWalletAddress(),
+      walletService.getAndSignCurrentWalletAddress(),
       partnerAddressService.getAttribution(packageName)
     )
-    { address, attributionEntity -> Pair(address, attributionEntity) }
+    { walletAddressModel, attributionEntity -> Pair(walletAddressModel, attributionEntity) }
       .flatMap { pair ->
-        val address = pair.first
+        val addressModel = pair.first
         val attrEntity = pair.second
         getCurrentPromoCodeUseCase()
           .flatMap { promoCode ->
-          sandboxRepository.createTransaction(
-            value = value,
-            currency = currency,
-            reference = reference,
-            walletAddress = address,
-            origin = origin,
-            packageName = packageName,
-            metadata = metadata,
-            sku = sku,
-            callbackUrl = callbackUrl,
-            transactionType = transactionType,
-            entityOemId = attrEntity.oemId,
-            entityDomain = attrEntity.domain,
-            entityPromoCode = promoCode.code,
-            userWallet = address,
-            referrerUrl = referrerUrl,
-            guestWalletId = guestWalletId,
-          )
-        }
+            sandboxRepository.createTransaction(
+              value = value,
+              currency = currency,
+              reference = reference,
+              walletAddress = addressModel.address,
+              walletSignature = addressModel.signedAddress,
+              origin = origin,
+              packageName = packageName,
+              metadata = metadata,
+              sku = sku,
+              callbackUrl = callbackUrl,
+              transactionType = transactionType,
+              entityOemId = attrEntity.oemId,
+              entityDomain = attrEntity.domain,
+              entityPromoCode = promoCode.code,
+              userWallet = addressModel.address,
+              referrerUrl = referrerUrl,
+              guestWalletId = guestWalletId,
+            )
+          }
       }
   }
 
