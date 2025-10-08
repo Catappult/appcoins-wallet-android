113607a23357195a97f52747dd0f90ec371d927b
Arthur Oliveira <arthur.oliveira@aptoide.com>
2025-10-08 15:21:48 +0100

    CustomTabLogin style update.

--- app/src/main/java/com/asfoundation/wallet/ui/custom_tab_login/viewModel/states/CustomTabVMStates.kt
+++
@@ -1,8 +0,0 @@
-package com.asfoundation.wallet.ui.custom_tab_login.viewModel.states
-
-internal sealed class CustomTabVMStates {
-  object Initial : CustomTabVMStates()
-  object FetchingUserKey : CustomTabVMStates()
-  object FinishActivity : CustomTabVMStates()
-  object FinishWithError : CustomTabVMStates()
-}
