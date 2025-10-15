351962d5126f21082fe0596c3ac12a3c598b2a31
Arthur Oliveira <arthur.oliveira@aptoide.com>
2025-10-15 15:04:00 +0100

    processLogin minor changes.
    function refactor.
    url build:
    'is_cct' and 'version' parameters are being added to url.

--- app/src/main/java/com/asfoundation/wallet/ui/login/ProcessLogin.kt
+++ app/src/main/java/com/asfoundation/wallet/ui/login/ProcessLogin.kt
@@ -2,20 +2,12 @@ package com.asfoundation.wallet.ui.login
 
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.ACTION_VIEW
-import android.content.pm.PackageManager
-import androidx.browser.customtabs.CustomTabsClient
+import android.util.Log
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.net.toUri
-import com.asfoundation.wallet.ui.login.custom_tab_login.launchCustomChromeTabIntent
+import com.asf.wallet.BuildConfig
 import com.asfoundation.wallet.ui.login.custom_tab_login.CustomTabLoginActivity
 import com.asfoundation.wallet.ui.login.webview_login.WebViewLoginActivity
-
-
-/**
- * Dummy domain to test if there is any app that can handle the custom tab intent.
- */
-private const val DUMMY_DOMAIN = "http://www.example.com"
-private val activityIntent = Intent(ACTION_VIEW, DUMMY_DOMAIN.toUri())
 
 /**
  * Process the login request by checking if there is a browser that supports Custom Tabs.
@@ -31,24 +23,43 @@ fun processLoginRequest(
   url: String,
   context: Context
 ) {
-  val packageNames =
-    context
-      .packageManager
-      .queryIntentActivities(activityIntent, PackageManager.MATCH_ALL)
-      .map { it.activityInfo.packageName }
-
-  CustomTabsClient.getPackageName(
-    context,
-    packageNames,
-    true
-  )?.let {
-    launchCustomChromeTabIntent(
-      url = url,
-      context = context
-    )
-  } ?: run {
+  val hasCustomChromeTabAvailable = hasCustomChromeTabAvailable(context)
+  val useUrl = url
+    .addIsCctParamToUrl(hasCustomChromeTabAvailable)
+    .addVersionParamToUrl(BuildConfig.VERSION_CODE.toString())
+  Log.d("processLoginRequest", "url: $useUrl")
+  if (hasCustomChromeTabAvailable) {
+    CustomTabsIntent
+      .Builder()
+      .build()
+      .launchUrl(context, useUrl.toUri())
+  } else {
     Intent(context, WebViewLoginActivity::class.java)
-      .apply { putExtra(WebViewLoginActivity.URL, "$url&is_ctt=false") }
+      .apply { putExtra(WebViewLoginActivity.URL, useUrl) }
       .also { context.startActivity(it) }
   }
+}
+
+/**
+ * Adds the "is_cct" parameter to the URL to indicate whether Custom Tabs are being used.
+ *
+ * @param this@addIsCctParamToUrl The original URL.
+ * @param isCct A boolean indicating if Custom Tabs are being used.
+ * @return The modified URL with the "is_cct" parameter appended.
+ */
+private fun String.addIsCctParamToUrl(isCct: Boolean): String {
+  val separator = if (contains("?")) "&" else "?"
+  return "${this}${separator}is_cct=$isCct"
+}
+
+/**
+ * Adds the "version" parameter to the URL to indicate the app version.
+ *
+ * @param this@addVersionParamToUrl The original URL.
+ * @param version The app version to append.
+ * @return The modified URL with the "app_version" parameter appended.
+ */
+private fun String.addVersionParamToUrl(version: String): String {
+  val separator = if (contains("?")) "&" else "?"
+  return "${this}${separator}version=$version"
 }
