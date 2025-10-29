50e483db0a26986999f3be1a14193e5a6fa04441
Rodolfo Cardoso <rodolfo.cardoso@aptoide.com>
2025-10-29 15:40:58 +0000

    [APPC-5082] IP for Cloud Gaming: unit test

---
+++ app/src/main/java/com/asfoundation/wallet/ui/webview_payment/usecases/GetCloudIpUseCase.kt
@@ -0,0 +1,12 @@
+package com.asfoundation.wallet.ui.webview_payment.usecases
+
+import com.appcoins.wallet.sharedpreferences.CommonsPreferencesDataSource
+import javax.inject.Inject
+
+class GetCloudIpUseCase @Inject constructor(
+    private val commonsPreferencesDataSource: CommonsPreferencesDataSource
+) {
+    operator fun invoke(): String? {
+        return commonsPreferencesDataSource.getCloudIp()
+    }
+}
