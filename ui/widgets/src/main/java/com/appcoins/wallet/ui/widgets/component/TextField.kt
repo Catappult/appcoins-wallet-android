40e16ead9ac2e5d6fbffe26b388633dbf0c01cc4
Arthur Oliveira <arthur.oliveira@aptoide.com>
2026-02-03 10:47:13 +0000

    [DOT-79] Unused Imports have been removed

--- ui/widgets/src/main/java/com/appcoins/wallet/ui/widgets/component/TextField.kt
+++ ui/widgets/src/main/java/com/appcoins/wallet/ui/widgets/component/TextField.kt
@@ -6,7 +6,6 @@ import android.view.inputmethod.InputMethodManager
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
@@ -21,7 +20,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,7 +33,6 @@ import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
