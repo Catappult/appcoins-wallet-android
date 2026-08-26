a5a539673a7f5f3ec6d7394e33a76de6247fd709
Arthur Oliveira <arthur.oliveira@aptoide.com>
2026-08-26 13:41:34 +0100

    Bump version code to 391 and update version name to 5.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
 
   defaultConfig {
     applicationId = "com.appcoins.wallet"
-    versionCode = 389
-    versionName = "4.21.0"
+    versionCode = 391
+    versionName = "5.0.1"
 
     externalNativeBuild {
       cmake {
